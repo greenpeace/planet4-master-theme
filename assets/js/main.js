4be8dc2c86fa38f2af3fcf9251772585e912309b
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-09-18 15:28:12 +0530

    PLANET-1265 Working on header block markup implementation - work in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,59 @@
-jQuery( document ).ready( function( $ ) {
-  // Your JavaScript goes here
-  	console.log('Planet4 javascript reporting for duty!')
-});
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 130){  
+        $('.fixed-element, .md-navigation').addClass("sticky");
+    }
+    else{
+        $('.fixed-element, .md-navigation').removeClass("sticky");
+    }
+  });
+// Footer JS goes in this
+// Header JS goes in this.
+// Returns width of browser viewport
+if($( window ).width() >= 768) {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 130){  
+        $('.fixed-element, .md-navigation').addClass("sticky");
+    }
+    else{
+        $('.fixed-element, .md-navigation').removeClass("sticky");
+    }
+  });
+  $(document).ready(function() {
+    $(".steps-action").hide();
+    $(".step-info-wrap").click(function(){
+      $('.step-info-wrap').parent('.col-md-3').removeClass('active');
+      $(this).parent().addClass('active');
+      
+      $(".steps-action").slideUp();
+      $(this).find(".steps-action").slideToggle();
+    })
+
+    $(function(){
+      var $searchlink = $('#searchtoggl i');
+      var $searchbar  = $('#searchbar');
+      
+      $('.mobile-search-wrap .fa-search').on('click', function(e){
+        e.preventDefault();
+        
+        if($(this).attr('id') == 'searchtoggl') {
+          if(!$searchbar.is(":visible")) { 
+            // if invisible we switch the icon to appear collapsable
+            $searchlink.removeClass('fa-search').addClass('fa-search-minus');
+          } else {
+            // if visible we switch the icon to appear as a toggle
+            $searchlink.removeClass('fa-search-minus').addClass('fa-search');
+          }
+          
+          $searchbar.slideToggle(300, function(){
+            // callback after search bar animation
+          });
+        }
+      });
+      
+      $('#searchform').submit(function(e){
+        e.preventDefault(); // stop form submission
+      });
+    });
+  });
+}
+
