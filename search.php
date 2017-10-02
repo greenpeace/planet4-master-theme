c6aa4194bf4c73d7dcae78c5936e8f9bf1194ad1
Angelos Theodorakopoulos <angtheod@gmail.com>
2017-10-02 17:02:23 +0300

    Added Search functionality for the navigation bar. Users can now search using the SearchWP plugin. Added home url link when user clicks on Greenpeace logo. Fixed escape issue with Posts content and title and Excerpt's content. Registered strings for translation. Coding standards fixes. Updated to 0.2.8.

--- search.php
+++ search.php
@@ -12,7 +12,7 @@
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 $context = Timber::get_context();
 
-$context['title'] = 'Search results for '. get_search_query();
+$context['title'] = 'Search results for ' . get_search_query();
 $context['posts'] = Timber::get_posts();
 
 Timber::render( $templates, $context );
