ad94e5450583bbb413696d269ee7cbfa0729e1c3
Kiriakos Diamantis <replicator31@hotmail.com>
2017-11-06 15:36:43 +0200

    Fixed indendation, used short syntax for arrays declaration. Removed content paragraph splitting.

--- functions.php
+++ functions.php
@@ -385,50 +385,49 @@ class P4_Master_Site extends TimberSite {
 			)
 		);
 
-		$p4_post = new_cmb2_box( array(
+		$p4_post = new_cmb2_box( [
 			'id'           => $prefix . 'metabox_post',
 			'title'        => __( 'Post Articles Element Fields', 'planet4-master-theme' ),
-			'object_types' => array( 'post' ),
-		) );
+			'object_types' => [ 'post' ],
+		] );
 
-		$p4_post->add_field( array(
+		$p4_post->add_field( [
 			'name' => __( 'Articles Title', 'planet4-master-theme' ),
 			'desc' => __( 'Title for articles block', 'planet4-master-theme' ),
 			'id'   => $prefix . 'articles_title',
 			'type' => 'text_medium',
-		) );
+		] );
 
-		$p4_post->add_field( array(
-				'name'       => __( 'Articles Count', 'planet4-master-theme' ),
-				'desc'       => __( 'Number of articles that should be displayed for articles block', 'planet4-master-theme' ),
-				'id'         => $prefix . 'articles_count',
-				'type'       => 'text_medium',
-				'attributes' => array(
-					'type' => 'number',
-				),
-			)
-		);
+		$p4_post->add_field( [
+			'name'       => __( 'Articles Count', 'planet4-master-theme' ),
+			'desc'       => __( 'Number of articles that should be displayed for articles block', 'planet4-master-theme' ),
+			'id'         => $prefix . 'articles_count',
+			'type'       => 'text_medium',
+			'attributes' => [
+				'type' => 'number',
+			],
+		] );
 
-		$p4_post->add_field( array(
+		$p4_post->add_field( [
 			'name' => __( 'Author Override', 'planet4-master-theme' ),
 			'desc' => __( 'Enter author name if you want to override the author', 'planet4-master-theme' ),
 			'id'   => $prefix . 'author_override',
 			'type' => 'text_medium',
-		) );
+		] );
 
-		$p4_post->add_field( array(
+		$p4_post->add_field( [
 			'name'         => __( 'Background Image Override', 'planet4-master-theme' ),
 			'desc'         => __( 'Upload an image or select one from the media library to override the background image', 'planet4-master-theme' ),
 			'id'           => $prefix . 'background_image_override',
 			'type'         => 'file',
-			'options'      => array(
+			'options'      => [
 				'url' => false,
-			),
-			'text'         => array(
+			],
+			'text'         => [
 				'add_upload_file_text' => __( 'Add Image', 'planet4-master-theme' ),
-			),
+			],
 			'preview_size' => 'large',
-		) );
+		] );
 	}
 
 	/**
--- single.php
+++ single.php
@@ -9,7 +9,14 @@
  * @since    Timber 0.1
  */
 
-// Add custom css class for body element
+use Timber\Timber;
+
+/**
+ * Add custom css class for body element hook.
+ *
+ * @param array $classes  Array of css classes passed by the hook.
+ * @return array
+ */
 function add_body_classes_for_post( $classes ) {
 	$classes[] = 'post_body';
 	return $classes;
@@ -25,44 +32,35 @@ $context['post']             = $post;
 
 // Get the cmb2 custom fields data
 // Articles block parameters to populate the articles block
-// Author override parameter. If this is set then the author profile section will not be displayed
+// Author override parameter. If this is set then the author profile section will not be displayed.
 $page_meta_data              = get_post_meta( $post->ID );
 $articles_title              = $page_meta_data['p4_articles_title'][0];
 $articles_count              = intval( $page_meta_data['p4_articles_count'][0] );
-$articles_count              = $articles_count == 0 ? 3 : $articles_count;
+$articles_count              = 0 === $articles_count ? 3 : $articles_count;
 $context['author_override']  = $page_meta_data['p4_author_override'][0];
 $context['background_image'] = $page_meta_data['p4_background_image_override'][0];
 
 
-// Build the shortcode for articles block
+// Build the shortcode for articles block.
 if ( ! empty( $articles_title ) ) {
 	$post->articles = "[shortcake_articles article_heading='$articles_title' article_count='$articles_count' /]";
 }
 
-
-// Breaking the content to retrieve first 2 paragraphs
-$parts = preg_split("/(\r\n|\n|\r)/", $post->post_content, 3);
-if ( count( $parts ) == 3 ) {
-	$post->first_paragraph  = $parts[0];
-	$post->second_paragraph = $parts[1];
-	$post->post_content     = $parts[2];
-}
-
-
-// Build an arguments array to customize wordpress comment form
-$comments_args = array(
-
+// Build an arguments array to customize WordPress comment form.
+$comments_args = [
 	'comment_notes_before' => '',
 	'comment_notes_after'  => '',
 	'comment_field'        => Timber::compile( 'comment_form/comment_field.twig' ),
 	'submit_button'        => Timber::compile( 'comment_form/submit_button.twig' ),
 	'title_reply'          => __( 'Leave Your Reply', 'planet4-master-theme' ),
-	'fields'               => apply_filters( 'comment_form_default_fields', array(
+	'fields'               => apply_filters( 'comment_form_default_fields',
+		[
 			'author' => Timber::compile( 'comment_form/author_field.twig' ),
 			'email'  => Timber::compile( 'comment_form/email_field.twig' ),
-		)
-	)
-);
+		]
+	),
+];
+
 $context['comments_args'] = $comments_args;
 
 
