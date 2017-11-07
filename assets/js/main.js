b288ad8ebd6d88fcbe4e1a352d76071409bf918a
Kiriakos Diamantis <replicator31@hotmail.com>
2017-11-07 16:56:13 +0200

    Added load more click event for content four column load more button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,71 @@
 $ = jQuery;
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 130){
-        $('.fixed-element, .md-navigation').addClass("sticky");
-    }
-    else{
-        $('.fixed-element, .md-navigation').removeClass("sticky");
-    }
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 130) {
+		$('.fixed-element, .md-navigation').addClass("sticky");
+	}
+	else {
+		$('.fixed-element, .md-navigation').removeClass("sticky");
+	}
 });
 // Footer JS goes in this
 // Header JS goes in this.
 // Returns width of browser viewport
-if($( window ).width() >= 768) {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 130){
-            $('.fixed-element, .md-navigation').addClass("sticky");
-        }
-        else{
-            $('.fixed-element, .md-navigation').removeClass("sticky");
-        }
-    });
-    $(document).ready(function() {
-        $(".steps-action").hide();
-        $(".step-info-wrap").click(function(){
-            $('.step-info-wrap').parent('.col-md-3').removeClass('active');
-            $(this).parent().addClass('active');
+if ($(window).width() >= 768) {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 130) {
+			$('.fixed-element, .md-navigation').addClass("sticky");
+		}
+		else {
+			$('.fixed-element, .md-navigation').removeClass("sticky");
+		}
+	});
+	$(document).ready(function () {
+		$(".steps-action").hide();
+		$(".step-info-wrap").click(function () {
+			$('.step-info-wrap').parent('.col-md-3').removeClass('active');
+			$(this).parent().addClass('active');
 
-            $(".steps-action").slideUp();
-            $(this).find(".steps-action").slideToggle();
-        });
+			$(".steps-action").slideUp();
+			$(this).find(".steps-action").slideToggle();
+		});
 
-        $(function(){
-            var $searchlink = $('#searchtoggl i');
-            var $searchbar  = $('#searchbar');
+		$(function () {
+			var $searchlink = $('#searchtoggl i');
+			var $searchbar = $('#searchbar');
 
-            $('.mobile-search-wrap .fa-search').on('click', function(e){
-                e.preventDefault();
+			$('.mobile-search-wrap .fa-search').on('click', function (e) {
+				e.preventDefault();
 
-                if($(this).attr('id') === 'searchtoggl') {
-                    if(!$searchbar.is(":visible")) {
-                        // if invisible we switch the icon to appear collapsable
-                        $searchlink.removeClass('fa-search').addClass('fa-search-minus');
-                    } else {
-                        // if visible we switch the icon to appear as a toggle
-                        $searchlink.removeClass('fa-search-minus').addClass('fa-search');
-                    }
+				if ($(this).attr('id') === 'searchtoggl') {
+					if (!$searchbar.is(":visible")) {
+						// if invisible we switch the icon to appear collapsable
+						$searchlink.removeClass('fa-search').addClass('fa-search-minus');
+					} else {
+						// if visible we switch the icon to appear as a toggle
+						$searchlink.removeClass('fa-search-minus').addClass('fa-search');
+					}
 
-                    $searchbar.slideToggle(300, function(){
-                        // callback after search bar animation
-                    });
-                }
-            });
+					$searchbar.slideToggle(300, function () {
+						// callback after search bar animation
+					});
+				}
+			});
 
-            $('#searchform').submit(function(e){
-                e.preventDefault(); // stop form submission
-            });
-        });
-    });
+			$('#searchform').submit(function (e) {
+				e.preventDefault(); // stop form submission
+			});
+		});
+	});
 }
+
+$(document).ready(function () {
+	// Add click event for load more button in content four column block
+	$(".content-four-column-load-btn").off("click").on("click", function () {
+		var content_four_column_row = '.c4-row-hidden';
+		$(content_four_column_row).first().css('display', '').removeClass('c4-row-hidden');
+		if ($(content_four_column_row).size() === 0) {
+			$('.load-more-button-div').css('display', 'none');
+		}
+	});
+});
