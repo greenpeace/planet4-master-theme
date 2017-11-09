dce79cc04a83c5be0325b0b3731df53468c1f581
Angelos Theodorakopoulos <angtheod@gmail.com>
2017-11-09 11:51:12 +0200

    Add footer menu items.

--- tag.php
+++ tag.php
@@ -34,36 +34,43 @@ if ( is_tag() ) {
 	$context['tag_name']        = single_tag_title( '', false );
 	$context['tag_description'] = $context['tag']->description;
 	$context['tag_image']       = get_term_meta( $context['tag']->term_id, 'tag_attachment', true );
+
+	// Footer Items.
+	$context['footer_social_menu']    = wp_get_nav_menu_items( 'Footer Social' );
+	$context['footer_primary_menu']   = wp_get_nav_menu_items( 'Footer Primary' );
+	$context['footer_secondary_menu'] = wp_get_nav_menu_items( 'Footer Secondary' );
+	$context['copyright_text']        = get_option( 'copyright', '' );
+
+
+	$campaign = new P4_Taxonomy_Campaign( $templates, $context );
+
+	$campaign->add_block( Covers::BLOCK_NAME, [
+		'title'       => __( 'Things you can do', 'planet4-master-theme' ),
+		'description' => __( 'We want you to take action because together we\'re strong.', 'planet4-master-theme' ),
+		'select_tag'  => $context['tag']->term_id,
+	] );
+
+	$campaign->add_block( Articles::BLOCK_NAME, [
+		'article_heading' => __( 'In the news', 'planet4-master-theme' ),
+		'article_count'   => 3,
+	] );
+
+	$campaign->add_block( ContentFourColumn::BLOCK_NAME, [
+		'select_tag' => $context['tag']->term_id,
+	] );
+
+	$campaign->add_block( CampaignThumbnail::BLOCK_NAME, [
+		'title'       => __( 'Related Campaigns', 'planet4-master-theme' ),
+		'category_id' => $category->term_id ?? __( 'This Campaign is not assigned to an Issue', 'planet4-master-theme' ),
+	] );
+
+	$campaign->add_block( HappyPoint::BLOCK_NAME, [
+		'background'       => get_term_meta( $context['tag']->term_id, 'happypoint_attachment_id', true ),
+		'boxout_title'     => __( 'Get action alerts in your inbox', 'planet4-master-theme' ),
+		'boxout_descr'     => __( 'Some text here about the transparency of the communications. Opt out or contact us at any time.', 'planet4-master-theme' ),
+		'boxout_link_text' => __( 'Subscribe', 'planet4-master-theme' ),
+		'boxout_link_url'  => '#',
+	] );
+
+	$campaign->view();
 }
-
-$campaign = new P4_Taxonomy_Campaign( $templates, $context );
-
-$campaign->add_block( Covers::BLOCK_NAME, [
-	'title'       => __( 'Things you can do', 'planet4-master-theme' ),
-	'description' => __( 'We want you to take action because together we\'re strong.', 'planet4-master-theme' ),
-	'select_tag'  => $context['tag']->term_id,
-] );
-
-$campaign->add_block( Articles::BLOCK_NAME, [
-	'article_heading' => __( 'In the news', 'planet4-master-theme' ),
-	'article_count'   => 3,
-] );
-
-$campaign->add_block( ContentFourColumn::BLOCK_NAME, [
-	'select_tag' => $context['tag']->term_id,
-] );
-
-$campaign->add_block( CampaignThumbnail::BLOCK_NAME, [
-	'title'       => __( 'Related Campaigns', 'planet4-master-theme' ),
-	'category_id' => $category->term_id ?? __( 'This Campaign is not assigned to an Issue', 'planet4-master-theme' ),
-] );
-
-$campaign->add_block( HappyPoint::BLOCK_NAME, [
-	'background'       => get_term_meta( $context['tag']->term_id, 'happypoint_attachment_id', true ),
-	'boxout_title'     => __( 'Get action alerts in your inbox', 'planet4-master-theme' ),
-	'boxout_descr'     => __( 'Some text here about the transparency of the communications. Opt out or contact us at any time.', 'planet4-master-theme' ),
-	'boxout_link_text' => __( 'Subscribe', 'planet4-master-theme' ),
-	'boxout_link_url'  => '#',
-] );
-
-$campaign->view();
