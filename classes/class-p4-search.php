329c2c862c4b1cf7fbf492b2d57d1595c559a977
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-11-17 20:12:04 +0530

    PLANET-1440 Added GTM code for search page

--- classes/class-p4-search.php
+++ classes/class-p4-search.php
@@ -79,6 +79,7 @@ if ( ! class_exists( 'P4_Search' ) ) {
 			$context['footer_secondary_menu'] = wp_get_nav_menu_items( 'Footer Secondary' );
 			$context['copyright_text']        = get_option( 'copyright', '' );
 			$context['page_category']         = $category->name ?? __( 'Search page', 'planet4-master-theme' );
+			$context['google_tag_value']      = get_option( 'google_tag_manager_identifier', '' ) ?? '';
 		}
 
 		/**
