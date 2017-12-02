8e1ddc8574a67caed8d4aa2986632bb389745325
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2017-12-02 12:25:13 +0530

    PLANET-1544 Minor fix

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
 
 $context                          = Timber::get_context();
 $context['posts']                 = Timber::get_posts();
-$context['google_tag_value']      = planet4_get_option( 'google_tag_manager_identifier', '' ) ?? '';
+$context['google_tag_value']      = planet4_get_option( 'google_tag_manager_identifier' ) ?? '';
 
 // Footer Items.
 $context['footer_social_menu']    = wp_get_nav_menu_items( 'Footer Social' );
 $context['footer_primary_menu']   = wp_get_nav_menu_items( 'Footer Primary' );
 $context['footer_secondary_menu'] = wp_get_nav_menu_items( 'Footer Secondary' );
-$context['copyright_text']        = planet4_get_option( 'copyright', '' ) ?? '';
+$context['copyright_text']        = planet4_get_option( 'copyright' ) ?? '';
 
 $templates = array( 'index.twig' );
 if ( is_home() ) {
