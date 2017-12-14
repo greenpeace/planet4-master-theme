1a51769803cc0c3b393bdfea6a405ffa2f29a5bc
Fränk Klein <frank@humanmade.co.uk>
2017-12-14 14:09:07 +0100

    Whitespace fixes.

--- functions.php
+++ functions.php
@@ -3,21 +3,16 @@
 if ( ! class_exists( 'Timber' ) ) {
 	add_action(
 		'admin_notices', function () {
-		    printf( '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="%s">Plugins menu</a></p></div>',
-			    esc_url( admin_url( 'plugins.php#timber' ) )
-		    );
-	    }
+			printf( '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="%s">Plugins menu</a></p></div>',
+				esc_url( admin_url( 'plugins.php#timber' ) )
+			);
+		}
 	);
 
 	add_filter(
-	/**
-	 * @param $template
-	 *
-	 * @return string
-	 */
 		'template_include', function ( $template ) {
-		    return get_stylesheet_directory() . '/static/no-timber.html';
-	    }
+			return get_stylesheet_directory() . '/static/no-timber.html';
+		}
 	);
 
 	return;
