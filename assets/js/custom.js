247a81b64b6a085667acd9698a86331be3d98ea7
Angelos Theodorakopoulos <angtheod@gmail.com>
2017-12-18 12:04:44 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -153,34 +153,28 @@ if($( window ).width() <= 768) {
   });
 };
 $(function() {
-	$('.publications-slider').slick({
-		infinite: false,
-		mobileFirst: true,
-		slidesToShow: 2.2,
-		slidesToScroll: 1,
-		arrows: false,
-		dots: false,
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: "unslick"
-			},
-			{
-				breakpoint: 768,
-				settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-			},
-			{
-				breakpoint: 576,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
+  $('.publications-slider').slick({
+	  infinite:       false,
+	  mobileFirst:    true,
+	  slidesToShow:   2.2,
+	  slidesToScroll: 1,
+	  arrows:         false,
+	  dots:           false,
+	  responsive: [
+		  {
+			  breakpoint: 992,
+			  settings: { slidesToShow: 4 }
+		  },
+		  {
+			  breakpoint: 768,
+			  settings: { slidesToShow: 3 }
+		  },
+		  {
+			  breakpoint: 576,
+			  settings: { slidesToShow: 2 }
+		  }
+	  ]
+  });
 });
 $(function() {
   $('#search-type button').click(function() {
