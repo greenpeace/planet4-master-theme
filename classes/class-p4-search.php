d8794bf91afe0ae5f67fa0c3dd3713eba0e87de0
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-02-12 13:09:25 +0530

    PLANET-1751 Added search filter params in Ajax call

--- classes/class-p4-search.php
+++ classes/class-p4-search.php
@@ -139,6 +139,29 @@ if ( ! class_exists( 'P4_Search' ) ) {
 					$subgroup           = $search_async->search_query ? $search_async->search_query : 'all';
 					$search_async->current_page = $paged;
 
+					parse_str( $query_string, $filters_array );
+					$selected_sort    = $filters_array[ 'orderby' ];
+					$selected_filters = $filters_array[ 'f' ];
+					$filters          = [];
+
+					// Handle submitted filter options.
+					if ( $selected_filters && is_array( $selected_filters ) ) {
+					    foreach ( $selected_filters as $type => $filter_type ) {
+					        foreach ( $filter_type as $name => $id ) {
+					            $filters[ $type ][] = [
+					                'id'   => $id,
+					                'name' => $name,
+					            ];
+					        }
+					    }
+					}
+
+					// Validate user input (sort, filters, etc).
+					if ( $search_async->validate( $selected_sort, $filters, $search_async->context ) ) {
+					    $search_async->selected_sort = $selected_sort;
+					    $search_async->filters       = $filters;
+					}
+
 					// TODO - Set the correct filters so that it will work when searching for specific term with filters applied.
 					// Check Object cache for stored key.
 					$search_async->check_cache( $query_string, "$group:$subgroup" );
