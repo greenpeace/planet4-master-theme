4666dcd670e60e40115631023cda085c48fd37fd
Kiriakos Diamantis <replicator31@hotmail.com>
2018-02-14 09:38:39 +0200

    Fix category slug extraction in planet4_settings js

--- assets/admin/js/planet4_settings.js
+++ assets/admin/js/planet4_settings.js
@@ -12,13 +12,22 @@ jQuery(document).ready(function () {
 		$('select[name^=p4_page_type_]').each(function () {
 			var slug = $(this).attr("name");
 			slug = slug.replace('p4_page_type_', '').replace('_category', '');
-			var category_slug = $.trim($(this).find(":selected").text()).toLowerCase();
-			var p4_page_type_id = p4_page_types.filter(function (e) {
-				return e.slug == slug;
-			});
-			var category_id = categories.filter(function (e) {
-				return e.slug == category_slug;
-			});
+			var category_slug = $.trim($(this).find(':selected').val());
+
+			var p4_page_type_id = [];
+			if (undefined !== p4_page_types) {
+				// Variable p4_page_types is passed from backend.
+				p4_page_type_id = p4_page_types.filter(function (e) {
+					return e.slug == slug;
+				});
+			}
+			var category_id = [];
+			if (undefined !== categories) {
+				// Variable categories is passed from backend.
+				category_id = categories.filter(function (e) {
+					return e.slug == category_slug;
+				});
+			}
 			p4_page_type_id = p4_page_type_id.length === 0 ? '' : p4_page_type_id[0].term_id;
 			category_id = category_id.length === 0 ? '' : category_id[0].term_id;
 			result.push({
