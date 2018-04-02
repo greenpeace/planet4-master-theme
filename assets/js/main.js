190d6509ba47b4585476d544a873c43548c9a859
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-04-02 16:29:40 +0300

    Adjust slickify to work well with the latest changes of the Content Four Column block.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,5 @@
 $ = jQuery; //eslint-disable-line no-global-assign
 
-// convert an element to slider using slick js
-function slickify(element) {
-  $(element).slick({
-    infinite:       false,
-    mobileFirst:    true,
-    slidesToShow:   2.2,
-    slidesToScroll: 1,
-    arrows:         false,
-    dots:           false,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: { slidesToShow: 4 }
-      },
-      {
-        breakpoint: 768,
-        settings: { slidesToShow: 3 }
-      },
-      {
-        breakpoint: 576,
-        settings: { slidesToShow: 2 }
-      }
-    ]
-  });
-}
-
 $(document).ready(function () {
   // Block: Content Four Column functionality.
   // Find out how many posts per row are being displayed.
