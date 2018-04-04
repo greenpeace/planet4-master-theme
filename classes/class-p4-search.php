d9bc03d504f276ffa4bee1903070a46b68df3071
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-04-04 10:07:16 +0300

    Changed functionality so that Left mouse click on search button will continue to perform search via SearchWP but Right mouse click will perform search via Elasticsearch.

--- classes/class-p4-search.php
+++ classes/class-p4-search.php
@@ -422,9 +422,6 @@ if ( ! class_exists( 'P4_Search' ) ) {
 				$args['s']            = $this->search_query;
 				$args['ep_integrate'] = true;
 
-//				echo '<pre>';
-//				print_r($args);
-//				echo '</pre>';
 				$posts = ( new WP_Query( $args ) )->posts;
 			}
 
@@ -805,7 +802,7 @@ if ( ! class_exists( 'P4_Search' ) ) {
 		 */
 		public function enqueue_public_assets() {
 			if ( is_search() ) {
-				wp_register_script( 'search', get_template_directory_uri() . '/assets/js/search.js', [ 'jquery' ], '0.1.9', true );
+				wp_register_script( 'search', get_template_directory_uri() . '/assets/js/search.js', [ 'jquery' ], '0.2.0', true );
 				wp_localize_script( 'search', 'localizations', $this->localizations );
 				wp_enqueue_script( 'search' );
 			}
