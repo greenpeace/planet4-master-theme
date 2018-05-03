eca71b01885b3390c1353281f82704fc95b95a79
Nikos Roussos <nikos@roussos.cc>
2018-05-03 16:55:39 +0300

    PLANET-2091 - Refactor and simplify action card scroll code

--- assets/js/partials/post_action.js
+++ assets/js/partials/post_action.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
   'use strict';
 
   var $sidebar = $('.post-content').find('> #action-card');
-  var $window = $(window);
   var offset = $sidebar.offset();
   var topPadding = 100;
 
-  if ($sidebar.length > 0 && $window.width() > 992) {
-    var absPosition = $('.post-details > p:last-child').offset().top - $sidebar.outerHeight() - topPadding;
+  function scroll_action_card() {
+    let absPosition = $('.post-details > p:last-child').offset().top - $sidebar.outerHeight() - topPadding;
 
-    $window.scroll(function () {
-      if ($window.scrollTop() > offset.top &&
-      $window.scrollTop() < absPosition) {
-        $sidebar.stop().animate({
-          marginTop: $window.scrollTop() - offset.top + topPadding
-        });
-      }
-      if ($window.scrollTop() < offset.top) {
-        $sidebar.stop().animate({
-          marginTop: 0
-        });
-      }
-    });
+    if ($(window).scrollTop() > offset.top && $(window).scrollTop() < absPosition) {
+      $sidebar.stop().animate({
+        marginTop: $(window).scrollTop() - offset.top + topPadding
+      });
+    }
+    if ($(window).scrollTop() < offset.top) {
+      $sidebar.stop().animate({
+        marginTop: 0
+      });
+    }
+  }
+
+  if ($sidebar.length > 0 && $(window).width() > 992) {
+    window.addEventListener('scroll', scroll_action_card);
   }
 });
