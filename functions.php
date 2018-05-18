c7a2199ceefcb4e89efe0e25e4bb850deb546b77
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-05-18 14:04:58 +0300

    Initialise the image credit varible

--- functions.php
+++ functions.php
@@ -895,6 +895,7 @@ class P4_Master_Site extends TimberSite {
 
 		$image_id     = trim( str_replace( 'attachment_', '', $atts['id'] ) );
 		$meta         = get_post_meta( $image_id );
+		$image_credit = '';
 		if ( isset( $meta['_credit_text'] ) && ! empty( $meta['_credit_text'] ) ) {
 			$image_credit = ' ' . $meta['_credit_text'][0];
 			if ( ! is_numeric( strpos( $meta['_credit_text'][0], '©' ) ) ) {
