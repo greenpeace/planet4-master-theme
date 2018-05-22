2863987930c157607848ee6dfe72ff82d38578c4
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 16:00:59 +0200

    Update country_select.js

--- assets/js/partials/country_select.js
+++ assets/js/partials/country_select.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   'use strict';
 
   $('.country-select-dropdown').click(function(){
-    $(this).parent().toggleClass('active-li');
-    $('.country-select-box').toggle();
+    $(this).parent().toggleClass('active-li')
+    $('.country-select-box').toggle()
   });
 
   $('.country-select-box .country-list li').click(function(){
-    $(this).parents('.country-select-box').find('li').removeClass('active');
-    $(this).addClass('active');
+    $(this).parents('.country-select-box').find('li').removeClass('active')
+    $(this).addClass('active')
   });
 
   $('.country-selectbox').click(function(){
-    $(this).toggleClass('active');
-    $(this).parent().find('.option-contry').toggleClass('active');
+    $(this).toggleClass('active')
+    $(this).parent().find('.option-contry').toggleClass('active')
   });
 });
