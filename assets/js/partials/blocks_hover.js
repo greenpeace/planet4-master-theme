27f1170e6b48614cc42063a845e844410ed71b7c
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 15:54:36 +0200

    Update blocks_hover.js

--- assets/js/partials/blocks_hover.js
+++ assets/js/partials/blocks_hover.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
 
   $('.article-list-item-image').hover(
     function() {
-      $('.article-list-item-headline', $(this).parent()).addClass('article-hover');
+      $('.article-list-item-headline', $(this).parent()).addClass('article-hover')
     }, function() {
-      $('.article-list-item-headline', $(this).parent()).removeClass('article-hover');
+      $('.article-list-item-headline', $(this).parent()).removeClass('article-hover')
     }
-  );
+  )
 
   $('.four-column-content-symbol').hover(
     function() {
-      $('h4', $(this).parent()).addClass('four-column-hover');
+      $('h4', $(this).parent()).addClass('four-column-hover')
     }, function() {
-      $('h4', $(this).parent()).removeClass('four-column-hover');
+      $('h4', $(this).parent()).removeClass('four-column-hover')
     }
-  );
-});
+  )
+})
