ee3eab30d8af50806de7e576c71fa5b5f07a66ca
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 16:05:55 +0200

    Update load_more.js

--- assets/js/partials/load_more.js
+++ assets/js/partials/load_more.js
@@ -2,51 +2,51 @@ $(document).ready(function () {
   // Block: Content Four Column functionality.
   // Find out how many posts per row are being displayed.
   $('.four-column-content').each( function() {
-    var visible_posts = $('.post-column:visible', $(this)).length;
+    var visible_posts = $('.post-column:visible', $(this)).length
 
     if (0 === visible_posts % 4) {
-      $(this).attr('data-posts_per_row', 4);
+      $(this).attr('data-posts_per_row', 4)
     } else if (0 === visible_posts % 3) {
-      $(this).attr('data-posts_per_row', 3);
+      $(this).attr('data-posts_per_row', 3)
     }
-  });
+  })
 
   // Add click event for load more button in Covers blocks.
   $('.btn-load-more-posts-click').off('click').on('click', function () {
-    var $row = $('.post-column:hidden', $(this).closest('.container'));
-    var posts_per_row = $(this).closest('.four-column-content').data('posts_per_row');
+    var $row = $('.post-column:hidden', $(this).closest('.container'))
+      , posts_per_row = $(this).closest('.four-column-content').data('posts_per_row')
 
     $(this).blur();
     if ($row.length > 0) {
-      $row.slice( 0, posts_per_row ).show('slow');
+      $row.slice( 0, posts_per_row ).show('slow')
     }
     if ($row.length <= posts_per_row) {
-      $(this).closest('.load-more-posts-button-div').hide('fast');
+      $(this).closest('.load-more-posts-button-div').hide('fast')
     }
-  });
+  })
 
   // Block: Covers functionality.
   // Find out how many posts per row are being displayed.
   $('.covers-block').each( function() {
-    var visible_covers = $('.cover-card-column:visible', $(this)).length;
+    var visible_covers = $('.cover-card-column:visible', $(this)).length
     if (0 === visible_covers % 3) {
-      $(this).attr('data-covers_per_row', 3);
+      $(this).attr('data-covers_per_row', 3)
     } else if (0 === visible_covers % 2) {
-      $(this).attr('data-covers_per_row', 2);
+      $(this).attr('data-covers_per_row', 2)
     }
-  });
+  })
 
   // Add click event for load more button in Covers blocks.
   $('.btn-load-more-covers-click').off('click').on('click', function () {
-    var $row = $('.cover-card-column:hidden', $(this).closest('.container'));
-    var covers_per_row = $(this).closest('.covers-block').data('covers_per_row');
+    var $row = $('.cover-card-column:hidden', $(this).closest('.container'))
+      , covers_per_row = $(this).closest('.covers-block').data('covers_per_row')
 
-    $(this).blur();
+    $(this).blur()
     if ($row.length > 0) {
-      $row.slice( 0, covers_per_row ).show('slow');
+      $row.slice( 0, covers_per_row ).show('slow')
     }
     if ($row.length <= covers_per_row) {
-      $(this).closest('.load-more-covers-button-div').hide('fast');
+      $(this).closest('.load-more-covers-button-div').hide('fast')
     }
-  });
-});
+  })
+})
