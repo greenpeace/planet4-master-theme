c3e9425f9d1af9e266b3a98cf207f8f84296540e
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 15:53:35 +0200

    Update cookies.js

--- assets/js/partials/cookies.js
+++ assets/js/partials/cookies.js
@@ -2,36 +2,37 @@ $(document).ready(function() {
   'use strict';
 
   function createCookie(name, value) {
-    document.cookie = encodeURI(name) + '=' + encodeURI(value) + ';domain=.' + document.domain + ';path=/;';
+    document.cookie = encodeURI(name) + '=' + encodeURI(value) + ';domain=.' + document.domain + ';path=/;'
   }
 
   function readCookie(name) {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
+    var nameEQ = name + '='
+      , ca = document.cookie.split(';')
+    
     for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
+      var c = ca[i]
       while (c.charAt(0)==' ') {
-        c = c.substring(1,c.length);
+        c = c.substring(1,c.length)
       }
       if (c.indexOf(nameEQ) == 0) {
-        return c.substring(nameEQ.length,c.length);
+        return c.substring(nameEQ.length,c.length)
       }
     }
-    return null;
+    return null
   }
 
   $(function() {
-    var cookie = readCookie('greenpeace');
+    var cookie = readCookie('greenpeace')
     if (cookie == null) {
-      $('.cookie-block').show();
-      const height = $('.cookie-block').height();
-      $('footer').css('margin-bottom', height + 'px');
+      $('.cookie-block').show()
+      const height = $('.cookie-block').height()
+      $('footer').css('margin-bottom', height + 'px')
     }
-  });
+  })
 
   $('#hidecookie').click(function () {
-    $('.cookie-block').slideUp('slow');
-    $('footer').css('margin-bottom', '0');
-    createCookie('greenpeace', 'policy-accepted');
-  });
-});
+    $('.cookie-block').slideUp('slow')
+    $('footer').css('margin-bottom', '0')
+    createCookie('greenpeace', 'policy-accepted')
+  })
+})
