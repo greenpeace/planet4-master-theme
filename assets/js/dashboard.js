bb80674ccf51557ddec8f44fe5979db42a99b540
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 15:41:57 +0200

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -5,16 +5,16 @@ $ = jQuery; //eslint-disable-line no-global-assign
 // Dashboard page.
 $(function() {
   $('.btn-cp-action').off('click').on('click', function (event) {
-    event.preventDefault();
-    var $btn      = $(this);
-    var $response = $( '.cp-subitem-response', $btn.parent() );
-    var confirmation_text = $btn.data( 'confirm' );
-    var answer;
+    event.preventDefault()
+    var $btn      = $(this)
+      , $response = $( '.cp-subitem-response', $btn.parent() )
+      , confirmation_text = $btn.data( 'confirm' )
+      , answer
 
     if ( confirmation_text ) {
-      answer = confirm( confirmation_text );
+      answer = confirm( confirmation_text )
       if ( ! answer ) {
-        return;
+        return
       }
     }
 
@@ -29,13 +29,13 @@ $(function() {
       dataType: 'json'
     }).done(function ( response ) {
       if ( response.message ) {
-        $response.hide().removeClass( 'cp-error cp-success' );
-        $response.text( response.message );
+        $response.hide().removeClass( 'cp-error cp-success' )
+        $response.text( response.message )
         if ( response.class ) {
-          $response.addClass( response.class );
+          $response.addClass( response.class )
         }
-        $response.show( 'slow' );
+        $response.show( 'slow' )
       }
-    });
-  });
-});
+    })
+  })
+})
