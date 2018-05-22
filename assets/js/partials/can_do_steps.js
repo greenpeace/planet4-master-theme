86c7d7f0ddab673587b2c4f6ab74e0955b9d4ec2
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 15:55:38 +0200

    Update can_do_steps.js

--- assets/js/partials/can_do_steps.js
+++ assets/js/partials/can_do_steps.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   'use strict';
 
   $('.can-do-steps .col').hover(function() {
-    const step = $(this).data('id');
-    $('#step-' + step).toggleClass('active');
-  });
-});
+    const step = $(this).data('id')
+    $('#step-' + step).toggleClass('active')
+  })
+})
