a0b2b415153cdca45a78305ae97f3f95648e8c80
Filippo Oretti <45kb@users.noreply.github.com>
2018-05-22 16:02:24 +0200

    Update happy_point.js

--- assets/js/partials/happy_point.js
+++ assets/js/partials/happy_point.js
@@ -5,25 +5,25 @@ $(document).ready(function() {
 
   function load_happy_point() {
     if ($('#happy-point > iframe').length > 0) {
-      window.removeEventListener('load', load_happy_point);
-      window.removeEventListener('resize', load_happy_point);
-      window.removeEventListener('scroll', load_happy_point);
-      return;
+      window.removeEventListener('load', load_happy_point)
+      window.removeEventListener('resize', load_happy_point)
+      window.removeEventListener('scroll', load_happy_point)
+      return
     }
 
-    const happy_pos = $('#happy-point')[0].getBoundingClientRect();
+    const happy_pos = $('#happy-point')[0].getBoundingClientRect()
 
     if (happy_pos.top < window.innerHeight) {
       $('#happy-point').append($('<iframe></iframe>')
-        .attr({src: url, cellSpacing: '0', allowtransparency: 'true', frameborder: '0', scrolling: 'no', width: '100%'}));
+        .attr({src: url, cellSpacing: '0', allowtransparency: 'true', frameborder: '0', scrolling: 'no', width: '100%'}))
     }
   }
 
-  const url = $('#happy-point').data('src');
+  const url = $('#happy-point').data('src')
 
   if (url) {
-    window.addEventListener('load', load_happy_point);
-    window.addEventListener('resize', load_happy_point);
-    window.addEventListener('scroll', load_happy_point);
+    window.addEventListener('load', load_happy_point)
+    window.addEventListener('resize', load_happy_point)
+    window.addEventListener('scroll', load_happy_point)
   }
-});
+})
