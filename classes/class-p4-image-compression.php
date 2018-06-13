2de7b2781fa3801149b3e31d2b05c0e51dbb48a0
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-06-13 12:42:17 +0300

    Add image sharpening (via unsharpMask).

--- classes/class-p4-image-compression.php
+++ classes/class-p4-image-compression.php
@@ -26,7 +26,24 @@ if ( ! class_exists( 'P4_Image_Compression' ) ) {
 				$filter_name = $this->filter;
 			}
 			parent::thumbnail_image( $dst_w, $dst_h, $filter_name, $strip_meta );
+
+			// The order of methods applied is: Resize -> Sharpen -> Compress.
 			try {
+				// Sharpen image after it has been resized.
+				if ( 'image/jpeg' === $this->mime_type ) {
+					if ( is_callable( array( $this->image, 'unsharpMaskImage' ) ) ) {
+						$this->image->unsharpMaskImage( 1, 0.45, 3, 0 );
+					}
+//					if ( is_callable( array( $this->image, 'adaptiveSharpenImage' ) ) ) {
+//						$this->image->adaptiveSharpenImage( 5, 1.5 );
+//					}
+				}
+			} catch ( Exception $e ) {
+				return new WP_Error( 'image_sharpening_error', $e->getMessage() );
+			}
+
+			try {
+				// Compress image after it has been sharpened.
 				if ( is_callable( [ $this->image, 'setInterlaceScheme' ] ) && defined( 'Imagick::INTERLACE_PLANE' ) ) {
 					$this->image->setInterlaceScheme( Imagick::INTERLACE_PLANE );
 				}
