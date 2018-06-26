38c5be25c51f273ea5fefe1f0340e7a43dc95bc0
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-06-26 14:51:59 +0530

    PLANET-2245 Add alt text while selecting image from GPML

--- classes/controller/tab/class-gpi-media-library-controller.php
+++ classes/controller/tab/class-gpi-media-library-controller.php
@@ -129,7 +129,7 @@ if ( ! class_exists( 'GPI_Media_Library_Controller' ) ) {
 				'show_scroll_times' => self::SHOW_SCROLL_TIMES,
 			];
 			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin.css', array(), '0.3' );
-			wp_register_script( 'p4ml_admin_script', P4ML_ADMIN_DIR . 'js/adminml.js', array(), '0.4', true );
+			wp_register_script( 'p4ml_admin_script', P4ML_ADMIN_DIR . 'js/adminml.js', array(), '0.5', true );
 			wp_localize_script( 'p4ml_admin_script', 'media_library_params', $params );
 			wp_enqueue_script( 'jquery-ui-core' );
 			wp_enqueue_script( 'jquery-ui-selectable' );
