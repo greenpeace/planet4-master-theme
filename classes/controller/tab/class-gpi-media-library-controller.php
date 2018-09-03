d51845e3a8a722f52be24147be0614a8cde7c5cb
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-09-03 19:44:41 +0530

    PLANET-2304 - GPML UI changes - work in progress

--- classes/controller/tab/class-gpi-media-library-controller.php
+++ classes/controller/tab/class-gpi-media-library-controller.php
@@ -190,7 +190,7 @@ if ( ! class_exists( 'GPI_Media_Library_Controller' ) ) {
 				'show_scroll_times' => self::SHOW_SCROLL_TIMES,
 			];
 
-			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin.css', [ 'media-views', 'media' ], '0.7' );
+			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin.css', [ 'media-views', 'media' ], '0.8' );
 			wp_register_script( 'p4ml_admin_script', P4ML_ADMIN_DIR . 'js/adminml.js', [], '0.10', true );
 			wp_localize_script( 'p4ml_admin_script', 'media_library_params', $params );
 			wp_enqueue_script( 'jquery-ui-core' );
