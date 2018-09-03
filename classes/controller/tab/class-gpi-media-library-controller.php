b1bf0e1dfe9139bbe0b86c55c0fc022d3010324e
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-09-03 21:03:53 +0530

    PLANET-2304 GPML UI fixes, increase per page media limit to 20

--- classes/controller/tab/class-gpi-media-library-controller.php
+++ classes/controller/tab/class-gpi-media-library-controller.php
@@ -190,7 +190,7 @@ if ( ! class_exists( 'GPI_Media_Library_Controller' ) ) {
 				'show_scroll_times' => self::SHOW_SCROLL_TIMES,
 			];
 
-			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin.css', [ 'media-views', 'media' ], '0.8' );
+			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin.css', [ 'media-views', 'media' ], '0.9' );
 			wp_register_script( 'p4ml_admin_script', P4ML_ADMIN_DIR . 'js/adminml.js', [], '0.10', true );
 			wp_localize_script( 'p4ml_admin_script', 'media_library_params', $params );
 			wp_enqueue_script( 'jquery-ui-core' );
@@ -210,7 +210,7 @@ if ( ! class_exists( 'GPI_Media_Library_Controller' ) ) {
 				'show_scroll_times' => self::SHOW_SCROLL_TIMES,
 			];
 
-			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin_search_ml.css', [], '0.3' );
+			wp_enqueue_style( 'p4ml_admin_style', P4ML_ADMIN_DIR . 'css/admin_search_ml.css', [], '0.4' );
 			wp_register_script( 'p4ml_admin_script', P4ML_ADMIN_DIR . 'js/admin_search_ml.js', [], '0.4', true );
 			wp_localize_script( 'p4ml_admin_script', 'media_library_params', $params );
 			wp_enqueue_script( 'p4ml_admin_script' );
