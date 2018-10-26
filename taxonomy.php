542fc4cee206556778bd3923beb37bba731e9edc
Pablo Cubico <pablocuadrado@gmail.com>
2018-10-26 08:31:55 -0300

    Simplistic approach to loading more posts

--- taxonomy.php
+++ taxonomy.php
@@ -7,10 +7,37 @@
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  */
 
-$templates = array( 'page_type.twig', 'index.twig' );
-
 $context = Timber::get_context();
-$context['page_type'] = get_queried_object();
-$context['posts'] = Timber::get_posts();
+$page_type = get_queried_object();
+$context['page_type'] = $page_type;
 
-Timber::render( $templates, $context );
+$post_args = [ 
+    'posts_per_page' => 1, 
+    'tax_query' => [
+		[
+			'taxonomy' => 'p4-page-type',
+			'field'    => 'slug',
+			'terms'    => 'press',
+        ],
+    ],
+    'post_type' => 'post',
+    'paged' => 1
+];
+
+if ( get_query_var('page') ) {
+    $templates = [ 'tease-page-type.twig' ];
+    $page = get_query_var('page');
+    $post_args['paged'] = $page;
+
+    $posts = new Timber\PostQuery( $post_args );
+
+    foreach($posts as $post) {
+        $context['post'] = $post;
+        Timber::render( $templates, $context );
+    }
+} else {
+    $templates = [ 'page_type.twig' ];
+    $context['posts'] = new Timber\PostQuery( $post_args );
+    
+    Timber::render( $templates, $context );
+}
