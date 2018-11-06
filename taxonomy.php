7f66da0507c15f3fa35300432ec314c60173793c
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-11-06 12:12:19 +0530

    PLANET-2820 Fix author override issue on new page type pages

--- taxonomy.php
+++ taxonomy.php
@@ -7,10 +7,22 @@
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  */
 
-$templates = array( 'page_type.twig', 'index.twig' );
+$templates = [ 'page_type.twig', 'index.twig' ];
 
-$context = Timber::get_context();
+$context              = Timber::get_context();
 $context['page_type'] = get_queried_object();
-$context['posts'] = Timber::get_posts();
+$pagetype_posts       = Timber::get_posts();
+
+if ( $pagetype_posts ) {
+	foreach ( $pagetype_posts as $pagetype_post ) {
+
+		$author_override                = get_post_meta( $pagetype_post->ID, 'p4_author_override', true );
+		$pagetype_post->author          = '' === $author_override ? get_the_author_meta( 'display_name', $pagetype_post->post_author ) : $author_override;
+		$pagetype_post->author_url      = '' === $author_override ? get_author_posts_url( $pagetype_post->post_author ) : '#';
+		$pagetype_post->author_override = $author_override;
+	}
+}
+
+$context['posts'] = $pagetype_posts;
 
 Timber::render( $templates, $context );
