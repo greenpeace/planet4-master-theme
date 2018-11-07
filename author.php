62ce769fd52f915b1bdfbc426986284dd3d4e12c
Pablo Cubico <pablocuadrado@gmail.com>
2018-11-07 08:15:38 -0300

    Fixing PHPCS Linter errors.

--- author.php
+++ author.php
@@ -8,36 +8,39 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
+
 wp_register_script( 'author', get_template_directory_uri() . '/assets/js/author.js', [ 'jquery' ], '0.0.1', true );
 wp_enqueue_script( 'author' );
 
-$context = Timber::get_context();
+$context          = Timber::get_context();
 $context['posts'] = Timber::get_posts( false, 'P4_Post' );
 
 $post_args = [
-    'posts_per_page' => 10,
-    'post_type' => 'post',
-    'paged' => 1
+	'posts_per_page' => 10,
+	'post_type'      => 'post',
+	'paged'          => 1,
 ];
 
 if ( isset( $wp_query->query_vars['author'] ) ) {
-	$author = new TimberUser( $wp_query->query_vars['author'] );
-	$context['author'] = $author;
-    $context['title'] = 'Author Archives: ' . $author->name();
-    $post_args['author'] = $wp_query->query_vars['author'];
+	$author              = new TimberUser( $wp_query->query_vars['author'] );
+	$context['author']   = $author;
+	$context['title']    = 'Author Archives: ' . $author->name();
+	$post_args['author'] = $wp_query->query_vars['author'];
 }
 
-if ( get_query_var('page') ) {
-    $templates = [ 'tease-author.twig' ];
-    $page = get_query_var('page');
-    $post_args['paged'] = $page;
-    $posts = new Timber\PostQuery( $post_args );
-	foreach($posts as $post) {
-        $context['post'] = $post;
-        Timber::render( $templates, $context );
-    }
+if ( get_query_var( 'page' ) ) {
+	$templates          = [ 'tease-author.twig' ];
+	$page               = get_query_var( 'page' );
+	$post_args['paged'] = $page;
+
+	$posts = new Timber\PostQuery( $post_args );
+	foreach ( $posts as $post ) {
+		$context['post'] = $post;
+		Timber::render( $templates, $context );
+	}
 } else {
-    $templates = [ 'author.twig', 'archive.twig' ];
-    $context['posts'] = new Timber\PostQuery( $post_args );
-    Timber::render( $templates, $context );
+	$templates        = [ 'author.twig', 'archive.twig' ];
+	$context['posts'] = new Timber\PostQuery( $post_args );
+
+	Timber::render( $templates, $context );
 }
