a5bc65cd252bbc8ec9105f42ad43094f7e69b2bc
Angelos Theodorakopoulos <angtheod@gmail.com>
2018-11-20 15:17:21 +0200

    Fix classname collision with Load more button of Blocks. Master Theme should use load-more-mt. Small improvements in js. Now can use the data-content attribute to indicate where the response should be appended to. Registered couple strings for translation.

--- assets/js/load_more.js
+++ assets/js/load_more.js
@@ -1,9 +1,10 @@
-const loadMore = $('button.load-more');
-loadMore.on('click', function (e) {
+const load_more = $('button.load-more-mt');
+load_more.off('click').on('click', function (e) {
   e.preventDefault();
 
+  const $content = $( this.dataset.content );
   const nextPage = parseInt(this.dataset.page) + 1;
-  const totalPages = this.dataset.total;
+  const totalPages = parseInt(this.dataset.total);
   const url = this.dataset.url + `?page=${ nextPage }`;
   this.dataset.page = nextPage;
 
@@ -13,12 +14,12 @@ loadMore.on('click', function (e) {
     dataType: 'html'
   }).done(function ( response ) {
     // Append the response at the bottom of the results and then show it.
-    $( '.multiple-search-result .list-unstyled' ).append( response );
+    $content.append( response );
   }).fail(function ( jqXHR, textStatus, errorThrown ) {
     console.log(errorThrown); //eslint-disable-line no-console
   });
 
-  if (nextPage == totalPages) {
-    loadMore.fadeOut();
+  if (nextPage === totalPages) {
+	  load_more.fadeOut();
   }
 });
