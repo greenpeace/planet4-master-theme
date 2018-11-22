f685027a5a7d7dc465dfd7494f2cdb48a3623990
Pablo Cubico <pablocuadrado@gmail.com>
2018-11-22 11:38:41 -0300

    Use tax_query instead of the shorthand method.

--- taxonomy.php
+++ taxonomy.php
@@ -25,7 +25,13 @@ $post_args = [
 	'posts_per_page' => 10,
 	'post_type'      => 'post',
 	'paged'          => 1,
-	'p4-page-type'   => $context['page_type']->slug
+	'tax_query' 	 => [
+		[
+			'taxonomy' => 'p4-page-type',
+			'field' => 'term_id',
+			'terms' => $context['page_type']->term_id
+		]
+	],
 ];
 
 $context['dummy_thumbnail'] = get_template_directory_uri() . '/images/dummy-thumbnail.png';
