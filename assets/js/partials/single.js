f5cacb71435e49d135f7c8960c7b7d34b83ef21f
Pablo Cubico <pablocuadrado@gmail.com>
2018-12-14 08:28:25 -0300

    Add close icon to image zoomer overlay.

--- assets/js/partials/single.js
+++ assets/js/partials/single.js
@@ -3,14 +3,14 @@ var $ = jQuery;
 $(document).on('ready', function() {
   function unzoom() {
     $('.image-zoomer').fadeOut(500, function() {
-      $('.image-zoomer').html('');
+      $('.image-zoomer-content').html('');
     });
   }
 
   function zoomImage(image) {
     var imageClone = $(image).clone();
-    $('.image-zoomer').html('');
-    $(imageClone).appendTo('.image-zoomer');
+    $('.image-zoomer-content').html('');
+    $(imageClone).appendTo('.image-zoomer-content');
     $('.image-zoomer').fadeIn();
     $('.image-zoomer').off('click').on('click', unzoom);
   }
