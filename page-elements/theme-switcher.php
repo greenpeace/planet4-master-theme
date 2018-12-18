fc60d6cc90eabeaffe3a6bb6bf15b312d905142a
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2018-12-18 15:11:34 +0530

    theme already selected of on same campaign theme

--- page-elements/theme-switcher.php
+++ page-elements/theme-switcher.php
@@ -1,6 +1,7 @@
 <div id="theme-switcher">
 	<?php _e( 'Switch theme', 'planet4-master-theme' ); ?>:
 	<?php
+    global $post;
 
 	$campaign_args  = array(
 		'post_type'  => 'page',
@@ -14,8 +15,9 @@
 			<?php
 			while ( $campaing_pages->have_posts() ) {
 				$campaing_pages->the_post();
+				$selected = ! empty( $post->ID ) && get_the_ID() == $post->ID ? 'selected="selected"' : '';
 				?>
-                <option value="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></option><?php
+                <option value="<?php echo get_the_permalink(); ?>" <?php echo $selected; ?>><?php echo get_the_title(); ?></option><?php
 			}
 			?>
         </select>
