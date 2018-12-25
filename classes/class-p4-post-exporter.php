564b998cda716929caf7744e5f9daa8fbffc4ff3
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2018-12-25 12:39:26 +0530

    Exporter for campaigns

---
+++ classes/class-p4-post-exporter.php
@@ -0,0 +1,78 @@
+<?php
+
+class P4_Post_Exporter {
+
+	/*
+		* AutoLoad Hooks
+		*/
+	public function __construct() {
+		add_action( 'admin_action_export_data',         array( &$this, 'single_post_export_data' ) );
+		add_filter( 'post_row_actions',                 array( &$this, 'single_post_export' ), 10, 2 );
+		add_filter( 'page_row_actions',                 array( &$this, 'single_post_export' ), 10, 2 );
+		add_action( 'admin_footer-edit.php',            array( &$this, 'single_post_export_bulk' ) );
+		add_action( 'load-edit.php',                    array( &$this, 'single_post_export_bulk_action' ) );
+	}
+
+	/*
+	* Main function
+	*/
+	public function single_post_export_data() {
+		if ( ! ( isset( $_GET['post'] ) || isset( $_POST['post'] ) || ( isset( $_REQUEST['action'] ) ) ) ) {
+			wp_die( 'No post to export has been supplied!' );
+		}
+		$post_id = ( isset( $_GET['post'] ) ? $_GET['post'] : $_POST['post'] );
+		if ( ! empty( $post_id ) ) {
+			include( get_template_directory() . '/exporter.php' );
+		} else {
+			wp_die( 'No post to export has been supplied!' );
+		}
+	}
+
+	/*
+	* Export multiple data
+	*/
+	public function single_post_export_bulk() {
+		if ( current_user_can( 'edit_posts' ) ) { ?>
+            <script type="text/javascript">
+				jQuery(document).ready(function () {
+					jQuery('<option>').val('export').text('<?php _e( 'Export', 'planet4-master-theme-backend' )?>').appendTo("select[name='action']");
+				});
+            </script>
+			<?php
+		}
+	}
+
+	/**
+	 * Export Bulk Action
+	 */
+	public function single_post_export_bulk_action() {
+		$wp_list_table   = _get_list_table( 'WP_Posts_List_Table' );
+		$action          = $wp_list_table->current_action();
+		$allowed_actions = array( 'export' );
+		if ( ! in_array( $action, $allowed_actions ) ) {
+			return false;
+		}
+		switch ( $action ) {
+			case 'export':
+				$sendback = 'admin.php?action=export_data&post=' . join( ',', $_REQUEST['post'] );
+				break;
+
+			default:
+				return;
+		}
+		wp_redirect( $sendback );
+		exit();
+	}
+
+	/*
+	 * Add Export Link
+	 */
+	public function single_post_export( $actions, $post ) {
+		if ( current_user_can( 'edit_posts' ) ) {
+			$actions['export'] = '<a href="admin.php?action=export_data&amp;post=' . $post->ID . '" title="' . __( 'Export', 'planet4-master-theme-backend' ) . '" rel="permalink">' . __( 'Export', 'planet4-master-theme-backend' ) . '</a>';
+		}
+
+		return $actions;
+	}
+
+}
---
+++ exporter.php
@@ -0,0 +1,378 @@
+<?php
+/**
+ * Post Exporter Functionality
+ */
+global $wpdb, $post;
+$defaults = array(
+	'content'    => 'all',
+	'author'     => false,
+	'category'   => false,
+	'start_date' => false,
+	'end_date'   => false,
+	'status'     => false,
+);
+$args     = wp_parse_args( $post, $defaults );
+$sitename = sanitize_key( get_bloginfo( 'name' ) );
+if ( ! empty( $sitename ) ) {
+	$sitename .= '.';
+}
+$filename = $sitename . date( 'Ymdhis' ) . '.xml';
+
+header( 'Content-Description: File Transfer' );
+header( 'Content-Disposition: attachment; filename=' . $filename );
+header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+
+if ( 'all' != $args['content'] && post_type_exists( $args['content'] ) ) {
+	$ptype = get_post_type_object( $args['content'] );
+
+	if ( ! $ptype->can_export ) {
+		$args['content'] = 'post';
+	}
+
+	$where = $wpdb->prepare( "{$wpdb->posts}.post_type = %s", $args['content'] );
+} else {
+	$post_types = get_post_types( array( 'can_export' => true ) );
+	$esses      = array_fill( 0, count( $post_types ), '%s' );
+	$where      = $wpdb->prepare( "{$wpdb->posts}.post_type IN (" . implode( ',', $esses ) . ')', $post_types );
+}
+
+if ( $args['status'] && ( 'post' == $args['content'] || 'page' == $args['content'] ) ) {
+	$where .= $wpdb->prepare( " AND {$wpdb->posts}.post_status = %s", $args['status'] );
+} else {
+	$where .= " AND {$wpdb->posts}.post_status != 'auto-draft'";
+}
+
+$join = '';
+if ( $args['category'] && 'post' == $args['content'] ) {
+	if ( $term = term_exists( $args['category'], 'category' ) ) {
+		$join  = "INNER JOIN {$wpdb->term_relationships} ON ({$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id)";
+		$where .= $wpdb->prepare( " AND {$wpdb->term_relationships}.term_taxonomy_id = %d", $term['term_taxonomy_id'] );
+	}
+}
+
+if ( 'post' == $args['content'] || 'page' == $args['content'] ) {
+	if ( $args['author'] ) {
+		$where .= $wpdb->prepare( " AND {$wpdb->posts}.post_author = %d", $args['author'] );
+	}
+
+	if ( $args['start_date'] ) {
+		$where .= $wpdb->prepare( " AND {$wpdb->posts}.post_date >= %s", date( 'Y-m-d', strtotime( $args['start_date'] ) ) );
+	}
+
+	if ( $args['end_date'] ) {
+		$where .= $wpdb->prepare( " AND {$wpdb->posts}.post_date < %s", date( 'Y-m-d', strtotime( '+1 month', strtotime( $args['end_date'] ) ) ) );
+	}
+}
+$post_ids = apply_filters( 'export_post_ids', $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} $join WHERE $where" ), $args );
+
+$cats = $tags = $terms = array();
+if ( isset( $term ) && $term ) {
+	$cat  = get_term( $term['term_id'], 'category' );
+	$cats = array( $cat->term_id => $cat );
+	unset( $term, $cat );
+} else if ( 'all' == $args['content'] ) {
+	$categories = (array) get_categories( array( 'get' => 'all' ) );
+	$tags       = (array) get_tags( array( 'get' => 'all' ) );
+
+	$custom_taxonomies = get_taxonomies( array( '_builtin' => false ) );
+	$custom_terms      = (array) get_terms( $custom_taxonomies, array( 'get' => 'all' ) );
+	while ( $cat = array_shift( $categories ) ) {
+		if ( $cat->parent == 0 || isset( $cats[ $cat->parent ] ) ) {
+			$cats[ $cat->term_id ] = $cat;
+		} else {
+			$categories[] = $cat;
+		}
+	}
+	while ( $t = array_shift( $custom_terms ) ) {
+		if ( $t->parent == 0 || isset( $terms[ $t->parent ] ) ) {
+			$terms[ $t->term_id ] = $t;
+		} else {
+			$custom_terms[] = $t;
+		}
+	}
+
+	unset( $categories, $custom_taxonomies, $custom_terms );
+}
+
+function p4_px_single_post_cdata( $str ) {
+	if ( seems_utf8( $str ) == false ) {
+		$str = utf8_encode( $str );
+	}
+	$str = '<![CDATA[' . str_replace( ']]>', ']]]]><![CDATA[>', $str ) . ']]>';
+
+	return $str;
+}
+
+function p4_px_single_post_site_url() {
+	if ( is_multisite() ) {
+		return network_home_url();
+	} else {
+		return get_bloginfo_rss( 'url' );
+	}
+}
+
+function p4_px_single_post_cat_name( $category ) {
+	if ( empty( $category->name ) ) {
+		return;
+	}
+
+	echo '<wp:cat_name>' . p4_px_single_post_cdata( $category->name ) . '</wp:cat_name>';
+}
+
+function p4_px_single_post_category_description( $category ) {
+	if ( empty( $category->description ) ) {
+		return;
+	}
+
+	echo '<wp:category_description>' . p4_px_single_post_cdata( $category->description ) . '</wp:category_description>';
+}
+
+function p4_px_single_post_tag_name( $tag ) {
+	if ( empty( $tag->name ) ) {
+		return;
+	}
+
+	echo '<wp:tag_name>' . p4_px_single_post_cdata( $tag->name ) . '</wp:tag_name>';
+}
+
+function p4_px_single_post_tag_description( $tag ) {
+	if ( empty( $tag->description ) ) {
+		return;
+	}
+
+	echo '<wp:tag_description>' . p4_px_single_post_cdata( $tag->description ) . '</wp:tag_description>';
+}
+
+function p4_px_single_post_term_name( $term ) {
+	if ( empty( $term->name ) ) {
+		return;
+	}
+
+	echo '<wp:term_name>' . p4_px_single_post_cdata( $term->name ) . '</wp:term_name>';
+}
+
+function p4_px_single_post_term_description( $term ) {
+	if ( empty( $term->description ) ) {
+		return;
+	}
+
+	echo '<wp:term_description>' . p4_px_single_post_cdata( $term->description ) . '</wp:term_description>';
+}
+
+function p4_px_single_post_authors_list() {
+	global $wpdb;
+
+	$authors = array();
+	$results = $wpdb->get_results( "SELECT DISTINCT post_author FROM $wpdb->posts WHERE post_status != 'auto-draft'" );
+	foreach ( (array) $results as $result ) {
+		$authors[] = get_userdata( $result->post_author );
+	}
+
+	$authors = array_filter( $authors );
+
+	foreach ( $authors as $author ) {
+		echo "\t<wp:author>";
+		echo '<wp:author_id>' . $author->ID . '</wp:author_id>';
+		echo '<wp:author_login>' . $author->user_login . '</wp:author_login>';
+		echo '<wp:author_email>' . $author->user_email . '</wp:author_email>';
+		echo '<wp:author_display_name>' . p4_px_single_post_cdata( $author->display_name ) . '</wp:author_display_name>';
+		echo '<wp:author_first_name>' . p4_px_single_post_cdata( $author->user_firstname ) . '</wp:author_first_name>';
+		echo '<wp:author_last_name>' . p4_px_single_post_cdata( $author->user_lastname ) . '</wp:author_last_name>';
+		echo "</wp:author>\n";
+	}
+}
+
+function p4_px_single_post_nav_menu_terms() {
+	$nav_menus = wp_get_nav_menus();
+	if ( empty( $nav_menus ) || ! is_array( $nav_menus ) ) {
+		return;
+	}
+
+	foreach ( $nav_menus as $menu ) {
+		echo "\t<wp:term><wp:term_id>{$menu->term_id}</wp:term_id><wp:term_taxonomy>nav_menu</wp:term_taxonomy><wp:term_slug>{$menu->slug}</wp:term_slug>";
+		p4_px_single_post_term_name( $menu );
+		echo "</wp:term>\n";
+	}
+}
+
+function p4_px_single_post_taxonomy() {
+	$post = get_post();
+
+	$taxonomies = get_object_taxonomies( $post->post_type );
+	if ( empty( $taxonomies ) ) {
+		return;
+	}
+	$terms = wp_get_object_terms( $post->ID, $taxonomies );
+
+	foreach ( (array) $terms as $term ) {
+		echo "\t\t<category domain=\"{$term->taxonomy}\" nicename=\"{$term->slug}\">" . p4_px_single_post_cdata( $term->name ) . "</category>\n";
+	}
+}
+
+function p4_px_single_post_filter_postmeta( $return_me, $meta_key ) {
+	if ( '_edit_lock' == $meta_key ) {
+		$return_me = true;
+	}
+
+	return $return_me;
+}
+
+add_filter( 'p4_px_single_post_export_skip_postmeta', 'p4_px_single_post_filter_postmeta', 10, 2 );
+
+echo '<?xml version="1.0" encoding="' . get_bloginfo( 'charset' ) . "\" ?>\n";
+
+?>
+<!-- This is a WordPress eXtended RSS file generated by WordPress as an export of your site. -->
+<!-- It contains information about your site's posts, pages, comments, categories, and other content. -->
+<!-- You may use this file to transfer that content from one site to another. -->
+<!-- This file is not intended to serve as a complete backup of your site. -->
+
+<!-- To import this information into a WordPress site follow these steps: -->
+<!-- 1. Log in to that site as an administrator. -->
+<!-- 2. Go to Tools: Import in the WordPress admin panel. -->
+<!-- 3. Install the "WordPress" importer from the list. -->
+<!-- 4. Activate & Run Importer. -->
+<!-- 5. Upload this file using the form provided on that page. -->
+<!-- 6. You will first be asked to map the authors in this export file to users -->
+<!--    on the site. For each author, you may choose to map to an -->
+<!--    existing user on the site or to create a new user. -->
+<!-- 7. WordPress will then import each of the posts, pages, comments, categories, etc. -->
+<!--    contained in this file into your site. -->
+
+<?php the_generator( 'export' ); ?>
+<rss version="2.0"
+     xmlns:excerpt="http://wordpress.org/export/1.2/excerpt/"
+     xmlns:content="http://purl.org/rss/1.0/modules/content/"
+     xmlns:wp="http://wordpress.org/export/1.0/"
+>
+
+    <channel>
+        <title><?php bloginfo_rss( 'name' ); ?></title>
+        <link><?php bloginfo_rss( 'url' ); ?></link>
+        <description><?php bloginfo_rss( 'description' ); ?></description>
+        <pubDate><?php echo date( 'D, d M Y H:i:s +0000' ); ?></pubDate>
+        <language><?php bloginfo_rss( 'language' ); ?></language>
+        <wp:wxr_version><?php echo get_bloginfo( 'version' ); ?></wp:wxr_version>
+        <wp:base_site_url><?php echo p4_px_single_post_site_url(); ?></wp:base_site_url>
+        <wp:base_blog_url><?php bloginfo_rss( 'url' ); ?></wp:base_blog_url>
+
+		<?php p4_px_single_post_authors_list(); ?>
+
+		<?php foreach ( $cats as $c ) : ?>
+            <wp:category>
+                <wp:term_id><?php echo $c->term_id ?></wp:term_id>
+                <wp:category_nicename><?php echo $c->slug; ?></wp:category_nicename>
+                <wp:category_parent><?php echo $c->parent ? $cats[ $c->parent ]->slug : ''; ?></wp:category_parent><?php p4_px_single_post_cat_name( $c ); ?><?php p4_px_single_post_category_description( $c ); ?>
+            </wp:category>
+		<?php endforeach; ?>
+		<?php foreach ( $tags as $t ) : ?>
+            <wp:tag>
+                <wp:term_id><?php echo $t->term_id ?></wp:term_id>
+                <wp:tag_slug><?php echo $t->slug; ?></wp:tag_slug><?php p4_px_single_post_tag_name( $t ); ?><?php p4_px_single_post_tag_description( $t ); ?>
+            </wp:tag>
+		<?php endforeach; ?>
+		<?php foreach ( $terms as $t ) : ?>
+            <wp:term>
+                <wp:term_id><?php echo $t->term_id ?></wp:term_id>
+                <wp:term_taxonomy><?php echo $t->taxonomy; ?></wp:term_taxonomy>
+                <wp:term_slug><?php echo $t->slug; ?></wp:term_slug>
+                <wp:term_parent><?php echo $t->parent ? $terms[ $t->parent ]->slug : ''; ?></wp:term_parent><?php p4_px_single_post_term_name( $t ); ?><?php p4_px_single_post_term_description( $t ); ?>
+            </wp:term>
+		<?php endforeach; ?>
+		<?php if ( 'all' == $args['content'] ) {
+			p4_px_single_post_nav_menu_terms();
+		} ?>
+
+		<?php
+		do_action( 'rss2_head' );
+		?>
+
+		<?php
+		$post_id = explode( ",", $_GET['post'] );
+		if ( $post_id != "" ) {
+			global $wp_query;
+			$wp_query->in_the_loop = true;
+			for ( $i = 0; $i < count( $post_id ); $i ++ ) {
+				$next_posts = array( $post_id[ $i ] );
+				$attachments = get_attached_media( '', $post_id[ $i ] );
+				if ( ! empty( $attachments ) ) {
+				    foreach( $attachments as $attachment ) {
+                        array_push( $next_posts, $attachment->ID );
+                    }
+                }
+				$where      = 'WHERE ID IN (' . implode( ',', $next_posts ) . ')';
+				$posts      = $wpdb->get_results( "SELECT * FROM {$wpdb->posts} $where" );
+				foreach ( $posts as $post ) {
+					setup_postdata( $post );
+					$is_sticky = is_sticky( $post->ID ) ? 1 : 0;
+					?>
+                    <item>
+                        <title><?php echo apply_filters( 'the_title_rss', $post->post_title ); ?></title>
+                        <link><?php the_permalink_rss() ?></link>
+                        <pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true ), false ); ?></pubDate>
+                        <dc:creator><?php echo p4_px_single_post_cdata( get_the_author_meta( 'login' ) ); ?></dc:creator>
+                        <guid isPermaLink="false"><?php the_guid(); ?></guid>
+                        <description></description>
+                        <content:encoded><?php echo p4_px_single_post_cdata( apply_filters( 'the_content_export', $post->post_content ) );
+							?></content:encoded>
+                        <excerpt:encoded><?php echo p4_px_single_post_cdata( apply_filters( 'the_excerpt_export', $post->post_excerpt ) );
+							?></excerpt:encoded>
+                        <wp:post_id><?php echo $post->ID; ?></wp:post_id>
+                        <wp:post_date><?php echo $post->post_date; ?></wp:post_date>
+                        <wp:post_date_gmt><?php echo $post->post_date_gmt; ?></wp:post_date_gmt>
+                        <wp:comment_status><?php echo $post->comment_status; ?></wp:comment_status>
+                        <wp:ping_status><?php echo $post->ping_status; ?></wp:ping_status>
+                        <wp:post_name><?php echo $post->post_name; ?></wp:post_name>
+                        <wp:status><?php echo $post->post_status; ?></wp:status>
+                        <wp:post_parent><?php echo $post->post_parent; ?></wp:post_parent>
+                        <wp:menu_order><?php echo $post->menu_order; ?></wp:menu_order>
+                        <wp:post_type><?php echo $post->post_type; ?></wp:post_type>
+                        <wp:post_password><?php echo $post->post_password; ?></wp:post_password>
+                        <wp:is_sticky><?php echo $is_sticky; ?></wp:is_sticky>
+						<?php if ( $post->post_type == 'attachment' ) : ?>
+                            <wp:attachment_url><?php echo wp_get_attachment_url( $post->ID ); ?></wp:attachment_url>
+						<?php endif; ?>
+						<?php p4_px_single_post_taxonomy(); ?>
+						<?php $postmeta = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $wpdb->postmeta WHERE post_id = %d", $post->ID ) );
+						foreach ( $postmeta as $meta ) :
+							if ( apply_filters( 'p4_px_single_post_export_skip_postmeta', false, $meta->meta_key, $meta ) ) {
+								continue;
+							}
+							?>
+                            <wp:postmeta>
+                                <wp:meta_key><?php echo $meta->meta_key; ?></wp:meta_key>
+                                <wp:meta_value><?php echo p4_px_single_post_cdata( $meta->meta_value ); ?></wp:meta_value>
+                            </wp:postmeta>
+						<?php endforeach; ?>
+						<?php $comments = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $wpdb->comments WHERE comment_post_ID = %d AND comment_approved <> 'spam'", $post->ID ) );
+						foreach ( $comments as $c ) : ?>
+                            <wp:comment>
+                                <wp:comment_id><?php echo $c->comment_ID; ?></wp:comment_id>
+                                <wp:comment_author><?php echo p4_px_single_post_cdata( $c->comment_author ); ?></wp:comment_author>
+                                <wp:comment_author_email><?php echo $c->comment_author_email; ?></wp:comment_author_email>
+                                <wp:comment_author_url><?php echo esc_url_raw( $c->comment_author_url ); ?></wp:comment_author_url>
+                                <wp:comment_author_IP><?php echo $c->comment_author_IP; ?></wp:comment_author_IP>
+                                <wp:comment_date><?php echo $c->comment_date; ?></wp:comment_date>
+                                <wp:comment_date_gmt><?php echo $c->comment_date_gmt; ?></wp:comment_date_gmt>
+                                <wp:comment_content><?php echo p4_px_single_post_cdata( $c->comment_content ) ?></wp:comment_content>
+                                <wp:comment_approved><?php echo $c->comment_approved; ?></wp:comment_approved>
+                                <wp:comment_type><?php echo $c->comment_type; ?></wp:comment_type>
+                                <wp:comment_parent><?php echo $c->comment_parent; ?></wp:comment_parent>
+                                <wp:comment_user_id><?php echo $c->user_id; ?></wp:comment_user_id>
+								<?php $c_meta = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $wpdb->commentmeta WHERE comment_id = %d", $c->comment_ID ) );
+								foreach ( $c_meta as $meta ) : ?>
+                                    <wp:commentmeta>
+                                        <wp:meta_key><?php echo $meta->meta_key; ?></wp:meta_key>
+                                        <wp:meta_value><?php echo p4_px_single_post_cdata( $meta->meta_value ); ?></wp:meta_value>
+                                    </wp:commentmeta>
+								<?php endforeach; ?>
+                            </wp:comment>
+						<?php endforeach; ?>
+                    </item>
+					<?php
+				}
+			}
+		}
+		?>
+    </channel>
+</rss>
