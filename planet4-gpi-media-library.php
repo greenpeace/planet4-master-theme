bbf436fbf53451da36c6bd725a07260d0c15fd5a
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-03-12 19:50:20 +0200

    v1.6

--- planet4-gpi-media-library.php
+++ planet4-gpi-media-library.php
@@ -3,7 +3,7 @@
  * Plugin Name: Planet4 - Gpi Media Library
  * Description: Connects Planet4 with the Media Library platform.
  * Plugin URI: http://github.com/greenpeace/planet4-plugin-medialibrary
- * Version: 1.5
+ * Version: 1.6
  * Php Version: 7.0
  *
  * Author: Greenpeace International
