4dcf6e2bcd53b84389b021e05f49149864dfb2a2
Angelos Theodorakopoulos <angtheod@gmail.com>
2019-03-26 17:34:23 +0200

    Get countries as a JSON file and use it to build the countries drop down menu.

--- assets/js/partials/country_select.js
+++ assets/js/partials/country_select.js
@@ -15,4 +15,41 @@ $(document).ready(function() {
     $(this).toggleClass('active');
     $(this).parent().find('.option-contry').toggleClass('active');
   });
+
+  // Get Countries List from <script> data block.
+  let countries_json = JSON.parse( $('#countries_script').text() );
+
+  // Build html for countries drop down list.
+  let countries_html = $(
+    '<div id="country-list" class="country-list">' +
+      '<a class="international" href=""></a>' +
+      '<ul class="countries_list"></ul>' +
+    '</div>'
+  );
+
+  $.each(countries_json, function (index, element) {
+    if ( "0" === index ) {
+      $('.international', countries_html)
+        .attr( 'href', element[0].url )
+        .text( element[0].name );
+
+    } else {
+      let countries_sublist = $(
+        '<li><h3 class="country-group-letter">' + index +
+          '</h3>' +
+          '<ul class="countries_sublist"></ul>' +
+        '</li>'
+      );
+      $('.countries_list', countries_html).append( countries_sublist );
+
+      $.each(element, function (index, inner_element) {
+        $('.countries_sublist', countries_sublist).append(
+          '<li>' +
+            '<a href="' + inner_element.url + '">' + inner_element.name + '</a>' +
+          '</li>');
+      });
+    }
+  });
+
+  $('#navbar-dropdown li').append( countries_html );
 });
