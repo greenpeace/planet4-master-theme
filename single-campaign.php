80b6f825f50d87f1ac585f31e51aa88b8af9d935
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-04-11 15:09:12 +0530

    PLANET-3396 CPP - Add DataLayer fields on campaign page

--- single-campaign.php
+++ single-campaign.php
@@ -79,6 +79,11 @@ $context['og_description']          = $post->get_og_description();
 $context['og_image_data']           = $post->get_og_image();
 $context['custom_styles']           = $custom_styles;
 
+// P4 Campaign/dataLayer fields.
+$context['cf_campaign_name'] = $page_meta_data['p4_campaign_name'][0] ?? '';
+$context['cf_basket_name']   = $page_meta_data['p4_basket_name'][0] ?? '';
+$context['cf_scope']         = $page_meta_data['p4_scope'][0] ?? '';
+$context['cf_department']    = $page_meta_data['p4_department'][0] ?? '';
 
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
