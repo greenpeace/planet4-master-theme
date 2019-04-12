1439ddef1daa19dfbd6149d389f00145ad6a8bdc
Pablo Cubico <pablocuadrado@gmail.com>
2019-04-12 00:13:31 -0300

    Used colors provided by Will.

--- single-campaign.php
+++ single-campaign.php
@@ -35,7 +35,7 @@ if ( $pf && array_key_exists( $pf, $header_font_style ) ) {
 	$header_font = "font-family: {$pf} !important;";
 }
 
-$footer_links_color = $post->campaign_logo_color == 'light' ? 'white' : 'black';
+$footer_links_color = $post->campaign_logo_color == 'light' ? '#FFFFFF' : '#1A1A1A';
 
 $custom_styles['css']['nav_color']               = $post->campaign_nav_color ? ".navbar { background-color: {$post->campaign_nav_color} !important;}" : null;
 $custom_styles['nav_type']                       = $post->campaign_nav_type;
