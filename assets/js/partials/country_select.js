3b5c4e4f553e788f643673f707a380939a86c945
Angelos Theodorakopoulos <angtheod@gmail.com>
2019-04-19 14:19:01 +0300

    PLANET-3495 Fixes admin panel JS error, related to country selector.

--- assets/js/partials/country_select.js
+++ assets/js/partials/country_select.js
@@ -17,39 +17,42 @@ $(document).ready(function() {
   });
 
   // Get Countries List from <script> data block.
-  let countries_json = JSON.parse( $('#countries_script').text() );
+  let $countries_script = $('#countries_script');
+  if ( $countries_script.length > 0 ) {
+    let countries_json = JSON.parse($countries_script.text());
 
-  // Build html for countries drop down list.
-  let countries_html = $(
-    '<div id="country-list" class="country-list">' +
+    // Build html for countries drop down list.
+    let countries_html = $(
+      '<div id="country-list" class="country-list">' +
       '<a class="international" href=""></a>' +
       '<ul class="countries_list"></ul>' +
-    '</div>'
-  );
+      '</div>'
+    );
 
-  $.each(countries_json, function (index, element) {
-    if ( '0' === index ) {
-      $('.international', countries_html)
-        .attr( 'href', element[0].url )
-        .text( element[0].name );
+    $.each(countries_json, function (index, element) {
+      if ('0' === index) {
+        $('.international', countries_html)
+          .attr('href', element[0].url)
+          .text(element[0].name);
 
-    } else {
-      let countries_sublist = $(
-        '<li>' +
+      } else {
+        let countries_sublist = $(
+          '<li>' +
           '<h3 class="country-group-letter">' + index + '</h3>' +
           '<ul class="countries_sublist"></ul>' +
-        '</li>'
-      );
-      $('.countries_list', countries_html).append( countries_sublist );
+          '</li>'
+        );
+        $('.countries_list', countries_html).append(countries_sublist);
 
-      $.each(element, function (index, inner_element) {
-        $('.countries_sublist', countries_sublist).append(
-          '<li>' +
+        $.each(element, function (index, inner_element) {
+          $('.countries_sublist', countries_sublist).append(
+            '<li>' +
             '<a href="' + inner_element.url + '">' + inner_element.name + '</a>' +
-          '</li>');
-      });
-    }
-  });
+            '</li>');
+        });
+      }
+    });
 
-  $('#navbar-dropdown #country-select').append( countries_html );
+    $('#navbar-dropdown #country-select').append(countries_html);
+  }
 });
