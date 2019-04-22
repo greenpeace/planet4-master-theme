f353c0948c22ca90871974f416a6a5b7efebd851
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-04-22 18:51:01 +0530

    fixup! Merge branch 'planet-3435' of https://github.com/greenpeace/planet4-master-theme into planet-3435

--- classes/class-p4-metabox-register.php
+++ classes/class-p4-metabox-register.php
@@ -18,7 +18,6 @@ class P4_Metabox_Register {
 	private $prefix = 'p4_';
 
 	/**
-<<<<<<< HEAD
 	 * The maximum number of take action pages to show in dropdown.
 	 *
 	 * @const int MAX_TAKE_ACTION_PAGES
@@ -26,8 +25,6 @@ class P4_Metabox_Register {
 	const MAX_TAKE_ACTION_PAGES = 100;
 
 	/**
-=======
->>>>>>> ba6919bc43d00ae1ba464de2ed17334862db2ce4
 	 * P4_Metabox_Register constructor.
 	 */
 	public function __construct() {
@@ -142,221 +139,6 @@ class P4_Metabox_Register {
 				'preview_size' => 'large',
 			]
 		);
-<<<<<<< HEAD
-=======
-
-		$p4_post = new_cmb2_box(
-			[
-				'id'           => $this->prefix . 'metabox_post',
-				'title'        => __( 'Post Articles Element Fields', 'planet4-master-theme-backend' ),
-				'object_types' => [ 'post' ],
-			]
-		);
-
-		$p4_post->add_field(
-			[
-				'name' => __( 'Author Override', 'planet4-master-theme-backend' ),
-				'desc' => __( 'Enter author name if you want to override the author', 'planet4-master-theme-backend' ),
-				'id'   => $this->prefix . 'author_override',
-				'type' => 'text_medium',
-			]
-		);
-
-		$p4_post->add_field(
-			[
-				'name'             => __( 'Take Action Page Selector', 'planet4-master-theme-backend' ),
-				'desc'             => __( 'Select a Take Action Page to populate take action boxout block', 'planet4-master-theme-backend' ),
-				'id'               => $this->prefix . 'take_action_page',
-				'type'             => 'select',
-				'show_option_none' => true,
-				'options_cb'       => [ $this, 'populate_act_page_children_options' ],
-			]
-		);
-
-		$p4_post->add_field(
-			[
-				'name'    => __( 'Include Articles In Post', 'planet4-master-theme-backend' ),
-				'id'      => 'include_articles',
-				'type'    => 'select',
-				'options' => [
-					'yes' => 'Yes',
-					'no'  => 'No',
-				],
-			]
-		);
-
-		$p4_post->add_field(
-			[
-				'name'         => __( 'Background Image Override', 'planet4-master-theme-backend' ),
-				'desc'         => __( 'Upload an image or select one from the media library to override the background image', 'planet4-master-theme-backend' ),
-				'id'           => $this->prefix . 'background_image_override',
-				'type'         => 'file',
-				'options'      => [
-					'url' => false,
-				],
-				'text'         => [
-					'add_upload_file_text' => __( 'Add Image', 'planet4-master-theme-backend' ),
-				],
-				'preview_size' => 'large',
-			]
-		);
-
-		$p4_open_graph = new_cmb2_box(
-			[
-				'id'           => $this->prefix . 'metabox_og',
-				'title'        => __( 'Open Graph/Social Fields', 'planet4-master-theme-backend' ),
-				'object_types' => [ 'page', 'post' ],
-				'closed'       => true,  // Keep the metabox closed by default.
-			]
-		);
-
-		$p4_open_graph->add_field(
-			[
-				'name' => __( 'Title', 'planet4-master-theme-backend' ),
-				'desc' => __( 'Enter title if you want to override the open graph title', 'planet4-master-theme-backend' ),
-				'id'   => $this->prefix . 'og_title',
-				'type' => 'text_medium',
-			]
-		);
-
-		$p4_open_graph->add_field(
-			[
-				'name'    => __( 'Description', 'planet4-master-theme-backend' ),
-				'desc'    => __( 'Enter description if you want to override the open graph description', 'planet4-master-theme-backend' ),
-				'id'      => $this->prefix . 'og_description',
-				'type'    => 'wysiwyg',
-				'options' => [
-					'media_buttons' => false,
-					'textarea_rows' => 5,
-				],
-			]
-		);
-
-		$p4_open_graph->add_field(
-			[
-				'name'         => __( 'Image Override', 'planet4-master-theme-backend' ),
-				'desc'         => __( 'Upload an image or select one from the media library to override the open graph image', 'planet4-master-theme-backend' ),
-				'id'           => $this->prefix . 'og_image',
-				'type'         => 'file',
-				'options'      => [
-					'url' => false,
-				],
-				'text'         => [
-					'add_upload_file_text' => __( 'Add Image', 'planet4-master-theme-backend' ),
-				],
-				'query_args'   => [
-					'type' => 'image',
-				],
-				'preview_size' => 'large',
-			]
-		);
-
-		// P4 Datalayer/Campaign fields.
-		$p4_campaign_fields = new_cmb2_box(
-			[
-				'id'           => $this->prefix . 'campaign_fields',
-				'title'        => __( 'Campaign information (dataLayer)', 'planet4-master-theme-backend' ),
-				'object_types' => [ 'page', 'campaign' ], // Post type.
-				'closed'       => true,  // Keep the metabox closed by default.
-				'context'      => 'side', // show cmb2box in right sidebar.
-				'priority'     => 'low',
-				'show_names'   => false, // Hide the labels.
-			]
-		);
-
-		$campaign_options = [
-			'not set'                            => __( '- Select Campaign -', 'planet4-master-theme-backend' ),
-			'Local Campaign'                     => 'Local Campaign',
-			'Ocean Sanctuaries'                  => 'Ocean Sanctuaries',
-			'Climate Emergency Response'         => 'Climate Emergency Response',
-			'All Eyes on the Amazon'             => 'All Eyes on the Amazon',
-			'Asia Energy Transition'             => 'Asia Energy Transition',
-			'BrAndino: Hold the Line'            => 'BrAndino: Hold the Line',
-			'Break Free'                         => 'Break Free',
-			'Climate Justice Liability'          => 'Climate Justice Liability',
-			'Congo Basin Forests'                => 'Congo Basin Forests',
-			'Corporate ICE/ Clean Air Now'       => 'Corporate ICE/ Clean Air Now',
-			'European Energy Transition'         => 'European Energy Transition',
-			'Cross-commodities markets campaign' => 'Cross-commodities markets campaign',
-			'The Future of Europe project'       => 'The Future of Europe project',
-			'Greenpeace Fires'                   => 'Greenpeace Fires',
-			'Indonesia Forests'                  => 'Indonesia Forests',
-			'Meat & Dairy'                       => 'Meat & Dairy',
-			'Plastics Free Future'               => 'Plastics Free Future',
-			'Shifting the trillions'             => 'Shifting the trillions',
-			'Stolen Fish'                        => 'Stolen Fish',
-			'Amazon Reef'                        => 'Amazon Reef',
-			'People vs. Oil'                     => 'People vs. Oil',
-			'Pipelines'                          => 'Pipelines',
-			'Ends of the Earth'                  => 'Ends of the Earth',
-			'Patagonia'                          => 'Patagonia',
-			'Urban Revolution'                   => 'Urban Revolution',
-		];
-
-		$p4_campaign_fields->add_field(
-			[
-				'name'       => __( 'Campaign Name', 'planet4-master-theme-backend' ),
-				'id'         => $this->prefix . 'campaign_name',
-				'type'       => 'select',
-				'options'    => $campaign_options,
-				'attributes' => [
-					'data-validation' => 'required',
-				],
-			]
-		);
-
-		$basket_options = [
-			'not set'              => __( '- Select Basket -', 'planet4-master-theme-backend' ),
-			'Forests'              => 'Forests',
-			'Oceans'               => 'Oceans',
-			'Good Life'            => 'Good Life',
-			'Food'                 => 'Food',
-			'Climate & Energy'     => 'Climate & Energy',
-			'Oil'                  => 'Oil',
-			'Political & Business' => 'Political & Business',
-		];
-
-		$p4_campaign_fields->add_field(
-			[
-				'name'       => __( 'Basket Name', 'planet4-master-theme-backend' ),
-				'id'         => $this->prefix . 'basket_name',
-				'type'       => 'select',
-				'options'    => $basket_options,
-				'attributes' => [
-					'data-validation' => 'required',
-				],
-			]
-		);
-
-		$scope_options = [
-			'not set'  => __( '- Select Scope -', 'planet4-master-theme-backend' ),
-			'Global'   => 'Global',
-			'National' => 'National',
-		];
-
-		$p4_campaign_fields->add_field(
-			[
-				'name'       => __( 'Scope', 'planet4-master-theme-backend' ),
-				'id'         => $this->prefix . 'scope',
-				'type'       => 'select',
-				'options'    => $scope_options,
-				'attributes' => [
-					'data-validation' => 'required',
-				],
-			]
-		);
-
-		$p4_campaign_fields->add_field(
-			[
-				'name'       => __( 'Department', 'planet4-master-theme-backend' ),
-				'id'         => $this->prefix . 'department',
-				'type'       => 'text_medium',
-				'attributes' => [
-					'placeholder' => __( 'Add Department', 'planet4-master-theme-backend' ),
-				],
-			]
-		);
->>>>>>> ba6919bc43d00ae1ba464de2ed17334862db2ce4
 	}
 
 	/**
@@ -606,11 +388,7 @@ class P4_Metabox_Register {
 				'orderby'          => 'menu_order',
 				'order'            => 'ASC',
 				'suppress_filters' => false,
-<<<<<<< HEAD
 				'numberposts'      => self::MAX_TAKE_ACTION_PAGES,
-=======
-				'numberposts'      => -1,
->>>>>>> ba6919bc43d00ae1ba464de2ed17334862db2ce4
 			];
 
 			$posts = get_posts( $take_action_pages_args );
