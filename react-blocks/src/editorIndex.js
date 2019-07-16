75628dac54e6fb985d9e48c10bd52f757e8abff5
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-07-16 20:27:31 +0530

    PLANET-3757 First version of Happypoint block

--- react-blocks/src/editorIndex.js
+++ react-blocks/src/editorIndex.js
@@ -2,6 +2,7 @@ import { ArticlesBlock } from './blocks/Articles/ArticlesBlock';
 import { CookiesBlock } from './blocks/Cookies/CookiesBlock';
 import { CoversBlock } from './blocks/Covers/CoversBlock';
 import { GalleryBlock } from './blocks/Gallery/GalleryBlock';
+import { HappypointBlock } from './blocks/Happypoint/HappypointBlock';
 import { MediaBlock } from './blocks/Media/MediaBlock';
 import { SocialmediaBlock } from './blocks/Socialmedia/SocialmediaBlock';
 import { SubmenuBlock } from './blocks/Submenu/SubmenuBlock';
@@ -11,7 +12,9 @@ const articlesBlock = new ArticlesBlock();
 const cookiesBlock = new CookiesBlock();
 const coversBlock = new CoversBlock();
 const galleryBlock = new GalleryBlock();
+const happypointBlock = new HappypointBlock();
 const mediaBlock = new MediaBlock();
 const socialmediaBlock = new SocialmediaBlock();
 const submenuBlock = new SubmenuBlock();
 const timelineBlock = new TimelineBlock();
+
