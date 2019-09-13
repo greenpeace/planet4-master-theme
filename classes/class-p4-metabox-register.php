6ab98095c70dd031d4c670348f6c73bd9909f071
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-09-13 14:54:08 +0530

    PLANET-3875 Allow user to hide page title on post type 'Page'

--- classes/class-p4-metabox-register.php
+++ classes/class-p4-metabox-register.php
@@ -142,6 +142,16 @@ class P4_Metabox_Register {
 				'preview_size' => 'large',
 			]
 		);
+
+		$p4_header->add_field(
+			[
+				'name'       => __( 'Hide page title', 'planet4-master-theme-backend' ),
+				'desc'       => __( 'Hide page title on frontend page.', 'planet4-master-theme-backend' ),
+				'id'         => $this->prefix . 'hide_page_title_checkbox',
+				'type'       => 'checkbox',
+				'show_on_cb' => [ $this, 'is_not_campaign_post' ],
+			]
+		);
 	}
 
 	/**
