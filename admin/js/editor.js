37d1ed71cc0428d6f47f2d05e4ac01b68fc4adc2
Nikos Roussos <nikos@roussos.cc>
2019-10-08 13:29:52 +0300

    PLANET-4152 Sync button styles to our styleguide
    Ref: https://jira.greenpeace.org/browse/PLANET-4152

---
+++ admin/js/editor.js
@@ -0,0 +1,20 @@
+/* global wp */
+
+wp.domReady(() => {
+  wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'secondary',
+    label: 'Secondary',
+    isDefault: true
+  });
+  wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'cta',
+    label: 'CTA'
+  });
+  wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'donate',
+    label: 'Donate'
+  });
+  wp.blocks.unregisterBlockStyle('core/button', 'default');
+  wp.blocks.unregisterBlockStyle('core/button', 'outline');
+  wp.blocks.unregisterBlockStyle('core/button', 'squared');
+});
