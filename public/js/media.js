90fbcae87e40da49bf69a9892b84d298fbbc3300
Pablo Cubico <pablocuadrado@gmail.com>
2019-11-08 00:52:35 -0300

    Just fixed a few typos in a comment

--- public/js/media.js
+++ public/js/media.js
@@ -2,7 +2,7 @@ jQuery(function ($) {
 
   let $all_oembed_videos = $( 'iframe[src*="youtube"]' );
 
-  //Remove height and with from imbed iframes and wrap im container
+  // Remove height and width from embedded iframes and wrap in container
   $all_oembed_videos.each(function() {
     $(this).removeAttr('height').removeAttr('width').wrap( '<div class="embed-container"></div>' );
   });
