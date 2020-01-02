bbf145836baf36795f8811aa9cd7c7aecf619c7f
inwerpsel <pieter.vincent1988@gmail.com>
2020-01-02 13:05:40 +0100

    PLANET-4481 Add script for back-top button
    this was in the previous blocks plugin but wasn't migrated.

--- public/js/submenu.js
+++ public/js/submenu.js
@@ -51,6 +51,31 @@ $(document).ready(function () {
     return false;
   });
 
+  const $backtop = $( '.back-top' );
+  const $submenu = $( '.submenu-block' );
+
+  if ( $submenu.length > 0 ) {
+    $( window ).scroll( function () {
+      if ( $( this ).scrollTop() > 400 ) {
+        $backtop.fadeIn();
+        if ( $( '.cookie-block:visible' ).length > 0 ) {
+          $backtop.css( 'bottom', '120px' );
+        } else {
+          $backtop.css( 'bottom', '50px' );
+        }
+      } else {
+        $backtop.fadeOut();
+      }
+    } );
+
+    $backtop.click( function () {
+      $( 'body, html' ).animate( {
+        scrollTop: 0
+      }, 800 );
+      return false;
+    } );
+  }
+
   /**
    * Append html links for a submenu entry children.
    *
