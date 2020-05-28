12c4ef9fbedd4f30e45524d684e6755852a79653
Nikos Roussos <nikos@roussos.cc>
2020-05-28 09:45:48 +0300

    v0.19

--- planet4-gutenberg-engagingnetworks.php
+++ planet4-gutenberg-engagingnetworks.php
@@ -3,7 +3,6 @@
  * Plugin Name: Planet4 - Gutenberg Engaging Networks
  * Description: Contains the Gutenberg blocks that are used by Planet4 project.
  * Plugin URI: http://github.com/greenpeace/planet4-plugin-engaging-networks
- * Version: 0.18
  * Php Version: 7.0
  *
  * Author: Greenpeace International
