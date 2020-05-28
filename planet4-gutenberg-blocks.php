70c41fe35c20d0fd503827728c8a456e842777ee
Nikos Roussos <nikos@roussos.cc>
2020-05-28 09:48:14 +0300

    v0.29

--- planet4-gutenberg-blocks.php
+++ planet4-gutenberg-blocks.php
@@ -3,7 +3,6 @@
  * Plugin Name: Planet4 - Gutenberg Blocks
  * Description: Contains the Gutenberg blocks that are used by Planet4 project.
  * Plugin URI: http://github.com/greenpeace/planet4-plugin-gutenberg-blocks
- * Version: 0.27.1
  * Php Version: 7.0
  *
  * Author: Greenpeace International
