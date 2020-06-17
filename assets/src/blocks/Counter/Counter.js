a8ce27cbb511a538bbb95ef4fdf9241792172421
Maud Leray <maudleray035@gmail.com>
2020-06-17 16:45:50 +0200

    PLANET-4799 MRefactor Counter title and description using RichText

--- assets/src/blocks/Counter/Counter.js
+++ assets/src/blocks/Counter/Counter.js
@@ -1,15 +1,17 @@
-import {Component, Fragment} from '@wordpress/element';
+import { Component } from '@wordpress/element';
 import {
   TextControl as BaseTextControl,
   TextareaControl as BaseTextareaControl,
-  ServerSideRender } from '@wordpress/components';
-import { LayoutSelector } from '../../components/LayoutSelector/LayoutSelector';
+  ServerSideRender
+} from '@wordpress/components';
 import { Preview } from '../../components/Preview';
 import withCharacterCounter from '../../components/withCharacterCounter/withCharacterCounter';
-import {URLInput} from "../../components/URLInput/URLInput";
+import { URLInput } from "../../components/URLInput/URLInput";
 
-const TextControl = withCharacterCounter( BaseTextControl );
-const TextareaControl = withCharacterCounter( BaseTextareaControl );
+const { RichText } = wp.editor;
+
+const TextControl = withCharacterCounter(BaseTextControl);
+const TextareaControl = withCharacterCounter(BaseTextareaControl);
 
 export class Counter extends Component {
   constructor(props) {
@@ -20,61 +22,65 @@ export class Counter extends Component {
     const { __ } = wp.i18n;
     return (
       <div>
-        <div>
-          <TextControl
-            label= { __('Title', 'p4ge') }
-            placeholder= { __('Enter title', 'p4ge') }
-            value={ this.props.title }
-            onChange={ this.props.onTitleChange }
+        <header>
+          <RichText
+            tagName="h2"
+            className="page-section-header"
+            placeholder={__('Enter title', 'p4ge')}
+            value={this.props.title}
+            onChange={this.props.onTitleChange}
+            keepPlaceholderOnFocus={true}
+            withoutInteractiveFormatting
             characterLimit={60}
           />
-        </div>
-
-        <div>
-          <TextareaControl
-            label= { __('Description', 'p4ge') }
-            placeholder= { __('Enter description', 'p4ge') }
-            value={ this.props.description }
-            onChange={ this.props.onDescriptionChange }
-            characterLimit={400}
-          />
-        </div>
+        </header>
+        <RichText
+          tagName="div"
+          className="page-section-description"
+          placeholder={__('Enter description', 'p4ge')}
+          value={this.props.description}
+          onChange={this.props.onDescriptionChange}
+          keepPlaceholderOnFocus={true}
+          multiline={true}
+          withoutInteractiveFormatting
+          characterLimit={400}
+        />
 
         <div>
           <TextControl
-            label= { __('Completed', 'p4ge') }
-            placeholder= { __('e.g. number of signatures', 'p4ge') }
+            label={__('Completed', 'p4ge')}
+            placeholder={__('e.g. number of signatures', 'p4ge')}
             type="number"
-            value={ this.props.completed }
-            onChange={ this.props.onCompletedChange }
+            value={this.props.completed}
+            onChange={this.props.onCompletedChange}
           />
         </div>
 
         <div>
           <URLInput
-            label={ __('Completed API URL', 'p4ge') }
-            placeholder={ __('API URL of completed number. If filled in will overide the \'Completed\' field', 'p4ge') }
-            value={ this.props.completed_api }
-            onChange={ this.props.onCompletedAPIChange }
+            label={__('Completed API URL', 'p4ge')}
+            placeholder={__('API URL of completed number. If filled in will overide the \'Completed\' field', 'p4ge')}
+            value={this.props.completed_api}
+            onChange={this.props.onCompletedAPIChange}
           />
         </div>
 
         <div>
           <TextControl
-            label= { __('Target', 'p4ge') }
-            placeholder= { __('e.g. target no. of signatures', 'p4ge') }
+            label={__('Target', 'p4ge')}
+            placeholder={__('e.g. target no. of signatures', 'p4ge')}
             type="number"
-            value={ this.props.target }
-            onChange={ this.props.onTargetChange }
+            value={this.props.target}
+            onChange={this.props.onTargetChange}
           />
         </div>
 
         <div>
           <TextareaControl
-            label= { __('Text', 'p4ge') }
-            placeholder= { __('e.g. "signatures collected of %target%"', 'p4ge') }
-            value={ this.props.text }
-            onChange={ this.props.onTextChange }
+            label={__('Text', 'p4ge')}
+            placeholder={__('e.g. "signatures collected of %target%"', 'p4ge')}
+            value={this.props.text}
+            onChange={this.props.onTextChange}
           />
           <p className='FieldHelp'>These placeholders can be used: <code>%completed%</code>, <code>%target%</code>, <code>%remaining%</code> </p>
         </div>
@@ -95,9 +101,9 @@ export class Counter extends Component {
             ? this.renderEdit()
             : null
         }
-        <Preview showBar={ this.props.isSelected }>
+        <Preview showBar={this.props.isSelected}>
           <ServerSideRender
-            block={ 'planet4-blocks/counter' }
+            block={'planet4-blocks/counter'}
             attributes={{
               title: this.props.title,
               description: this.props.description,
