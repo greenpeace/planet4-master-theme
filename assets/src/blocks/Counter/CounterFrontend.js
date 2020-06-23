69554bc341d1f571bca18c38e51a128ce634a504
Maud Leray <maudleray035@gmail.com>
2020-06-23 15:06:10 +0200

    PLANET-4799 Update CounterFrontend to work properly with completed_api

--- assets/src/blocks/Counter/CounterFrontend.js
+++ assets/src/blocks/Counter/CounterFrontend.js
@@ -1,34 +1,82 @@
 import { Component, Fragment } from '@wordpress/element';
 
 export class CounterFrontend extends Component {
-  render() {
-    const {
-      className,
-      title,
-      description,
-      text
-    } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      remaining: 0,
+      completed: 0
+    };
 
-    // Calculate percent and remaining values depending on completed and target props
-    let { target, completed } = this.props;
-    let remaining = 0;
-    let percent = 0;
-    if (target === undefined) target = 0;
-    if (completed === undefined) completed = 0;
-    if (target > 0 || completed > 0) {
-      remaining = target - completed;
-      percent = (completed / target) * 100;
+    this.calculateRemaining = this.calculateRemaining.bind(this);
+    this.getCounterText = this.getCounterText.bind(this);
+  }
+
+  componentDidMount() {
+    // Calculate completed and remaining values depending on props
+    this.calculateRemaining();
+  }
+
+  componentDidUpdate({ target: prevTarget, completed: prevCompleted, completed_api: prevCompletedApi }) {
+    // Update completed and remaining values depending on props
+    const { target, completed, completed_api } = this.props;
+    if (target !== prevTarget || completed !== prevCompleted || completed_api !== prevCompletedApi) {
+      this.calculateRemaining();
     }
+  }
 
-    let style = 'plain';
-    if (className) style = className.split('is-style-')[1];
-    let arcLength = 31.5;
+  calculateRemaining() {
+    const { completed_api } = this.props;
+    const target = this.props.target || 0;
+    let completed = this.props.completed || 0;
+    let remaining = 0;
+    if (completed_api) {
+      fetch(completed_api)
+        .then(response => response.json())
+        .then(({ unique_count }) => {
+          if (unique_count) {
+            completed = unique_count;
+            this.setState({
+              completed,
+              remaining: target - completed
+            });
+          }
+        });
+    } else if (target > 0 || completed > 0) {
+      remaining = target - completed;
+      this.setState({ remaining, completed });
+    }
+  }
+
+  getCounterText() {
+    const { text, target } = this.props;
+    const { remaining, completed } = this.state;
 
     const COUNTER_TEXT = {
       '%completed%': `<span class="counter-target">${completed}</span>`,
       '%target%': `<span class="counter-target">${target}</span>`,
       '%remaining%': `<span class="counter-target">${remaining}</span>`
     };
+
+    return text.replace(/%completed%|%target%|%remaining%/gi, match => COUNTER_TEXT[match]);
+  }
+
+  render() {
+    const {
+      className,
+      title,
+      description,
+      text,
+      target
+    } = this.props;
+
+    const { completed } = this.state;
+
+    let style = 'plain';
+    if (className) style = className.split('is-style-')[1];
+    let arcLength = 31.5;
+    
+    const percent = target > 0 ? Math.round(completed / target * 100) : 0;
 
     return (
       <Fragment>
@@ -60,7 +108,7 @@ export class CounterFrontend extends Component {
             {text &&
               <div
                 className={`counter-text ${100 <= percent ? 'counter-text-goal_reached' : ''}`}
-                dangerouslySetInnerHTML={{ __html: text.replace(/%completed%|%target%|%remaining%/gi, match => COUNTER_TEXT[match]) }}
+                dangerouslySetInnerHTML={{ __html: this.getCounterText() }}
               />
             }
           </div>
