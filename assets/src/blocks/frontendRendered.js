0fd995879ea18e3c877e1bcfb24b6073d7fbb9f9
Pablo Cubico <pablocuadrado@gmail.com>
2020-07-05 17:49:06 -0300

    Make the frontendRendered definition a bit more verbose.

--- assets/src/blocks/frontendRendered.js
+++ assets/src/blocks/frontendRendered.js
@@ -4,15 +4,21 @@ import { FrontendBlockNode } from '../components/FrontendBlockNode/FrontendBlock
  * This function is used in the `save()` method of `registerBlock` to
  * render React blocks in the frontend.
  *
+ * It returns a function with the same arguments as the `save()`
+ * method.
+ *
  * Be careful! Making changes in this function or in the `FrontendBlockNode`
  * component could potentially cause block validation errors in Gutenberg.
  *
  * @param {string} block
  */
-export const frontendRendered = ( block ) => ( attributes, className ) => {
-  return <FrontendBlockNode
-    attributes={ attributes }
-    className={ className }
-    blockName={ block }
-  />;
+export const frontendRendered = ( block ) => {
+  return ( attributes, className ) => {
+    return <FrontendBlockNode
+      attributes={ attributes }
+      className={ className }
+      blockName={ block }
+    />;
+  }
 }
+
