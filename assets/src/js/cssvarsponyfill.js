76b9de64d11312410814590f24560de27118ef37
inwerpsel <pieter.vincent1988@gmail.com>
2020-10-06 21:17:53 +0200

    Check UA in js

--- assets/src/js/cssvarsponyfill.js
+++ assets/src/js/cssvarsponyfill.js
@@ -1,5 +1,17 @@
 /* global cssVars */
 export const setupCSSVarsPonyfill = () => {
+  // Taken from https://stackoverflow.com/questions/36653217/opera-mini-browser-detection-using-javascript.
+  const isOperaMini = Object.prototype.toString.call(window.operamini) === '[object OperaMini]';
+
+  if (isOperaMini) {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/css-vars-ponyfill/2.3.1/css-vars-ponyfill.min.js';
+    script.onload = () => cssVars();
+    document.head.append(script);
+
+    return;
+  }
+
   if ('undefined' !== typeof cssVars) {
     cssVars();
   }
