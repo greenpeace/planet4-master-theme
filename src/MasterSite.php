419c0d5169e2eb1c19916cdc055003e5ccb96cd2
Florent Hernandez <florent.hernandez@greenpeace.org>
2020-12-11 10:46:27 +0100

    PLANET-5654: WP_Block_Type_Registry::register was called incorrectly
    Ref: https://jira.greenpeace.org/browse/PLANET-5654
    
    `register_block_type('core/image')` is called twice, first by WP,
    then by us to add a render_callback. This is not allowed in WP>=5.5.
    
    Fix by hooking to the `register_block_type_args` which allows to
    modify block type args at register, including `render_callback`.

--- src/MasterSite.php
+++ src/MasterSite.php
@@ -195,7 +195,7 @@ class MasterSite extends TimberSite {
 		add_action( 'init', [ $this, 'login_redirect' ], 1 );
 		add_filter( 'attachment_fields_to_edit', [ $this, 'add_image_attachment_fields_to_edit' ], 10, 2 );
 		add_filter( 'attachment_fields_to_save', [ $this, 'add_image_attachment_fields_to_save' ], 10, 2 );
-		add_action( 'init', [ $this, 'p4_register_core_image_block' ] );
+		add_filter( 'register_block_type_args', [ $this, 'register_core_blocks_callback' ] );
 		add_action( 'admin_notices', [ $this, 'show_dashboard_notice' ] );
 		add_action( 'wp_ajax_dismiss_dashboard_notice', [ $this, 'dismiss_dashboard_notice' ] );
 
@@ -1122,12 +1122,17 @@ class MasterSite extends TimberSite {
 
 	/**
 	 * Add callback function to Gutenberg core/image block.
+	 *
+	 * @param array $args Parameters given during block register.
+	 *
+	 * @return array Parameters of the block.
 	 */
-	public function p4_register_core_image_block() {
-		register_block_type(
-			'core/image',
-			[ 'render_callback' => [ $this, 'p4_core_image_block_render' ] ]
-		);
+	public function register_core_blocks_callback( array $args ): array {
+		if ( 'core/image' === $args['name'] ) {
+			$args['render_callback'] = [ $this, 'p4_core_image_block_render' ];
+		}
+
+		return $args;
 	}
 
 	/**
