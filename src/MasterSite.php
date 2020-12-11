f5cb85bd2c2e1c56bac48eeea0c65771a6fbfb32
Florent Hernandez <florent.hernandez@greenpeace.org>
2020-12-11 12:31:37 +0100

    Apply fix on proper version only

--- src/MasterSite.php
+++ src/MasterSite.php
@@ -195,7 +195,9 @@ class MasterSite extends TimberSite {
 		add_action( 'init', [ $this, 'login_redirect' ], 1 );
 		add_filter( 'attachment_fields_to_edit', [ $this, 'add_image_attachment_fields_to_edit' ], 10, 2 );
 		add_filter( 'attachment_fields_to_save', [ $this, 'add_image_attachment_fields_to_save' ], 10, 2 );
-		add_filter( 'register_block_type_args', [ $this, 'register_core_blocks_callback' ] );
+		version_compare( get_bloginfo( 'version' ), '5.5', '<' )
+			? add_action( 'init', [ $this, 'p4_register_core_image_block' ] )
+			: add_filter( 'register_block_type_args', [ $this, 'register_core_blocks_callback' ] );
 		add_action( 'admin_notices', [ $this, 'show_dashboard_notice' ] );
 		add_action( 'wp_ajax_dismiss_dashboard_notice', [ $this, 'dismiss_dashboard_notice' ] );
 
@@ -1118,6 +1120,16 @@ class MasterSite extends TimberSite {
 		}
 
 		return $content;
+	}
+
+	/**
+	 * Add callback function to Gutenberg core/image block.
+	 */
+	public function p4_register_core_image_block() {
+		register_block_type(
+			'core/image',
+			[ 'render_callback' => [ $this, 'p4_core_image_block_render' ] ]
+		);
 	}
 
 	/**
