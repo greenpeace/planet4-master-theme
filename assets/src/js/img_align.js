e4e9fa58e8dc8263105966d231e2c706ef44b273
Nikos Roussos <nikos@roussos.cc>
2020-12-14 12:51:01 +0200

    PLANET-5759 Remove unused img_align script

--- assets/src/js/img_align.js
+++
@@ -1,8 +0,0 @@
-export const setupImageAlign = function($) {
-  'use strict';
-
-  $('div.wp-caption[class*="align"]').each( function() {
-    const imgwidth = $(this).find('img').attr('width');
-    $(this).css('cssText', 'width: ' + imgwidth +'px !important;');
-  });
-};
