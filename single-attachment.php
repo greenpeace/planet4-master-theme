72f30bb2ef2caf95174d617e13216e630a497fc8
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2021-01-28 12:25:38 +0530

    PLANET-5816 Add P4 attachment page

---
+++ single-attachment.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The Template for displaying all attachment posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+use P4\MasterTheme\Context;
+use P4\MasterTheme\Post;
+use Timber\Timber;
+
+// Initializing variables.
+$context = Timber::get_context();
+/**
+ * P4 Post Object
+ *
+ * @var Post $post
+ */
+$post                       = Timber::query_post( false, Post::class ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+$context['post']            = $post;
+$context['social_accounts'] = Post::filter_social_accounts( $context['footer_social_menu'] );
+
+Timber::render( [ 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ], $context );
