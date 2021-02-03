ef0c1ad9a269c75cec337e573443804e302abb64
inwerpsel <pieter.vincent1988@gmail.com>
2021-02-03 13:11:58 +0100

    PLANET-5666 Gallery 3 columns image sizes

---
+++ assets/src/blocks/Gallery/imageSizes.js
@@ -0,0 +1,15 @@
+export default {
+  threeColumns0: `
+  (min-width: 1200px) 500px,
+  (min-width: 780px) 400px,
+  (min-width: 580px) 246px,
+  200px`,
+  threeColumns1: `
+  (min-width: 580px) 600px,
+  200px`,
+  threeColumns2: `
+  (min-width: 1200px) 500px,
+  (min-width: 780px) 400px,
+  (min-width: 580px) 276px,
+  200px`,
+}
