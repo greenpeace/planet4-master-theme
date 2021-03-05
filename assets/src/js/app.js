1453107e0ef8f691b25e2fa440d3a68707a0c09c
inwerpsel <pieter.vincent1988@gmail.com>
2021-03-05 15:44:03 +0100

    Load icons from less deep level

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -14,7 +14,7 @@ function requireAll(r) {
   r.keys().forEach(r);
 }
 
-requireAll(require.context('../../../images/icons/', true, /\.svg$/));
+requireAll(require.context('../images/icons/', true, /\.svg$/));
 
 window.$ = $ || jQuery;
 
