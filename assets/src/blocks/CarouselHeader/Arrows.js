267623fe86c8f6d4c194322eb2a3e71bc6363211
Maud Leray <maudleray035@gmail.com>
2021-04-02 13:53:51 +0200

    PLANET-4809 Use button tags for CarouselHeader arrows

--- assets/src/blocks/CarouselHeader/Arrows.js
+++ assets/src/blocks/CarouselHeader/Arrows.js
@@ -1,12 +1,14 @@
+const { __ } = wp.i18n;
+
 export const Arrows = ({ goToPrevSlide, goToNextSlide }) => {
   return <>
-    <a onClick={goToPrevSlide} className="carousel-control-prev" on="#carousel-wrapper-header" role="button">
-      <span className="carousel-control-prev-icon" aria-hidden="true"><i></i></span>
-      <span className="visually-hidden">Prev</span>
-    </a>
-    <a onClick={goToNextSlide} className="carousel-control-next" href="#carousel-wrapper-header" role="button">
-      <span className="carousel-control-next-icon" aria-hidden="true"><i></i></span>
-      <span className="visually-hidden">Next</span>
-    </a>
+    <button className='carousel-control-prev' role='button' onClick={goToPrevSlide}>
+      <span className='carousel-control-prev-icon' aria-hidden='true'><i></i></span>
+      <span className='visually-hidden'>{__('Previous', 'planet4-blocks')}</span>
+    </button>
+    <button className='carousel-control-next' role='button' onClick={goToNextSlide}>
+      <span className='carousel-control-next-icon' aria-hidden='true'><i></i></span>
+      <span className='visually-hidden'>{__('Next', 'planet4-blocks')}</span>
+    </button>
   </>;
 }
