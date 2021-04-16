sg7801/planet4-plugin-gutenberg-blocks

PLANET-5523 Remove Take Action and Campaign Covers block styles in campaigns

admin/js/editor.js
@@ -1,19 +1,28 @@
 /* global wp */
+const { registerBlockStyle, unregisterBlockStyle } = wp.blocks;
+const { __ } = wp.i18n;
 
 wp.domReady(() => {
-  wp.blocks.registerBlockStyle( 'core/button', {
+  registerBlockStyle('core/button', {
     name: 'secondary',
     label: 'Secondary',
     isDefault: true
   });
-  wp.blocks.registerBlockStyle( 'core/button', {
+  registerBlockStyle('core/button', {
     name: 'cta',
     label: 'CTA'
   });
-  wp.blocks.registerBlockStyle( 'core/button', {
+  registerBlockStyle('core/button', {
     name: 'donate',
     label: 'Donate'
   });
-  wp.blocks.unregisterBlockStyle('core/button', 'outline');
-  wp.blocks.unregisterBlockStyle('core/button', 'fill');
+  unregisterBlockStyle('core/button', 'outline');
+  unregisterBlockStyle('core/button', 'fill');
+
+  // Remove Take Action and Campaign covers styles for Covers block in campaigns
+  const postType = document.querySelector('form.metabox-base-form input#post_type').value;
+  if (postType === 'campaign') {
+    unregisterBlockStyle('planet4-blocks/covers', 'take-action');
+    unregisterBlockStyle('planet4-blocks/covers', 'campaign');
+  }
 });

greenpeace/planet4-master-theme

admin/js/editor.js
@@ -1,7 +1,15 @@
 /* global wp */
+const { unregisterBlockStyle } = wp.blocks;
 
 wp.domReady(() => {
-  // Image block styles
-  wp.blocks.unregisterBlockStyle('core/image', 'rounded');
-  wp.blocks.unregisterBlockStyle('core/image', 'default');
+  // Remove Image block styles
+  unregisterBlockStyle('core/image', 'rounded');
+  unregisterBlockStyle('core/image', 'default');
+
+  // Remove Take Action and Campaign covers styles for Covers block in campaigns
+  const postType = document.querySelector('form.metabox-base-form input#post_type').value;
+  if (postType === 'campaign') {
+    unregisterBlockStyle('planet4-blocks/covers', 'take-action');
+    unregisterBlockStyle('planet4-blocks/covers', 'campaign');
+  }
 });
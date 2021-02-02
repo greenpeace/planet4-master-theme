36189d7654e6d592b7f4581f79039c9a9cef0652
Maud Leray <maudleray035@gmail.com>
2021-02-02 09:49:47 +0100

    PLANET-5523 Remove Take Action and Campaign Covers block styles in campaigns

--- admin/js/editor.js
+++ admin/js/editor.js
@@ -1,7 +1,15 @@
 /* global wp */
+const { unregisterBlockStyle } = wp.blocks;
 
 wp.domReady(() => {
-  // Image block styles
-  wp.blocks.unregisterBlockStyle('core/image', 'rounded');
-  wp.blocks.unregisterBlockStyle('core/image', 'default');
+  // Remove Image block styles
+  unregisterBlockStyle('core/image', 'rounded');
+  unregisterBlockStyle('core/image', 'default');
+
+  // Remove Take Action and Campaign covers styles for Covers block in campaigns
+  const postType = document.querySelector('form.metabox-base-form input#post_type').value;
+  if (postType === 'campaign') {
+    unregisterBlockStyle('planet4-blocks/covers', 'take-action');
+    unregisterBlockStyle('planet4-blocks/covers', 'campaign');
+  }
 });
