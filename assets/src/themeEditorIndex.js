284cf4314125655e6e934f5e9c38e89601e9443d
inwerpsel <pieter.vincent1988@gmail.com>
2021-05-04 17:52:41 +0200

    Use theme editor from separate package

--- assets/src/themeEditorIndex.js
+++ assets/src/themeEditorIndex.js
@@ -1,4 +1,4 @@
-import { setupThemeEditor } from './theme/initializeThemeEditor';
+import { setupThemeEditor } from 'use-theme-editor/build/initializeThemeEditor';
 import { p4ServerThemes } from './theme/p4ServerThemes';
 
 document.addEventListener('DOMContentLoaded', async () => {
