f4d189e117dd2691c9bba071ba60be071fea664f
inwerpsel <pieter.vincent1988@gmail.com>
2021-05-04 17:36:58 +0200

    Remove theme code that moved to other repo

--- assets/src/theme/highlight.js
+++
@@ -1,8 +0,0 @@
-const HIGHLIGHT_CLASS = 'theme-editor-highlight';
-export const addHighlight = element => !!element && element.classList.add(HIGHLIGHT_CLASS);
-export const removeHighlight = element => !!element && element.classList.remove(HIGHLIGHT_CLASS);
-
-export const whileHoverHighlight = element => ({
-  onMouseEnter: () => addHighlight(element),
-  onMouseLeave: () => removeHighlight(element),
-});
--- assets/src/theme/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback} from 'react';
-
-export const useToggle = (initialState = false) => {
-  // Initialize the state
-  const [state, setState] = useState(initialState);
-  const toggle = useCallback(() => setState(state => !state), []);
-
-  return [state, toggle]
-}
