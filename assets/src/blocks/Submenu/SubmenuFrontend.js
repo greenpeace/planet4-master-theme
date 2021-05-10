ce58e876c2e0946039491617f0fcbe08e3fb07f3
Maud Leray <maudleray035@gmail.com>
2021-05-10 15:21:12 +0200

    PLANET-6002 Remove Submenu back to top button

--- assets/src/blocks/Submenu/SubmenuFrontend.js
+++ assets/src/blocks/Submenu/SubmenuFrontend.js
@@ -1,27 +1,9 @@
-import { useEffect } from '@wordpress/element';
 import { getSubmenuStyle } from './getSubmenuStyle';
 import { SubmenuItems } from './SubmenuItems';
 import { makeHierarchical } from './makeHierarchical';
 import { getHeadingsFromDom } from './getHeadingsFromDom';
 
 export const SubmenuFrontend = ({ title, className, levels, submenu_style }) => {
-
-  const enableBackTop = () => {
-    const backTop = document.querySelector('.back-top');
-    if (!backTop) {
-      return;
-    }
-    document.addEventListener('scroll', () => {
-      if (document.documentElement.scrollTop > 400) {
-        backTop.classList.remove('d-none');
-      } else {
-        backTop.classList.add('d-none');
-      }
-    });
-  };
-
-  // Enable back top on initial render.
-  useEffect(enableBackTop, []);
 
   const headings = getHeadingsFromDom(levels);
   const menuItems = makeHierarchical(headings);
