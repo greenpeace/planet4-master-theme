a5a4ee3e37e91ea454a21ed09f167d463201f82e
Keevan <danceparty@github.com>
2021-05-10 16:08:34 -0700

    fix issues with displaying bullet unnecessarily

--- assets/src/blocks/Articles/ArticlePreview.js
+++ assets/src/blocks/Articles/ArticlePreview.js
@@ -41,16 +41,13 @@ export class ArticlePreview extends Component {
 
     if (authorName) {
       return (
-        <>
-          <span className="article-list-item-author">
-            {!isLink ?
-              authorName
-              :
-              <a href={authorLink}>{authorName}</a>
-            }
-          </span>
-          <span className="article-list-item-bullet" aria-hidden="true">&#8226;</span>
-        </>
+        <span className="article-list-item-author">
+          {!isLink ?
+            authorName
+            :
+            <a href={authorLink}>{authorName}</a>
+          }
+        </span>
       );
     }
   }
@@ -110,6 +107,7 @@ export class ArticlePreview extends Component {
     } = this.props;
 
     const articleClassName = tags.reduce((classname, tag) => classname + ` tag-${tag.slug}`, 'article-list-item');
+    const authorLink = this.getAuthorLink()
 
     return (
       <article className={articleClassName} >
@@ -121,7 +119,9 @@ export class ArticlePreview extends Component {
                 this.getPageTypesTags(page_type, page_type_link)
               }
 
-              <span className="tag-wrap-bullet" aria-hidden="true">&#8226;</span>
+              {(tags.length > 0 && page_type) && (
+                <span className="tag-wrap-bullet" aria-hidden="true">&#8226;</span>
+              )}
 
               {tags.length > 0 &&
                 <div className="tag-wrap tags">
@@ -155,7 +155,12 @@ export class ArticlePreview extends Component {
               </h4>
             }
             <p className="article-list-item-meta">
-              {this.getAuthorLink()}
+              {authorLink}
+
+              {(authorLink && date_formatted) && (
+                <span className="article-list-item-bullet" aria-hidden="true">&#8226;</span>
+              )}
+
               {date_formatted &&
               <time className="article-list-item-date" dateTime="">
                 {date_formatted}
