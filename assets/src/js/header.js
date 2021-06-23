8e9a467a31ae887b0059c240205981f6e665e76e
Himanshu <addyjeridiq@gmail.com>
2021-06-23 16:53:35 +0530

    PLANET-5758 Remove jQuery from header.js
    Ref: https://jira.greenpeace.org/browse/PLANET-5758
    
    ---
    
    Migrated jquery code to javascript

--- assets/src/js/header.js
+++ assets/src/js/header.js
@@ -1,7 +1,7 @@
-export const setupHeader = function($) {
+export const setupHeader = function() {
   'use strict';
 
-  $(document).on('click', [
+  document.querySelector(document).addEventListener('click', [
     '.navbar-dropdown-toggle',
     '.country-dropdown-toggle',
     '.navbar-search-toggle',
@@ -9,79 +9,79 @@ export const setupHeader = function($) {
     evt.preventDefault();
     evt.stopPropagation();
 
-    const $button = $(this);
-    const target = $button.data('bs-target');
+    const button = document.querySelector(this);
+    const target = button.data('bs-target');
     if (!target) {
       throw new Error('Missing `data-bs-target` attribute: specify the container to be toggled');
     }
-    const toggleClass = $button.data('bs-toggle');
+    const toggleClass = button.data('bs-toggle');
     if (!toggleClass) {
       throw new Error('Missing `data-bs-toggle` attribute: specify the class to toggle');
     }
 
     // Toggle visibility of the target specified via data-bs-target.
-    $(target).toggleClass(toggleClass);
-    $(this).toggleClass(toggleClass);
+    document.querySelector(target).classList.toggle(toggleClass);
+    document.querySelector(this).classList.toggle(toggleClass);
 
     // Toggle aria-expanded attribute.
-    $button.attr('aria-expanded', function(i, attr) {
+    button.attr('aria-expanded', function(i, attr) {
       return attr === 'false' ? 'true' : 'false';
     });
 
     // Toggle data-ga-action attribute used in GTM tracking.
-    $('.country-dropdown-toggle').attr( 'data-ga-action', $('.country-dropdown-toggle').attr('aria-expanded') === 'false' ? 'Open Country Selector' : 'Close Country Selector' );
-    $('.navbar-search-toggle').attr( 'data-ga-action', $('.navbar-search-toggle').attr('aria-expanded') === 'false' ? 'Open Search' : 'Close Search' );
+    document.querySelector('.country-dropdown-toggle').attr( 'data-ga-action', document.querySelector('.country-dropdown-toggle').attr('aria-expanded') === 'false' ? 'Open Country Selector' : 'Close Country Selector' );
+    document.querySelector('.navbar-search-toggle').attr( 'data-ga-action', document.querySelector('.navbar-search-toggle').attr('aria-expanded') === 'false' ? 'Open Search' : 'Close Search' );
   });
 
   // Close all menus when clicking somewhere else
-  $(document).on('click', function closeInactiveMenus(evt) {
+  document.querySelector(document).addEventListener('click', function closeInactiveMenus(evt) {
     const clickedElement = evt.target;
-    $('button[aria-expanded="true"]').each(function(i, button) {
-      const $button = $(button);
-      const buttonTarget = $($button.data('bs-target')).get( 0 );
-      if (buttonTarget && ! $.contains(buttonTarget, clickedElement)) {
+    document.querySelector('button[aria-expanded="true"]').each(function(i, button) {
+      const button = document.querySelector(button);
+      const buttonTarget = document.querySelector(button.data('bs-target')).get( 0 );
+      if (buttonTarget && ! .contains(buttonTarget, clickedElement)) {
         // Spoof a click on the open menu's toggle to close that menu.
-        $button.trigger('click');
+        button.trigger('click');
       }
     });
   });
 
   // Close all menus on escape pressed
-  $(document).bind('keyup', function(event){
+  document.querySelector(document).bind('keyup', function(event){
     if (event.which === 27) {
-      $(document).trigger('click');
+      document.querySelector(document).trigger('click');
     }
   });
 
   //Close the menu if the user clicks the dedicated dropdown close button.
-  $(document).on('click', '.close-navbar-dropdown', function (evt) {
+  document.querySelector(document).addEventListener('click', '.close-navbar-dropdown', function (evt) {
     evt.preventDefault();
     // Proxy to the main navbar close button
-    $('.navbar-dropdown-toggle').trigger('click');
+    document.querySelector('.navbar-dropdown-toggle').trigger('click');
   });
 
   // Hide Header on on scroll down
   function hasScrolled(lastScrollTop, delta, navbarHeight) {
-    const st = $(this).scrollTop();
+    const st = document.querySelector(this).scrollTop;
     if (Math.abs(lastScrollTop - st) <= delta) {
       return;
     }
     if (st > lastScrollTop && st > navbarHeight){
-      $('.top-navigation').removeClass('nav-down').addClass('nav-up');
+      document.querySelector('.top-navigation').removeClass('nav-down').classList.add('nav-up');
     } else {
-      if(st + $(window).height() < $(document).height()) {
-        $('.top-navigation').removeClass('nav-up').addClass('nav-down');
+      if(st + document.querySelector(window).height() < document.querySelector(document).height()) {
+        document.querySelector('.top-navigation').removeClass('nav-up').classList.add('nav-down');
       }
     }
     lastScrollTop = st;
   }
 
-  if($( window ).width() <= 768) {
+  if(document.querySelector( window ).width() <= 768) {
     let didScroll;
     let lastScrollTop = 0;
     const delta = 5;
-    const navbarHeight = $('.top-navigation').outerHeight();
-    $(window).scroll(function(){
+    const navbarHeight = document.querySelector('.top-navigation').outerHeight();
+    document.querySelector(window).scroll(function(){
       didScroll = true;
     });
     setInterval(function() {
@@ -91,35 +91,35 @@ export const setupHeader = function($) {
       }
     }, 250);
 
-    const $slider = $('.mobile-menus');
-    $(document).click(function() {
-      if($('.menu').hasClass('active')){
+    const slider = document.querySelector('.mobile-menus');
+    document.querySelector(document).click(function() {
+      if(document.querySelector('.menu').classList.contains('active')){
         //Hide the menus if visible
-        $slider.animate({
-          left: parseInt($slider.css('left'),10) == 0 ? -320 : 0
+        slider.animate({
+          left: parseInt(slider.css('left'),10) == 0 ? -320 : 0
         });
-        $('.menu').removeClass('active');
+        document.querySelector('.menu').removeClass('active');
       }
-      if($('.search-box').hasClass('active')){
+      if(document.querySelector('.search-box').classList.contains('active')){
         // Hide the search if visible
-        $searchBox.slideToggle().toggleClass('active');
+        searchBox.slideToggle().classList.toggle('active');
       }
     });
 
-    $('.menu').click(function() {
+    document.querySelector('.menu').click(function() {
       event.stopPropagation();
-      $(this).toggleClass('active');
-      $slider.animate({
-        left: parseInt($slider.css('left'),10) == -320 ? 0 : -320
+      document.querySelector(this).classList.toggle('active');
+      slider.animate({
+        left: parseInt(slider.css('left'),10) == -320 ? 0 : -320
       });
     });
 
-    const $searchBox = $('#search .search-box');
-    const $searchTrigger = $('#search-trigger');
+    const searchBox = document.querySelector('#search .search-box');
+    const searchTrigger = document.querySelector('#search-trigger');
 
-    $searchTrigger.on('click', function(event) {
+    searchTrigger.addEventListener('click', function(event) {
       event.stopPropagation();
-      $searchBox.slideToggle().toggleClass('active');
+      searchBox.slideToggle().classList.toggle('active');
     });
   }
 };
