892b10537e4cddb6082ce0163e8e388e2d24adfb
inwerpsel <pieter.vincent1988@gmail.com>
2021-09-03 12:00:29 +0200

    Allow re-ordering slides
    * Use drag listeners to insert the dragged slide before the slide the
    drag ends on.
    * While dragging add a div at the end, so that dropping will always
    insert before the drop target.
    * Add the slide title and image to the draggable panel title.
    * Add top border to visualize drop target behavior.
    * Accessibility concerns: drag and drop only work with mouse or
    touchpad. We should consider buttons instead or besides d&d that move
    the slide up or down.
    
    Prevent flickering of drop target
    
    * Set draggable to false on the image, so that the shown element is
    always the entire slide section.
    * Add a timeout before ending the drop target state, to prevent
    flickering when events fire for descendants.
    * Remove some console.logs.
    
    Add placeholder in case slide has no title
    
    * We may also want to add the index.
    
    Provide small margin for slide image
    
    * Maybe later we can have a better alignment.
    * Reduce transition timing to .01s to avoid jumpiness.

--- assets/src/blocks/CarouselHeader/Sidebar.js
+++ assets/src/blocks/CarouselHeader/Sidebar.js
@@ -3,11 +3,29 @@ import {
   PanelBody,
   PanelRow,
 } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
 
 import { InspectorControls } from '@wordpress/block-editor';
 import { SidebarSlide } from './SidebarSlide';
+import { useState, useRef } from 'react';
 
 const { __ } = wp.i18n;
+
+const reOrderSlides = (slides, oldIndex, newIndex) => {
+  const movingSlide = slides[oldIndex];
+  return slides.reduce((orderedSlides, slide, index) => {
+    if (index === newIndex) {
+      orderedSlides.push(movingSlide);
+    }
+    if (index !== oldIndex) {
+      orderedSlides.push(slide);
+    }
+    if (index === slides.length -1 && newIndex > index) {
+      orderedSlides.push(movingSlide);
+    }
+    return orderedSlides;
+  }, []);
+}
 
 export const Sidebar = ({
   carouselAutoplay,
@@ -16,8 +34,12 @@ export const Sidebar = ({
   currentSlide,
   changeSlideAttribute,
   goToSlide,
-}) => (
-  <InspectorControls>
+}) => {
+  const [dragTarget, setDragTarget] = useState(null);
+  const [draggedSlide, setDraggedSlide] = useState(null);
+  const timeoutRef = useRef(null);
+
+  return <InspectorControls>
     <PanelBody title={__('Setting', 'planet4-blocks-backend')}>
       <CheckboxControl
         label={__('Carousel Autoplay', 'planet4-blocks-backend')}
@@ -27,13 +49,58 @@ export const Sidebar = ({
         onChange={value => setAttributes({ carousel_autoplay: value })}
       />
     </PanelBody>
-    {slides.map((slide, index) => (
+    { slides.map((slide, index) => <div
+      style={ {
+        // When you start dragging an element, what is shown while dragging is a bitmap snapshot of what the element
+        // looked like the moment the drag started. The .01s delay should be plenty to ensure that snapshot is taken
+        // before we hide the element.
+        transform: index === draggedSlide ? 'scaleY(0)' : 'none',
+        transitionDelay: '.01s',
+        transitionProperty: 'transform',
+      } }
+      className={ index === dragTarget ? 'carousel-sidebar-insert-before' : 'carousel-sidebar-slide' }
+      key={ `${ slide.image_srcset }${ index }` }
+      draggable
+      onDragOver={ () => {
+        timeoutRef.current && window.clearTimeout(timeoutRef.current);
+        setDragTarget(index);
+      } }
+      onDragLeave={ () => {
+        // The event fires also when going over descendant elements, then immediately fires on the parent again.
+        // Without this timeout it continuously set and unset the drag target.
+        timeoutRef.current = window.setTimeout(() => {
+          setDragTarget(null);
+        }, 100);
+      } }
+      onDragStart={ () => {
+        setDraggedSlide(index);
+      } }
+      onDragEnd={ () => {
+        setDraggedSlide(null);
+        if (dragTarget === null) {
+          return;
+        }
+        setAttributes({ slides: reOrderSlides(slides, index, dragTarget) });
+        setDragTarget(null);
+      } }
+    >
       <PanelBody
-        key={index}
-        title={`${__('Slide', 'planet4-blocks-backend')} ${index + 1}`}
-        opened={currentSlide === index}
-        onToggle={() => {
-          if (currentSlide !== index) {
+        key={ index }
+        title={
+          <Fragment>
+            <img
+              draggable={ false }
+              srcSet={ slide.image_srcset }
+              height={ 50 }
+              alt={ slide.image_alt }
+              style={ { marginRight: '8px', maxHeight: '50px' } }
+            />
+            <span>{ slide.header || <i>{__('No title', 'planet4-blocks-backend')}</i> }</span>
+          </Fragment>
+        }
+        initialOpen={ false }
+        onToggle={ (isOpened) => {
+          if (isOpened) {
             goToSlide(index);
           }
         }}
@@ -47,6 +114,11 @@ export const Sidebar = ({
           />
         </PanelRow>
       </PanelBody>
-    ))}
-  </InspectorControls>
-);
+    </div>) }
+    { draggedSlide !== null && draggedSlide !== slides.length - 1 && <div
+      className={slides.length === dragTarget ? 'carousel-sidebar-insert-before' : ''}
+      onDragOver={ () => setDragTarget(slides.length) }
+      onDragLeave={ () => setDragTarget(null)}
+      style={{height: '80px'}}/> }
+  </InspectorControls>;
+};
