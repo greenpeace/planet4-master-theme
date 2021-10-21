83ec506e238e4d5f1b7b7e5446d322ca022ae9ba
Maud Leray <maudleray035@gmail.com>
2021-10-21 15:50:35 +0200

    PLANET-5799 Remove PDF link icon from Take Action boxout title and overlay
    We do not want to show icons in titles. For the boxout we already didn't show the external link one, but the PDF link icon could still appear, also for the overlay.

--- assets/src/js/pdf_icon.js
+++ assets/src/js/pdf_icon.js
@@ -1,13 +1,15 @@
 // Add pdf icon to pdf links
 export const setupPDFIcon = () => {
-  const links = [...document.querySelectorAll('a[href*=".pdf"]:not(.search-result-item-headline)')];
+  const links = [...document.querySelectorAll('a[href*=".pdf"]:not(.search-result-item-headline):not(.cover-card-heading)')];
 
   links.forEach(link => {
     // We don't want to show the icon in headings/titles,
     // or in links that are images
+    const text = link.textContent || link.innerText;
     if (
       ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(link.parentElement.nodeName)
       || link.querySelectorAll('img').length > 0
+      || text.trim().length === 0
     ) {
       return;
     }
