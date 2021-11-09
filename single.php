7df6c02c4808d9eb94ddaaed15c0c2b091f20e95
inwerpsel <pieter.vincent1988@gmail.com>
2021-11-09 17:25:13 +0100

    Keep boxout in place, output meta at end of content
    * Don't regex remove it.
    * Simplify the check before using meta, so it only does has_block.

--- single.php
+++ single.php
@@ -23,17 +23,6 @@ $context = Timber::get_context();
 $post            = Timber::query_post( false, Post::class ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 $context['post'] = $post;
 
-// Strip Take Action Boxout block from the post content to add outside the normal block container.
-if ( false !== strpos( $post->post_content, '<!-- wp:planet4-blocks/take-action-boxout' ) ) {
-
-	$take_action_boxout_block_start  = strpos( $post->post_content, '<!-- wp:planet4-blocks/take-action-boxout' );
-	$take_action_boxout_block_end    = strpos( $post->post_content, '/-->', $take_action_boxout_block_start ) + 3;
-	$take_action_boxout_block_length = $take_action_boxout_block_end - $take_action_boxout_block_start + 1;
-	$take_action_boxout_block        = substr( $post->post_content, $take_action_boxout_block_start, $take_action_boxout_block_length );
-
-	$post->post_content = str_replace( $take_action_boxout_block, '', $post->post_content );
-}
-
 // Set Navigation Issues links.
 $post->set_issues_links();
 
@@ -88,11 +77,7 @@ if ( 'yes' === $post->include_articles ) {
 	$post->articles = '<!-- wp:planet4-blocks/articles ' . wp_json_encode( $block_attributes, JSON_UNESCAPED_SLASHES ) . ' /-->';
 }
 
-// Build the shortcode for take action boxout block
-// Break the content to retrieve first 2 paragraphs and split the content if the take action page has been defined.
-if ( isset( $take_action_boxout_block ) ) {
-	$post->take_action_boxout = $take_action_boxout_block;
-} elseif ( ! empty( $take_action_page ) ) {
+if ( ! empty( $take_action_page ) && ! has_block( 'planet4-blocks/take-action-boxout' ) ) {
 	$post->take_action_page = $take_action_page;
 
 	$block_attributes = [
