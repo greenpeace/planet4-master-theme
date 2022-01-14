13b8f6a01753ed13625f759a765489a6ee2580dd
Dan Tovbein <dtovbein@gmail.com>
2022-01-14 10:54:17 -0300

    Implement reusable Sharebutton
    - Replace the Whatsapp, Facebook, Email and Twiter share button

---
+++ assets/src/blocks/ShareButtons/ShareButton.js
@@ -0,0 +1,29 @@
+const { __ } = wp.i18n;
+
+export const ShareButton = ({
+  href = '',
+  providerName = '',
+  icon = '',
+  hiddenText = '',
+  gaEvent,
+  gaEventCategory,
+  gaCategory,
+  gaAction,
+  gaLabel,
+  openInNewTab,
+}) => (
+  <a href={href}
+    className={`social-share-button ${providerName}`}
+    data-ga-event={gaEvent}
+    data-ga-event-category={gaEventCategory}
+    data-ga-category={gaCategory}
+    data-ga-action={gaAction}
+    data-ga-label={gaLabel}
+    { ...openInNewTab && { target: '_blank' } }
+  >
+    <svg viewBox="0 0 32 32" className='icon'>
+      <use xlinkHref={ `${window.p4bk_vars.themeUrl}/assets/build/sprite.symbol.svg#${icon}` } />
+    </svg>
+    <span className="visually-hidden">{__( hiddenText, 'planet4-master-theme' )}</span>
+  </a>
+);
