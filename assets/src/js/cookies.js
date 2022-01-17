3c80804879db0562d2aee09004937ea232ab751e
Maud Leray <maudleray035@gmail.com>
2022-01-17 13:34:01 +0100

    PLANET-6075 Add settings to Cookies box
    This is to make it easier for the site visitors to see which cookies we use and which ones they want to allow. The copy for these settings is defined per site in the backend, in Planet 4 > Cookies.

--- assets/src/js/cookies.js
+++ assets/src/js/cookies.js
@@ -4,9 +4,28 @@ export const setupCookies = () => {
   const ENABLE_ANALYTICAL_COOKIES = window.p4bk_vars.enable_analytical_cookies;
   const ENABLE_GOOGLE_CONSENT_MODE = window.p4bk_vars.enable_google_consent_mode;
 
+  // Possible cookie values
+  const ONLY_NECESSARY = '1';
+  const ALL_COOKIES = '2';
+  const NECESSARY_ANALYTICAL_MARKETING = '4';
+
   function gtag() {
     dataLayer.push(arguments);
   }
+
+  const updateGoogleConsent = (key, granted) => {
+    if (!ENABLE_GOOGLE_CONSENT_MODE) {
+      return;
+    }
+
+    gtag('consent', 'update', {
+      [key]: granted ? 'granted' : 'denied',
+    });
+    dataLayer.push({
+      'event': 'updateConsent',
+      [key]: granted ? 'granted' : 'denied',
+    });
+  };
 
   // If Google Consent Mode is enabled, set default ad storage and analytics storage to 'denied' if needed
   if (ENABLE_GOOGLE_CONSENT_MODE) {
@@ -25,7 +44,7 @@ export const setupCookies = () => {
     }
   }
 
-  window.createCookie = (name, value, days) => {
+  const createCookie = (name, value, days) => {
     let date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     const secureMode = document.location.protocol === 'http:'
@@ -34,7 +53,7 @@ export const setupCookies = () => {
     document.cookie = encodeURI(name) + '=' + encodeURI(value) + ';domain=.' + document.domain + ';path=/;' + '; expires=' + date.toGMTString() + secureMode;
   };
 
-  window.readCookie = name => {
+  const readCookie = name => {
     const nameEQ = name + '=';
     const ca = document.cookie.split(';');
     let c;
@@ -47,58 +66,98 @@ export const setupCookies = () => {
     return null;
   };
 
-  const cookie = window.readCookie('active_consent_choice');
-  const cookieElement = document.querySelector('#set-cookie');
+  const cookie = readCookie('active_consent_choice');
+  const cookiesBox = document.querySelector('#set-cookie');
   const nro = document.body.dataset.nro;
 
   if (cookie === null) {
-    if (cookieElement) {
-      cookieElement.classList.add('shown');
+    if (cookiesBox) {
+      cookiesBox.classList.add('shown');
     }
   } else {
-    window.createCookie('gp_nro', nro, 30);
+    createCookie('gp_nro', nro, 30);
   }
 
-  const hideCookieButton = document.querySelector('#hidecookie');
-  if (hideCookieButton) {
-    hideCookieButton.onclick = () => {
-      window.createCookie('active_consent_choice', '1', 365);
-      const newCookieValue = ENABLE_ANALYTICAL_COOKIES ? '4' : '2';
-      window.createCookie('greenpeace', newCookieValue, 365);
+  const allowAllCookies = () => {
+    createCookie('active_consent_choice', '1', 365);
+    const newCookieValue = ENABLE_ANALYTICAL_COOKIES ? NECESSARY_ANALYTICAL_MARKETING : ALL_COOKIES;
+    createCookie('greenpeace', newCookieValue, 365);
 
-      // Remove the 'no_track' cookie, if user accept the cookies consent.
-      window.createCookie('no_track', '0', -1);
+    // Remove the 'no_track' cookie, if user accept the cookies consent.
+    createCookie('no_track', '0', -1);
 
-      // Create cookie to store last visited nro.
-      window.createCookie('gp_nro', nro, 30);
+    // Create cookie to store last visited nro.
+    createCookie('gp_nro', nro, 30);
 
-      // Grant ad storage and analytics storage if Google Consent Mode is enabled.
-      if (ENABLE_GOOGLE_CONSENT_MODE) {
-        gtag('consent', 'update', {
-          'ad_storage': 'granted',
-          ...ENABLE_ANALYTICAL_COOKIES && { 'analytics_storage': 'granted' },
-        });
-        dataLayer.push({
-          'event' : 'updateConsent',
-          'ad_storage': 'granted',
-          ...ENABLE_ANALYTICAL_COOKIES && { 'analytics_storage': 'granted' },
-        });
+    // Grant ad storage and analytics storage if Google Consent Mode is enabled.
+    updateGoogleConsent('ad_storage', true);
+    if (ENABLE_ANALYTICAL_COOKIES) {
+      updateGoogleConsent('analytics_storage', true);
+    }
+
+    // DataLayer push event on cookies consent.
+    dataLayer.push({
+      'event' : 'cookiesConsent'
+    });
+
+    cookiesBox.classList.remove('shown');
+  };
+
+  const allowAllCookiesButtons = [...document.querySelectorAll('.allow-all-cookies')];
+  allowAllCookiesButtons.forEach(allowAllCookiesButton => allowAllCookiesButton.onclick = allowAllCookies);
+
+  const toggleCookiesSettings = () => {
+    const cookiesSettings = document.querySelector('.cookies-settings');
+    const cookiesIntro = document.querySelector('.cookies-intro');
+    cookiesSettings.classList.toggle('d-none');
+    cookiesIntro.classList.toggle('d-none');
+  };
+
+  const showCookiesSettingsButton = cookiesBox.querySelector('#show-cookies-settings');
+  if (showCookiesSettingsButton) {
+    showCookiesSettingsButton.onclick = toggleCookiesSettings;
+  }
+
+  const closeCookiesSettingsButton = cookiesBox.querySelector('.close-cookies-settings');
+  if (closeCookiesSettingsButton) {
+    closeCookiesSettingsButton.onclick = toggleCookiesSettings;
+  }
+
+  const saveCookiesSettingsButton = document.querySelector('#save-cookies-settings');
+  if (saveCookiesSettingsButton) {
+    saveCookiesSettingsButton.onclick = () => {
+      // Get checked settings
+      const analyticalCookiesCheckbox = cookiesBox.querySelector('input[name="analytical_cookies"]');
+      const allCookiesCheckbox = cookiesBox.querySelector('input[name="all_cookies"]');
+
+      const analyticalCookiesChecked = analyticalCookiesCheckbox ? analyticalCookiesCheckbox.checked : false;
+      const allCookiesChecked = allCookiesCheckbox ? allCookiesCheckbox.checked : false;
+
+      let newCookieValue = allCookiesChecked ? ALL_COOKIES : ONLY_NECESSARY;
+      if (ENABLE_ANALYTICAL_COOKIES && analyticalCookiesChecked) {
+        newCookieValue = NECESSARY_ANALYTICAL_MARKETING;
       }
 
-      // DataLayer push event on cookies consent.
-      dataLayer.push({
-        'event' : 'cookiesConsent'
-      });
+      // Update cookie value
+      createCookie('greenpeace', newCookieValue, 365);
+      createCookie('active_consent_choice', '1', 365);
 
-      cookieElement.classList.remove('shown');
+      // Update ad storage and analytics storage if Google Consent Mode is enabled
+      updateGoogleConsent('ad_storage', allCookiesChecked);
+      if (ENABLE_ANALYTICAL_COOKIES) {
+        updateGoogleConsent('analytics_storage', analyticalCookiesChecked);
+      }
+
+      // Hide cookies box
+      cookiesBox.classList.remove('shown');
     };
   }
 
-  const greenpeace = window.readCookie('greenpeace');
-  const no_track = window.readCookie('no_track');
+  const greenpeace = readCookie('greenpeace');
+  const no_track = readCookie('no_track');
   // Make the necessary cookies checked by default on user's first visit.
   // Here if the No cookies set(absence of 'greenpeace' & 'no_track' cookies) consider as first visit of user.
   if (ENABLE_ANALYTICAL_COOKIES && greenpeace === null && no_track === null) {
-    window.createCookie('greenpeace', '1', 365);
+    createCookie('greenpeace', ONLY_NECESSARY, 365);
   }
 };
