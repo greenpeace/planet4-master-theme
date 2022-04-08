be59872fddffb3a34e9acaa01bcc87745d580803
Maud Leray <maudleray035@gmail.com>
2022-04-08 16:37:51 +0200

    PLANET-6690 Remove Table block overrides
    These are now handled in theme.json

--- assets/src/BlockFilters.js
+++ assets/src/BlockFilters.js
@@ -1,11 +1,9 @@
 const { addFilter } = wp.hooks;
 
-import P4TableEdit from './components/p4_table/edit';
 import { ImageBlockEdit } from './components/Image/ImageBlockEdit';
 
 export const addBlockFilters = () => {
   addFileBlockFilter();
-  addTableBlockFilter();
   addImageBlockFilter();
 };
 
@@ -22,26 +20,6 @@ const addFileBlockFilter = () => {
   };
 
   addFilter('blocks.registerBlockType', 'planet4-blocks/filters/file', setDownloadButtonToggleDefualtFalse);
-};
-
-const addTableBlockFilter = () => {
-
-  const updateTableBlockEditMethod = (settings, name) => {
-
-    if ('core/table' !== name) {
-      return settings;
-    }
-
-    if ( settings.name === 'core/table' ) {
-      lodash.assign( settings, {
-        edit: P4TableEdit,
-      } );
-    }
-
-    return settings;
-  };
-
-  addFilter('blocks.registerBlockType', 'planet4-blocks/filters/table', updateTableBlockEditMethod);
 };
 
 const addImageBlockFilter = () => addFilter('editor.BlockEdit', 'core/image/edit', ImageBlockEdit);
