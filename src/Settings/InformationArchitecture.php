a90b4b03242e6efc4ec39872a816a2c40757b799
inwerpsel <pieter.vincent1988@gmail.com>
2022-04-11 14:12:24 +0200

    Temporarily re-add for plugin dependency
    * Otherwise it's a chicken egg situation in CI.

--- src/Settings/InformationArchitecture.php
+++ src/Settings/InformationArchitecture.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace P4\MasterTheme\Settings;
 
+use P4\MasterTheme\Feature;
 use P4\MasterTheme\Features\ActionPostType;
 use P4\MasterTheme\Features\MobileTabsMenu;
 use P4\MasterTheme\Loader;
@@ -16,6 +17,9 @@ use P4\MasterTheme\Loader;
 class InformationArchitecture {
 	/** @var string Option key */
 	public const OPTIONS_KEY = 'planet4_ia';
+
+	/* @var string feature flag of action page type option key. Preserved here because plugin uses it. **/
+	public const ACTION_POST_TYPE = 'action_post_type';
 
 	/**
 	 * Get the features options page settings.
@@ -44,5 +48,18 @@ class InformationArchitecture {
 			MobileTabsMenu::get_cmb_field(),
 			ActionPostType::get_cmb_field(),
 		];
+	}
+
+	/**
+	 * Check whether an option is active.
+	 *
+	 * @param string $name Name of the option we're checking.
+	 *
+	 * @return bool Whether the option is active.
+	 */
+	public static function is_active( string $name ): bool {
+		$options = get_option( self::OPTIONS_KEY );
+
+		return 'on' === $options[ $name ];
 	}
 }
