952e80da1f7c6dda7b1bd96f97c1c7114c79d824
Florent Hernandez <florent.hernandez@greenpeace.org>
2022-07-11 11:41:57 +0200

    PLANET-6832: Fix early execution of frontend js code (#888)
    Ref: https://jira.greenpeace.org/browse/PLANET-6832
    
    Wait for dom content to be loaded before executing our js

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -25,21 +25,23 @@ const COMPONENTS = {
   'planet4-blocks/enform': ENFormFrontend,
 };
 
-document.querySelectorAll( `[data-render]` ).forEach(
-  blockNode => {
-    const blockName = blockNode.dataset.render;
-    if (!COMPONENTS[ blockName ]) {
-      return;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll( `[data-render]` ).forEach(
+    blockNode => {
+      const blockName = blockNode.dataset.render;
+      if (!COMPONENTS[ blockName ]) {
+        return;
+      }
 
-    const BlockFrontend = COMPONENTS[ blockName ];
-    if (!BlockFrontend) {
-      return;
+      const BlockFrontend = COMPONENTS[ blockName ];
+      if (!BlockFrontend) {
+        return;
+      }
+      const attributes = JSON.parse( blockNode.dataset.attributes );
+      wp.element.render( <BlockFrontend { ...attributes.attributes } />, blockNode );
     }
-    const attributes = JSON.parse( blockNode.dataset.attributes );
-    wp.element.render( <BlockFrontend { ...attributes.attributes } />, blockNode );
-  }
-);
+  );
 
-setupMediaElementJS();
-setupLightboxForImages();
+  setupMediaElementJS();
+  setupLightboxForImages();
+});
