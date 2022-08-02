2dfb67b843b8cb43c8746b6e51697322805c60fd
Dan Tovbein <dtovbein@gmail.com>
2022-08-02 10:05:27 -0300

    Implement News & Stories page
    - Add the new news-and-story php file

---
+++ news-and-story.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying News and Stories.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ */
+
+use P4\MasterTheme\Features\Dev\ListingPageGridView;
+use P4\MasterTheme\Features\ListingPagePagination;
+use P4\MasterTheme\Post;
+use Timber\Timber;
+
+$templates = [ 'taxonomy.twig', 'index.twig' ];
+
+$context             = Timber::get_context();
+$context['taxonomy'] = get_queried_object();
+$context['wp_title'] = $context['taxonomy']->name;
+
+if ( ListingPagePagination::is_active() ) {
+	$view = ListingPageGridView::is_active() ? 'grid' : 'list';
+
+	$query_template = file_get_contents( get_template_directory() . "/parts/query-$view.html" );
+
+	$content = do_blocks( $query_template );
+
+	$context['query_loop'] = $content;
+	Timber::render( $templates, $context );
+	exit();
+}
+
+$post_args = [
+	'cat'            => $context['taxonomy']->term_id,
+	'posts_per_page' => 10,
+	'post_type'      => 'post',
+	'paged'          => 1,
+	'has_password'   => false,  // Skip password protected content.
+];
+if ( get_query_var( 'page' ) ) {
+	$templates          = [ 'tease-taxonomy-post.twig' ];
+	$post_args['paged'] = get_query_var( 'page' );
+	$pagetype_posts     = new \Timber\PostQuery( $post_args, Post::class );
+	foreach ( $pagetype_posts as $pagetype_post ) {
+		$context['post'] = $pagetype_post;
+		Timber::render( $templates, $context );
+	}
+} else {
+	$pagetype_posts   = new \Timber\PostQuery( $post_args, Post::class );
+	$context['posts'] = $pagetype_posts;
+	Timber::render( $templates, $context );
+}
