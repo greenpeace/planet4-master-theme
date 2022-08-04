c6c45cfa68f4279fb285286ef704f1d6ff30937a
Maud Leray <maudleray035@gmail.com>
2022-08-04 11:13:20 +0200

    Fix SideImageWithTextAndCta image alignment
    We didn't use the right attribute mediaPosition for it

--- classes/patterns/class-sideimagewithtextandcta.php
+++ classes/patterns/class-sideimagewithtextandcta.php
@@ -28,21 +28,23 @@ class SideImageWithTextAndCta extends Block_Pattern {
 	 * @param array $params Optional array of parameters for the config.
 	 */
 	public static function get_config( $params = [] ): array {
-		$media_link           = esc_url( get_template_directory_uri() ) . '/images/placeholders/placeholder-546x415.jpg';
-		$media_position_class = array_key_exists( 'media_position', $params ) && 'right' === $params['media_position'] ? 'has-media-on-the-right' : '';
-		$title_placeholder    = $params['title_placeholder'] ?? '';
-		$background_color     = $params['background_color'] ?? null;
-		$background_attribute = $background_color ? ',"backgroundColor":"' . $background_color . '"' : '';
-		$background_classes   = $background_color ? 'has-' . $background_color . '-background-color has-background' : '';
-		$alignfull            = $params['alignfull'] ?? false;
-		$alignment_class      = $alignfull ? 'alignfull' : '';
-		$alignment_attribute  = $alignfull ? ',"align":"full"' : '';
+		$media_link               = esc_url( get_template_directory_uri() ) . '/images/placeholders/placeholder-546x415.jpg';
+		$media_position           = $params['media_position'] ?? '';
+		$media_position_class     = 'right' === $media_position ? 'has-media-on-the-right' : '';
+		$media_position_attribute = 'right' === $media_position ? ',"mediaPosition":"right"' : '';
+		$title_placeholder        = $params['title_placeholder'] ?? '';
+		$background_color         = $params['background_color'] ?? null;
+		$background_attribute     = $background_color ? ',"backgroundColor":"' . $background_color . '"' : '';
+		$background_classes       = $background_color ? 'has-' . $background_color . '-background-color has-background' : '';
+		$alignfull                = $params['alignfull'] ?? false;
+		$alignment_class          = $alignfull ? 'alignfull' : '';
+		$alignment_attribute      = $alignfull ? ',"align":"full"' : '';
 
 		return [
 			'title'      => __( 'Side image with text and CTA', 'planet4-blocks-backend' ),
 			'categories' => [ 'planet4' ],
 			'content'    => '
-				<!-- wp:media-text {"mediaLink":"' . $media_link . '","mediaType":"image","className":"block ' . $media_position_class . '"' . $background_attribute . $alignment_attribute . '} -->
+				<!-- wp:media-text {"mediaLink":"' . $media_link . '","mediaType":"image","className":"block ' . $media_position_class . '"' . $background_attribute . $alignment_attribute . $media_position_attribute . '} -->
 					<div class="wp-block-media-text ' . $alignment_class . ' is-stacked-on-mobile block ' . $media_position_class . ' ' . $background_classes . '">
 						<figure class="wp-block-media-text__media">
 							<img src="' . $media_link . '" alt="' . __( 'Default image', 'planet4-blocks-backend' ) . '"/>
