e26b72f4ce34142601a9c3ebdf9c8fb3e5837588
Dan Tovbein <dtovbein@gmail.com>
2022-08-22 13:06:03 -0300

    Create all posts template
    * Use it through default listing page when the "Posts page" setting
    * From Reading Settings is changed.

--- index.php
+++ index.php
@@ -13,11 +13,30 @@
  * @since   Timber 0.1
  */
 
+use P4\MasterTheme\Features\Dev\ListingPageGridView;
+use P4\MasterTheme\Features\ListingPagePagination;
+use Timber\Timber;
+
 $context          = Timber::get_context();
 $context['posts'] = Timber::get_posts();
+$context['title'] = html_entity_decode( $context['wp_title'] );
 
 $templates = [ 'index.twig' ];
+
 if ( is_home() ) {
-	array_unshift( $templates, 'home.twig' );
+	array_unshift( $templates, 'all-posts.twig' );
+
+	if ( ListingPagePagination::is_active() ) {
+		$view = ListingPageGridView::is_active() ? 'grid' : 'list';
+
+		$query_template = file_get_contents( get_template_directory() . "/parts/query-$view.html" );
+
+		$content = do_blocks( $query_template );
+
+		$context['query_loop'] = $content;
+		Timber::render( $templates, $context );
+		exit();
+	}
 }
+
 Timber::render( $templates, $context );
