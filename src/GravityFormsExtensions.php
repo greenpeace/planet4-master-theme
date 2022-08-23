7186429717b170b45f1e4094d06cd7976e0051e0
Maud Leray <maudleray035@gmail.com>
2022-08-23 14:17:47 +0200

    PLANET-6808 Add P4 confirmation message option for Gravity Forms
    This will allow editors to use our P4 confirmation message or to create their own

--- src/GravityFormsExtensions.php
+++ src/GravityFormsExtensions.php
@@ -51,6 +51,25 @@ class GravityFormsExtensions {
 	];
 
 	/**
+	 * @var string The default gravity form confirmation message.
+	 */
+	public const DEFAULT_GF_CONFIRMATION = 'planet4';
+
+	/**
+	 * @var array The Planet4 Gravity form confirmation messages.
+	 */
+	public const P4_GF_CONFIRMATIONS = [
+		[
+			'label' => 'Planet 4 message',
+			'value' => 'planet4',
+		],
+		[
+			'label' => 'Custom message',
+			'value' => 'custom',
+		],
+	];
+
+	/**
 	 * The constructor.
 	 */
 	public function __construct() {
@@ -61,18 +80,18 @@ class GravityFormsExtensions {
 	 * Class hooks.
 	 */
 	private function hooks() {
-		add_filter( 'gform_form_settings_fields', [ $this, 'p4_gf_type_setting' ], 5, 2 );
+		add_filter( 'gform_form_settings_fields', [ $this, 'p4_gf_settings' ], 5, 2 );
 	}
 
 	/**
-	 * Add a setting to gravity Forms to set the type of form.
+	 * Add settings to Gravity Forms: one to set the type of form, one to choose the confirmation message.
 	 *
 	 * @param array  $fields The form settings fields.
 	 * @param object $form The Gravity form Object.
 	 *
 	 * @return array The new fields array.
 	 */
-	public function p4_gf_type_setting( $fields, $form ) {
+	public function p4_gf_settings( $fields, $form ) {
 
 		if ( ! array_key_exists( 'p4_options', $fields ) ) {
 			$new_fields['p4_options'] = [
@@ -91,6 +110,16 @@ class GravityFormsExtensions {
 			'required'       => true,
 			'default_value ' => self::DEFAULT_GF_TYPE,
 			'choices'        => self::P4_GF_TYPES,
+		];
+
+		$fields['p4_options']['fields'][] = [
+			'type'           => 'select',
+			'name'           => 'p4_gf_confirmation',
+			'label'          => __( 'Confirmation message', 'planet4-master-theme-backend' ),
+			'tooltip'        => __( 'If you use the Planet 4 confirmation message, you will not be able to see it and edit it via the Confirmations menu item', 'planet4-master-theme-backend' ),
+			'required'       => true,
+			'default_value ' => self::DEFAULT_GF_CONFIRMATION,
+			'choices'        => self::P4_GF_CONFIRMATIONS,
 		];
 
 		return $fields;
