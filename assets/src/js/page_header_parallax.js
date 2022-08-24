2cb51c53fa6405cd3ea926bb6b59ca9f517334c7
Maud Leray <maudleray035@gmail.com>
2022-08-24 15:39:20 +0200

    Updates after PR review
    Small code refactoring

--- assets/src/js/page_header_parallax.js
+++ assets/src/js/page_header_parallax.js
@@ -1,8 +1,8 @@
 export const setupPageHeaderParallax = () => {
-  const pageHeaders = [...document.querySelectorAll('.is-pattern-p4-page-header > .wp-block-media-text__media > img')];
+  const pageHeaders = document.querySelectorAll('.is-pattern-p4-page-header > .wp-block-media-text__media > img');
 
   window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
-    pageHeaders.forEach(pageHeader => pageHeader.style.transform = 'translateY(' + scrollPosition * 0.6 + 'px)');
+    pageHeaders.forEach(pageHeader => pageHeader.style.transform = `translateY(${scrollPosition * 0.6}px)`);
   });
 };
