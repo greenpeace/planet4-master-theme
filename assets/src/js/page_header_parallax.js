42da2076c2b70336ae72a0502f534d2bcdb7c7ba
Maud Leray <maudleray035@gmail.com>
2022-08-25 10:12:28 +0200

    Add parallax effect only in large screens and up
    We don't want the parallax effect on mobile and tablet

--- assets/src/js/page_header_parallax.js
+++ assets/src/js/page_header_parallax.js
@@ -1,15 +1,38 @@
+const isSmallWindow = () => window.innerWidth < 992;
+
+const addPageHeaderParallax = pageHeaderImages => {
+  const scrollPosition = window.scrollY;
+
+  pageHeaderImages.forEach(pageHeaderImage => {
+    const pageHeaderPosition = pageHeaderImage.scrollHeight;
+
+    if (scrollPosition > pageHeaderPosition) {
+      pageHeaderImage.style.transform = `translateY(${(scrollPosition - pageHeaderPosition) * 0.6}px)`;
+    }
+  });
+};
+
+const removePageHeaderParallax = pageHeaderImages => {
+  pageHeaderImages.forEach(pageHeaderImage => {
+    pageHeaderImage.style.transform = 'none';
+  });
+};
+
 export const setupPageHeaderParallax = () => {
   const pageHeaderImages = document.querySelectorAll('.is-pattern-p4-page-header > .wp-block-media-text__media > img');
 
-  window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
+  const pageHeaderParallax = () => addPageHeaderParallax(pageHeaderImages);
 
-    pageHeaderImages.forEach(pageHeaderImage => {
-      const pageHeaderPosition = pageHeaderImage.scrollHeight;
+  if (!isSmallWindow()) {
+    window.addEventListener('scroll', pageHeaderParallax);
+  }
 
-      if (scrollPosition > pageHeaderPosition) {
-        pageHeaderImage.style.transform = `translateY(${(scrollPosition - pageHeaderPosition) * 0.6}px)`;
-      }
-    });
+  window.addEventListener('resize', () => {
+    if (isSmallWindow()) {
+      window.removeEventListener('scroll', pageHeaderParallax);
+      removePageHeaderParallax(pageHeaderImages);
+    } else {
+      window.addEventListener('scroll', pageHeaderParallax);
+    }
   });
 };
