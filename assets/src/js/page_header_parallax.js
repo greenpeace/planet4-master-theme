0b037aa8fc094b2acca56fcc9cf5d302f00f2844
Maud Leray <maudleray035@gmail.com>
2022-08-30 13:11:24 +0200

    Move smaller functions within main function
    Refactoring

--- assets/src/js/page_header_parallax.js
+++ assets/src/js/page_header_parallax.js
@@ -1,39 +1,37 @@
-const isSmallWindow = () => window.innerWidth < 992;
-
-const addPageHeaderParallax = pageHeaders => {
-  pageHeaders.forEach(pageHeader => {
-    const pageHeaderImage = pageHeader.querySelector('.wp-block-media-text__media > img');
-    const pageHeaderRect = pageHeader.getBoundingClientRect();
-
-    // 100 to take into account the navbar + a bit of extra spacing
-    if (pageHeaderRect.top < 100) {
-      pageHeaderImage.style.transform = `translateY(${(100 - pageHeaderRect.top) * 0.6}px)`;
-    }
-  });
-};
-
-const removePageHeaderParallax = pageHeaders => {
-  pageHeaders.forEach(pageHeader => {
-    const pageHeaderImage = pageHeader.querySelector('.wp-block-media-text__media > img');
-    pageHeaderImage.style.transform = 'none';
-  });
-};
-
 export const setupPageHeaderParallax = () => {
   const pageHeaders = document.querySelectorAll('.is-pattern-p4-page-header');
 
-  const pageHeaderParallax = () => addPageHeaderParallax(pageHeaders);
+  const isSmallWindow = () => window.innerWidth < 992;
+
+  const addPageHeaderParallax = () => {
+    pageHeaders.forEach(pageHeader => {
+      const pageHeaderImage = pageHeader.querySelector('.wp-block-media-text__media > img');
+      const pageHeaderRect = pageHeader.getBoundingClientRect();
+
+      // 100 to take into account the navbar + a bit of extra spacing
+      if (pageHeaderRect.top < 100) {
+        pageHeaderImage.style.transform = `translateY(${(100 - pageHeaderRect.top) * 0.6}px)`;
+      }
+    });
+  };
+
+  const removePageHeaderParallax = () => {
+    pageHeaders.forEach(pageHeader => {
+      const pageHeaderImage = pageHeader.querySelector('.wp-block-media-text__media > img');
+      pageHeaderImage.style.transform = 'none';
+    });
+  };
 
   if (!isSmallWindow()) {
-    window.addEventListener('scroll', pageHeaderParallax);
+    window.addEventListener('scroll', addPageHeaderParallax);
   }
 
   window.addEventListener('resize', () => {
     if (isSmallWindow()) {
-      window.removeEventListener('scroll', pageHeaderParallax);
-      removePageHeaderParallax(pageHeaders);
+      window.removeEventListener('scroll', addPageHeaderParallax);
+      removePageHeaderParallax();
     } else {
-      window.addEventListener('scroll', pageHeaderParallax);
+      window.addEventListener('scroll', addPageHeaderParallax);
     }
   });
 };
