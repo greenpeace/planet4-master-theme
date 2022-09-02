0618e71e5b3672c26c8c28e414ba2b0b8bdfb9ad
Maud Leray <maudleray035@gmail.com>
2022-09-02 12:40:12 +0200

    Set speed to -1 on mobile to minimise spacing changes
    The default speed is -2

--- assets/src/js/parallax.js
+++ assets/src/js/parallax.js
@@ -1,5 +1,18 @@
 export const setupParallax = () => {
-  const rellax = new Rellax('.is-style-parallax img', { // eslint-disable-line no-unused-vars, no-undef
-    center: true
+  const rellax = new Rellax('.is-style-parallax img', { // eslint-disable-line no-undef
+    center: true,
   });
+
+  const parallaxImages = document.querySelectorAll('.is-style-parallax img');
+  let mobileSpeedAllSetup = false;
+  parallaxImages.forEach((image, index) => {
+    image.setAttribute('data-rellax-xs-speed', -1); // the default value is -2, we want to keep it for bigger screens.
+    if (index === parallaxImages.length - 1) {
+      mobileSpeedAllSetup = true;
+    }
+  });
+
+  if (mobileSpeedAllSetup) {
+    rellax.refresh();
+  }
 };
