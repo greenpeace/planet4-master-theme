1acfd4786e99fc5ca42344094372f7809c4c7185
Maud Leray <maudleray035@gmail.com>
2022-09-06 10:46:19 +0200

    PLANET-6851 Add share buttons to Gravity Forms confirmation settings
    This way editors will be able to add sharing functionalities to their form's confirmation messages.

--- src/GravityFormsExtensions.php
+++ src/GravityFormsExtensions.php
@@ -72,6 +72,37 @@ class GravityFormsExtensions {
 	];
 
 	/**
+	 * @var array The Planet4 Gravity Forms share buttons options.
+	 */
+	public const P4_SHARE_BUTTONS = [
+		[
+			'label'         => 'WhatsApp',
+			'name'          => 'whatsapp',
+			'default_value' => 1,
+		],
+		[
+			'label'         => 'Facebook',
+			'name'          => 'facebook',
+			'default_value' => 1,
+		],
+		[
+			'label'         => 'Twitter',
+			'name'          => 'twitter',
+			'default_value' => 1,
+		],
+		[
+			'label'         => 'Email',
+			'name'          => 'email',
+			'default_value' => 1,
+		],
+		[
+			'label'         => 'Native share (mobile only)',
+			'name'          => 'native',
+			'default_value' => 1,
+		],
+	];
+
+	/**
 	 * The constructor.
 	 */
 	public function __construct() {
@@ -85,17 +116,17 @@ class GravityFormsExtensions {
 		add_filter( 'gform_form_settings_fields', [ $this, 'p4_gf_settings' ], 5, 2 );
 		add_filter( 'gform_secure_file_download_url', [ $this, 'p4_gf_file_download_url' ], 10, 2 );
 		add_action( 'gform_after_save_form', [ $this, 'p4_gf_custom_initial_settings' ], 10, 2 );
+		add_filter( 'gform_confirmation_settings_fields', [ $this, 'p4_gf_confirmation_settings' ], 10, 3 );
 	}
 
 	/**
-	 * Add settings to Gravity Forms: one to set the type of form, one to choose the confirmation message.
+	 * Add form settings to Gravity Forms: one to set the type of form, one to choose the confirmation message.
 	 *
-	 * @param array  $fields The form settings fields.
-	 * @param object $form The Gravity form Object.
+	 * @param array $fields The form settings fields.
 	 *
 	 * @return array The new fields array.
 	 */
-	public function p4_gf_settings( $fields, $form ) {
+	public function p4_gf_form_settings( $fields ) {
 
 		if ( ! array_key_exists( 'p4_options', $fields ) ) {
 			$new_fields['p4_options'] = [
@@ -170,5 +201,105 @@ class GravityFormsExtensions {
 
 			GFAPI::update_form( $form );
 		}
+	}
+
+	/**
+	 * Add confirmation settings to Gravity Forms: the ability to add share buttons.
+	 *
+	 * @param array $fields The general confirmation settings fields.
+	 * @param array $confirmation The current form's confirmation settings.
+	 * @param array $form The current form's settings.
+	 *
+	 * @return array The new fields array.
+	 */
+	public function p4_gf_confirmation_settings( $fields, $confirmation, $form ) {
+		echo '
+			<style>
+				.hidden {
+					display: none !important;
+				}
+			</style>
+		';
+
+		// This bit of code is to hide the confirmation settings if the P4 default message option is selected in the form's settings.
+		if ( self::DEFAULT_GF_CONFIRMATION === $form['p4_gf_confirmation'] ) {
+			echo '
+				<script>
+					addEventListener("DOMContentLoaded", () => {
+						const saveButton = document.querySelector(".gform-settings-save-container");
+						const settingsSection = document.querySelector(".gform-settings__content");
+
+						saveButton.classList.add("hidden");
+
+						const handbookLink = document.createElement("a");
+						handbookLink.target = "_blank";
+						handbookLink.innerText = "P4 handbook";
+						handbookLink.href = "https://planet4.greenpeace.org/manage/integrate/form-builder/build-a-form-in-gravity-forms/#confirmation-settings--thank-you-page-";
+
+						const explanation = document.createElement("div");
+						explanation.innerText = "You cannot edit confirmation messages here since this form uses our P4 default message. If you want to change this, you can do so in the form\'s general settings. You can find more information about this in the ";
+						explanation.appendChild(handbookLink);
+
+						settingsSection.appendChild(explanation);
+					});
+				</script>
+			';
+			return [];
+		}
+
+		// This bit of code is to hide the "Share Buttons" section if editors select "Page" or "Redirect" as confirmation message.
+		echo '
+			<script>
+				addEventListener("DOMContentLoaded", () => {
+					const confirmationTypeCheckboxes = [...document.querySelectorAll("input[name=\"_gform_setting_type\"]")];
+					const textTypeCheckbox = confirmationTypeCheckboxes.find(input => input.value === "message");
+					const shareButtonsSettings = document.querySelector("#gform-settings-section-share-buttons");
+
+					const onChange = checkbox => {
+						if (checkbox.value === "message" && checkbox.checked) {
+							shareButtonsSettings.classList.remove("hidden");
+						} else {
+							shareButtonsSettings.classList.add("hidden");
+						}
+					}
+
+					confirmationTypeCheckboxes.forEach(input => input.addEventListener("change", event => onChange(event.currentTarget)));
+
+					onChange(textTypeCheckbox);
+				});
+			</script>
+		';
+
+		if ( ! array_key_exists( 'p4_share_buttons', $fields ) ) {
+			$share_buttons['p4_share_buttons'] = [
+				'title' => __( 'Share buttons', 'planet4-master-theme-backend' ),
+			];
+
+			// Add new field to end of the $fields array.
+			$fields = array_merge( $fields, $share_buttons );
+		}
+
+		$fields['p4_share_buttons']['fields'][] = [
+			'type'    => 'checkbox',
+			'name'    => 'p4_gf_share_platforms',
+			'label'   => __( 'Show share buttons below message', 'planet4-master-theme-backend' ),
+			'choices' => self::P4_SHARE_BUTTONS,
+		];
+
+		$fields['p4_share_buttons']['fields'][] = [
+			'type'    => 'text',
+			'name'    => 'p4_gf_share_text_override',
+			'label'   => __( 'Share text', 'planet4-master-theme-backend' ),
+			'tooltip' => __( 'This is the text that will be shared when a user clicks a share button (if the platform supports share text)', 'planet4-master-theme-backend' ),
+		];
+
+		$fields['p4_share_buttons']['fields'][] = [
+			'type'    => 'text',
+			'name'    => 'p4_gf_share_url_override',
+			'label'   => __( 'Override share URL', 'planet4-master-theme-backend' ),
+			'tooltip' => __( 'By default, share buttons will share the URL of the page that the form was submitted on. Use this field to override with a different URL.', 'planet4-master-theme-backend' ),
+		];
+
+		return $fields;
 	}
 }
