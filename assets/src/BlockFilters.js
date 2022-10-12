121f22428c1c457d3482a8e048b4237fe233370b
Sagar Deshmukh <sagarsdeshmukh@users.noreply.github.com>
2022-10-12 19:22:15 +0530

    PLANET-6843 Added a block filter to enforce "AJAX" toggle setting enabled by default on GF block. (#951)
    PLANET-6843 Added a block filter to enforce "AJAX" toggle setting enabled by default on GF block
    
    Ref https://jira.greenpeace.org/browse/PLANET-6843

--- assets/src/BlockFilters.js
+++ assets/src/BlockFilters.js
@@ -5,6 +5,7 @@ import { ImageBlockEdit } from './components/Image/ImageBlockEdit';
 export const addBlockFilters = () => {
   addFileBlockFilter();
   addImageBlockFilter();
+  addGravityFormsBlockFilter();
 };
 
 const addFileBlockFilter = () => {
@@ -23,3 +24,19 @@ const addFileBlockFilter = () => {
 };
 
 const addImageBlockFilter = () => addFilter('editor.BlockEdit', 'core/image/edit', ImageBlockEdit);
+
+// Enforce "AJAX" toggle setting enabled by default, on Gravity form block.
+const addGravityFormsBlockFilter = () => {
+  const setAJAXToggleDefaultTrue = (settings, name) => {
+
+    if ('gravityforms/form' !== name) {
+      return settings;
+    }
+
+    settings.attributes['ajax']['default'] = true;
+
+    return settings;
+  };
+
+  addFilter('blocks.registerBlockType', 'planet4-blocks/filters/file', setAJAXToggleDefaultTrue);
+};
