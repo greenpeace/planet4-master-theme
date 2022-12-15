531ee2c3407771412158f5727852888e287e033d
Osong Agberndifor <osongagberndifor@gmail.com>
2022-12-15 12:48:19 +0100

    PLANET-6960 Add sidebar option to define canonical link
    - Changed the filter function to replace the default canonical url if set in the sidebar options, so we don't have 2 links in the post head

--- functions.php
+++ functions.php
@@ -475,15 +475,16 @@ add_action(
 	}
 );
 
-/**
- * Adds the canonical link from the sidebar menu to the post head
- */
-function add_meta_tags() {
-	global $post;
-	if ( isset( $post->p4_seo_canonical_url ) && '' !== $post->p4_seo_canonical_url ) {
-		$url = $post->p4_seo_canonical_url;
-		echo '<link rel="canonical" href="' . $url . '">'; // phpcs:ignore
-	}
-}
-
-add_action( 'wp_head', 'add_meta_tags', 10 );
+// This filter replaces the default Canonical URL with what is set in the Sidebar Options.
+// If no URL is set for the Canonical link, the default WP url is used.
+add_filter(
+	'get_canonical_url',
+	function( $canonical_url, $post ) {
+		if ( isset( $post->p4_seo_canonical_url ) && '' !== $post->p4_seo_canonical_url ) {
+			$canonical_url = $post->p4_seo_canonical_url;
+		}
+		return $canonical_url;
+	},
+	10,
+	2
+);
