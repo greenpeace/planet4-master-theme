1ac8faa2396e8cadb5532cf8beb429db3b15930d
Maud Leray <maudleray035@gmail.com>
2023-02-02 10:28:53 +0100

    PLANET-7055 Setup Playwright
    This commit also includes 2 simple tests

---
+++ tests/e2e/cookies-banner.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+test('check cookies banner', async ({ page }) => {
+  await page.goto('/');
+
+  // Check that cookies banner is present.
+  const cookiesBanner = page.locator('#set-cookie');
+  await expect(cookiesBanner).toBeVisible();
+
+  // Check that the text is the one from the P4 settings.
+  const cookiesText = await page.evaluate('window.p4bk_vars.cookies_field');
+  await expect(cookiesText).toBeDefined();
+  await expect(cookiesBanner.locator('.cookies-text')).toHaveText(cookiesText);
+
+  // Accept all cookies.
+  await cookiesBanner.getByText('Accept all cookies').click();
+
+  // Check that the cookies banner disappears.
+  await expect(cookiesBanner).toBeHidden();
+});
