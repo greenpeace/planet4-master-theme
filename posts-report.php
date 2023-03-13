5aa63d0d0d64d779bce66327acfcb46c8f6b6115
Maud Leray <maudleray035@gmail.com>
2023-03-13 14:32:58 +0100

    PLANET-7000 Fix posts report spinner gif url
    Before this fix, the file worked in local environments but not in other sites

--- posts-report.php
+++ posts-report.php
@@ -8,6 +8,8 @@
 
 use Timber\Timber;
 
-Timber::render([ 'posts_report.twig' ], []);
+Timber::render([ 'posts_report.twig' ], [
+    'spinner_gif' => get_template_directory_uri() . '/images/wpspin_light-2x.gif',
+]);
 
 require dirname(__FILE__) . '/underscore_templates/posts_reports.tpl.php';
