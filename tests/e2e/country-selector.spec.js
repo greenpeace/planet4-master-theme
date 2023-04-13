b70f8187c2730f403fdbadd77fea4210a7689a2a
Maud Leray <maudleray035@gmail.com>
2023-04-13 15:10:33 +0200

    PLANET-7062 Rewrite country selector test with Playwright
    This commit also removes the old test that was flaky

---
+++ tests/e2e/country-selector.spec.js
@@ -0,0 +1,8 @@
+const {test, expect} = require('@playwright/test');
+
+test('check the country selector behaviour', async ({page}) => {
+  await page.goto('./');
+  await expect(page.locator('.country-selector-toggle-container')).toBeVisible();
+  await page.locator('button.country-control-toggle').click();
+  await expect(page.locator('.countries-list')).toBeVisible();
+});
