d49f9f54265225fbf6efbd1f8fdad46835b43181
Florent Hernandez <florent.hernandez@greenpeace.org>
2023-04-17 13:58:30 +0200

    PLANET-7064: Playwright CI job (#1037)
    Ref: https://jira.greenpeace.org/browse/PLANET-7064
    Copy of: https://github.com/greenpeace/planet4-master-theme/pull/1925

---
+++ tests/e2e/tools/lib/rest.js
@@ -0,0 +1,40 @@
+import { nonce } from './nonce';
+
+async function rest(context, options) {
+  const { path, ...fetchOptions } = options;
+
+  try {
+    const response = await context.request.fetch( path, {
+      ...fetchOptions,
+      failOnStatusCode: false,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-WP-Nonce': context.storageState.nonce,
+        ...( fetchOptions.headers || {} ),
+      },
+    } );
+    const json = await response.json();
+
+    if ( ! response.ok() ) {
+      throw json;
+    }
+
+    return json;
+  } catch ( error ) {
+    // Nonce in invalid, retry again with a renewed nonce.
+    if (
+      typeof error === 'object' &&
+      error !== null &&
+      Object.prototype.hasOwnProperty.call( error, 'code' ) &&
+      error.code === 'rest_cookie_invalid_nonce'
+    ) {
+      await nonce(context);
+
+      return rest( options );
+    }
+
+    throw error;
+  }
+}
+
+export { rest };
