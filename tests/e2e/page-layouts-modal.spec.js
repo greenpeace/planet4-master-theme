5266af7ce17f12ad08115cef1e2d5b207814df00
Osong Agberndifor <38656549+Osong-Michael@users.noreply.github.com>
2023-05-09 09:43:17 +0100

    PLANET-7070 Re-write the Page Layouts Modal test in Playwright (#1038)
    - Added test for page layout modal in Playwright
    - Removed old acceptance test

---
+++ tests/e2e/page-layouts-modal.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test');
+import {login} from './tools/lib/login';
+
+test('checks if the welcome modal on the editor is present and closed when the button is clicked', async ({page, context}) => {
+  test.setTimeout(240 * 1000);
+  await page.goto('./');
+  await login(page, context);
+  await page.goto('./wp-admin/post-new.php?post_type=page');
+  const locator = page.locator('.components-modal__screen-overlay');
+  await expect(locator).toBeVisible();
+  await page.getByRole('button', {name: 'Close'}).click();
+  await expect(locator).toBeHidden();
+});
