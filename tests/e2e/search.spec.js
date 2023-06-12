9b7c2a01af2dc0eab089c3214a82fb55cec71ff2
Osong Agberndifor <osongagberndifor@gmail.com>
2023-06-12 14:52:51 +0100

    PLANET-7163 Re-write the Search test in Playwright
    - Added test for search functionality

---
+++ tests/e2e/search.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+test('check search works', async ({page}) => {
+  await page.goto('./');
+
+  const searchBox = page.getByPlaceholder('Search');
+  await searchBox.click();
+  await searchBox.type('climate');
+  await page.keyboard.press('Enter');
+
+  const searchResult = await page.innerHTML('.result-statement');
+  const searchTags = await page.locator('.search-result-item-headline').allInnerTexts();
+
+  expect(searchResult).toContain('climate');
+  expect(searchTags).toContain('#Climate');
+});
