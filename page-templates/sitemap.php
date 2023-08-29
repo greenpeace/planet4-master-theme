6c1e1b08120efdccd88b7cf0d7b3dd16ce6094f4
Florent Hernandez <florent.hernandez@greenpeace.org>
2023-08-29 17:19:39 +0200

    PLANET-6793: Sitemap native taxonomies (#2085)
    Ref: https://jira.greenpeace.org/browse/PLANET-6793

--- page-templates/sitemap.php
+++ page-templates/sitemap.php
@@ -13,23 +13,40 @@ use Timber\Timber;
 
 $context = Timber::get_context();
 $post = new Post(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-$sitemap = new Sitemap();
 $page_meta_data = get_post_meta($post->ID);
 
 $context['post'] = $post;
-$context['header_title'] = is_front_page() ? '' : ( $page_meta_data['p4_title'][0] ?? $post->title );
+$context['header_title'] = is_front_page() ? '' : ($page_meta_data['p4_title'][0] ?? $post->title);
 $context['background_image'] = wp_get_attachment_url(get_post_meta(get_the_ID(), 'background_image_id', 1));
 $context['custom_body_classes'] = 'white-bg';
-
-$context['actions_title'] = __('Act', 'planet4-master-theme');
-$context['issues_title'] = __('Explore', 'planet4-master-theme');
-$context['evergreen_title'] = __('About Greenpeace', 'planet4-master-theme');
-$context['page_types_title'] = __('Articles', 'planet4-master-theme');
-
-$context['actions'] = $sitemap->get_actions();
-$context['issues'] = $sitemap->get_issues();
-$context['evergreen_pages'] = $sitemap->get_evergreen_pages();
-$context['page_types'] = $sitemap->get_page_types();
 $context['page_category'] = 'Sitemap Page';
 
-Timber::render([ 'sitemap.twig' ], $context);
+if (get_theme_mod('new_identity_styles')) {
+    $context['categories'] = get_categories(['orderby' => 'name', 'order' => 'ASC']);
+    $context['posts'] = [];
+    foreach ($context['categories'] as $cat) {
+        if ($cat->slug === 'uncategorized') {
+            continue;
+        }
+        $context['posts'][$cat->term_id] = get_posts([
+            'post_type' => 'page',
+            'category' => $cat->term_id,
+            'orderby' => 'title',
+            'order' => 'ASC',
+        ]);
+    }
+} else {
+    $sitemap = new Sitemap();
+
+    $context['actions_title'] = __('Act', 'planet4-master-theme');
+    $context['issues_title'] = __('Explore', 'planet4-master-theme');
+    $context['evergreen_title'] = __('About Greenpeace', 'planet4-master-theme');
+    $context['page_types_title'] = __('Articles', 'planet4-master-theme');
+
+    $context['actions'] = $sitemap->get_actions();
+    $context['issues'] = $sitemap->get_issues();
+    $context['evergreen_pages'] = $sitemap->get_evergreen_pages();
+    $context['page_types'] = $sitemap->get_page_types();
+}
+
+Timber::render(['sitemap.twig'], $context);
