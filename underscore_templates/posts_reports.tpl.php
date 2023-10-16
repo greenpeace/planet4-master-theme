a58c6c2618218e7fefd0d8eceadaf709c287fb41
Maud Leray <maudleray035@gmail.com>
2023-10-16 10:52:25 +0200

    PLANET-7250 Add Actions list to posts report
    This is for the new IA

--- underscore_templates/posts_reports.tpl.php
+++ underscore_templates/posts_reports.tpl.php
@@ -52,3 +52,18 @@
         <button class="button button-primary refresh"><?php _e( 'Refresh', 'planet4-master-theme-backend' ) ?></button>
     </p>
 </script>
+
+<script type="text/template" id="tmpl-p4-action-list">
+    <table class="wp-list-table widefat">
+        <thead>
+            <th><?php _e( 'Title', 'planet4-master-theme-backend' ) ?></th>
+            <th><?php _e( 'Publish Status', 'planet4-master-theme-backend' ) ?></th>
+            <th><?php _e( 'Published at', 'planet4-master-theme-backend' ) ?></th>
+            <th><?php _e( 'Modified at', 'planet4-master-theme-backend' ) ?></th>
+        </thead>
+        <tbody class="p4-actions"></tbody>
+    </table>
+    <p>
+        <button class="button button-primary refresh"><?php _e( 'Refresh', 'planet4-master-theme-backend' ) ?></button>
+    </p>
+</script>
