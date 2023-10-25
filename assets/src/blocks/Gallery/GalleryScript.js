4c8331792dd5948fd27c3bbce3a8f3231bd6fe6d
Maud Leray <maudleray035@gmail.com>
2023-10-25 12:04:18 +0200

    Fix old blocks not rendering in the frontend
    This commit includes the Gallery, Timeline, ENForm and Covers blocks

--- assets/src/blocks/Gallery/GalleryScript.js
+++ assets/src/blocks/Gallery/GalleryScript.js
@@ -1,4 +1,14 @@
+import {createRoot} from 'react-dom/client';
 import {GalleryFrontend} from './GalleryFrontend';
 import {hydrateBlock} from '../../functions/hydrateBlock';
 
 hydrateBlock('planet4-blocks/gallery', GalleryFrontend, {renderLightbox: true});
+
+// Fallback for non migrated content. Remove after migration.
+document.querySelectorAll('[data-render="planet4-blocks/gallery"]').forEach(
+  blockNode => {
+    const attributes = JSON.parse(blockNode.dataset.attributes);
+    const rootElement = createRoot(blockNode);
+    rootElement.render(<GalleryFrontend {...attributes} renderLightbox />);
+  }
+);
