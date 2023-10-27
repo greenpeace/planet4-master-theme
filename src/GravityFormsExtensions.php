e77e4686e86cb46926627c43657404a737179cff
Dan Tovbein <dtovbein@gmail.com>
2023-10-27 12:05:11 -0300

    Add code GravityForms Hubspot snippet
    - Changes to the lifecycle stage in forms

--- src/GravityFormsExtensions.php
+++ src/GravityFormsExtensions.php
@@ -112,6 +112,7 @@ class GravityFormsExtensions
         add_action('gform_post_form_duplicated', [ $this, 'p4_gf_duplicated_form' ], 10, 2);
         add_filter('gform_form_update_meta', [$this, 'p4_gf_enable_default_meta_settings'], 10, 1);
         add_filter('gform_form_post_get_meta', [$this, 'p4_gf_enable_default_meta_settings'], 10, 1);
+        add_filter('gform_hubspot_form_object_pre_save_feed', [$this, 'p4_gf_hb_form_object_pre_save_feed', 10, 1]);
 
         add_action('gform_stripe_fulfillment', [ $this, 'record_fulfillment_entry' ], 10, 2);
     }
@@ -750,5 +751,49 @@ class GravityFormsExtensions
         $meta['personalData']['retention']['retain_entries_days'] = 90;
         $meta['personalData']['exportingAndErasing']['enabled'] = true;
         return $meta;
+    }
+
+    /**
+     * Changes to the lifecycle stage in forms
+     * Changing how the lifecycle stage for CRM records is handled in forms.
+     * Instead of being a form field, the lifecycle stage will be included in the
+     * form settings in a new selectedExternalOptions field.
+     *
+     * More info: https://developers.hubspot.com/changelog/changes-to-the-lifecycle-stage-in-forms
+     *
+     * @param array $meta hubspot form.
+     *
+    */
+    public function p4_gf_hb_form_object_pre_save_feed(array $hs_form): array
+    {
+        if (! empty($hs_form['selectedExternalOptions'])) {
+            $lifecyclestage_id = '';
+            foreach ($hs_form['selectedExternalOptions'] as $key => $external_option) {
+                if (rgar($external_option, 'propertyName') !== 'lifecyclestage') {
+                    continue;
+                }
+
+                if (rgar($external_option, 'objectTypeId') === '0-1') {
+                    $lifecyclestage_id = rgar($external_option, 'id');
+                    continue;
+                }
+
+                if (rgar($external_option, 'objectTypeId') === '0-2') {
+                    $lifecyclestage_id = '';
+                    break;
+                }
+            }
+
+            if (! empty($lifecyclestage_id)) {
+                $hs_form['selectedExternalOptions'][] = array(
+                    'referenceType' => 'PIPELINE_STAGE',
+                    'objectTypeId' => '0-2',
+                    'propertyName' => 'lifecyclestage',
+                    'id' => $lifecyclestage_id,
+                );
+            }
+        }
+
+        return $hs_form;
     }
 }
