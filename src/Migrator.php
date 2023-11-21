6e191db1824f9c7ac1c8b14bbdb99b2fe2126220
Dan Tovbein <dtovbein@gmail.com>
2023-11-21 07:19:00 -0300

    Rename file
    - Change to 26

--- src/Migrator.php
+++ src/Migrator.php
@@ -27,7 +27,7 @@ use P4\MasterTheme\Migrations\M022UpdatePostRevisions;
 use P4\MasterTheme\Migrations\M023EnablePlanet4Blocks;
 use P4\MasterTheme\Migrations\M024RemoveNewIdentitySylesOption;
 use P4\MasterTheme\Migrations\M025CreateDefaultPostsPage;
-use P4\MasterTheme\Migrations\M025ReplaceDeprecatedColorsFromContent;
+use P4\MasterTheme\Migrations\M026ReplaceDeprecatedColorsFromContent;
 
 /**
  * Run any new migration scripts and record results in the log.
@@ -72,7 +72,7 @@ class Migrator
             M023EnablePlanet4Blocks::class,
             M024RemoveNewIdentitySylesOption::class,
             M025CreateDefaultPostsPage::class,
-            M025ReplaceDeprecatedColorsFromContent::class,
+            M026ReplaceDeprecatedColorsFromContent::class,
         ];
 
         // Loop migrations and run those that haven't run yet.
