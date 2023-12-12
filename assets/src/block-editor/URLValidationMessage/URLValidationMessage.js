991f3729784d2f798e8ca7b29d0c3e8eef701194
Dan Tovbein <dtovbein@gmail.com>
2023-12-12 15:14:39 -0300

    Create block-editor directory
    - Here we will include all components used by the editor

---
+++ assets/src/block-editor/URLValidationMessage/URLValidationMessage.js
@@ -0,0 +1,28 @@
+import {Component} from '@wordpress/element';
+
+export class URLValidationMessage extends Component {
+  isValid(url) {
+    if (!url) {
+      return true;
+    }
+
+    if (!url.toLowerCase().startsWith('https://')) {
+      return false;
+    }
+
+    return true;
+  }
+
+  render() {
+    const {__} = wp.i18n;
+    const {url} = this.props;
+
+    if (this.isValid(url)) {
+      return null;
+    }
+
+    return (
+      <span className="input_error">{ __('The URL must start with "https://"', 'planet4-blocks-backend') }</span>
+    );
+  }
+}
