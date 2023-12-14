04b97104a7cd9346a39120f0fa7035e8b90551d2
Dan Tovbein <dtovbein@greenpeace.org>
2023-12-14 07:09:40 -0300

    Maintenance: Add functions directory from plugin into the theme (#2177)
    Add functions directory
    
    - Include the `hydrateBlock.js` file for now 
    
    Co-authored-by: Dan Tovbein <dtovbein@gmail.com>

---
+++ assets/src/functions/hydrateBlock.js
@@ -0,0 +1,21 @@
+import {hydrateRoot} from 'react-dom/client';
+
+/**
+ * This function hydrate a SSR Component
+ * More info: https://reactjs.org/docs/react-dom.html#hydrate
+ *
+ * @param {*} blockName     refers to the block name
+ * @param {*} Component     It's the component that is going to be rendered
+ * @param {*} csrAttributes pass some attrs only to CSR (client-side rendering)
+ */
+export const hydrateBlock = (blockName, Component, csrAttributes = {}) => { // eslint-disable-line no-unused-vars
+  const blocks = document.querySelectorAll(`[data-hydrate="${blockName}"]`);
+  blocks.forEach(
+    blockNode => {
+      if (blockNode) {
+        const attributes = JSON.parse(blockNode.dataset.attributes);
+        hydrateRoot(blockNode, <Component {...attributes} {...csrAttributes} />);
+      }
+    }
+  );
+};
