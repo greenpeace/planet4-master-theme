f6ac81ae519e824aec6c9655db5b9daf04e58000
Dan Tovbein <dtovbein@greenpeace.org>
2024-01-02 11:16:01 -0300

    PLANET-7345: Disable Posts List Query Loop WP variation (#2182)
    Ref: https://jira.greenpeace.org/browse/PLANET-7345
    
    - Avoid naming confusions to editors since we have already
    - A Posts List P4 variation with the same name
    
    Co-authored-by: Dan Tovbein <dtovbein@gmail.com>

--- assets/src/blocks/editorIndex.js
+++ assets/src/blocks/editorIndex.js
@@ -1,6 +1,11 @@
 import {registerPostsListBlock} from './PostsList';
 import {registerActionsList} from './ActionsList';
 
-// Beta blocks
-registerActionsList();
-registerPostsListBlock();
+wp.domReady(() => {
+  // Make sure to unregister the posts-list native variation before registering planet4-blocks/posts-list-block
+  wp.blocks.unregisterBlockVariation('core/query', 'posts-list');
+
+  // Beta blocks
+  registerActionsList();
+  registerPostsListBlock();
+});
