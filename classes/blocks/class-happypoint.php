5100baec5e7fa60583a3367a6190f0f15a0f00f1
Dan Tovbein <dtovbein@greenpeace.org>
2024-01-04 08:42:32 -0300

    PLANET-7378: Make sure to not instantiate twice the Happypoint class (#1180)
    PLANET-7378: Make sure to not re-instantiate the Happypoint class
    
    Ref: https://jira.greenpeace.org/browse/PLANET-7378
    
    Co-authored-by: Dan Tovbein <dtovbein@gmail.com>

--- classes/blocks/class-happypoint.php
+++ classes/blocks/class-happypoint.php
@@ -8,6 +8,8 @@
 
 namespace P4GBKS\Blocks;
 
+use WP_Block_Type_Registry;
+
 /**
  * Class Happypoint_Controller
  *
@@ -27,6 +29,10 @@ class Happypoint extends Base_Block {
 	 * Happypoint constructor.
 	 */
 	public function __construct() {
+		if ( WP_Block_Type_Registry::get_instance()->is_registered( self::get_full_block_name() ) ) {
+			return;
+		}
+
 		register_block_type(
 			self::get_full_block_name(),
 			[
