3b3778c99b426b84a966ed5f739ef8ea8e6894d6
Dan Tovbein <dtovbein@gmail.com>
2024-01-23 08:55:08 -0300

    Validate if it is already registered
    - Getting some issues when the block is not registered by the editor script

--- assets/src/blocks/Happypoint/HappypointBlock.js
+++ assets/src/blocks/Happypoint/HappypointBlock.js
@@ -1,57 +1,60 @@
 import {HappypointEditor} from './HappypointEditor';
 import {HappypointBlock as HappypointBlockV1} from './deprecated/HappypointBlocklV1.js';
 
-const {registerBlockType} = wp.blocks;
+const {registerBlockType, getBlockType} = wp.blocks;
 
-export const registerHappypointBlock = () =>
-  registerBlockType('planet4-blocks/happypoint', {
-    title: 'Happypoint',
-    icon: 'format-image',
-    category: 'planet4-blocks',
-    supports: {
-      multiple: false, // Use the block just once per post.
-      html: false, // Disable "Edit as HTMl" block option.
-    },
-    attributes: {
-      focus_image: {
-        type: 'string',
+export const registerHappypointBlock = () => {
+  if (!getBlockType('planet4-blocks/happypoint')) {
+    registerBlockType('planet4-blocks/happypoint', {
+      title: 'Happypoint',
+      icon: 'format-image',
+      category: 'planet4-blocks',
+      supports: {
+        multiple: false, // Use the block just once per post.
+        html: false, // Disable "Edit as HTMl" block option.
       },
-      opacity: {
-        type: 'number',
-        default: 30,
+      attributes: {
+        focus_image: {
+          type: 'string',
+        },
+        opacity: {
+          type: 'number',
+          default: 30,
+        },
+        mailing_list_iframe: {
+          type: 'boolean',
+        },
+        iframe_url: {
+          type: 'string',
+        },
+        id: {
+          type: 'number',
+        },
+        load_iframe: {
+          type: 'boolean',
+        },
+        use_embed_code: {
+          type: 'boolean',
+        },
+        embed_code: {
+          type: 'string',
+        },
+        override_default_content: {
+          type: 'boolean',
+          default: false,
+        },
+        local_content_provider: {
+          type: 'string',
+          default: 'none',
+        },
       },
-      mailing_list_iframe: {
-        type: 'boolean',
+      edit: HappypointEditor,
+      save() {
+        return null;
       },
-      iframe_url: {
-        type: 'string',
-      },
-      id: {
-        type: 'number',
-      },
-      load_iframe: {
-        type: 'boolean',
-      },
-      use_embed_code: {
-        type: 'boolean',
-      },
-      embed_code: {
-        type: 'string',
-      },
-      override_default_content: {
-        type: 'boolean',
-        default: false,
-      },
-      local_content_provider: {
-        type: 'string',
-        default: 'none',
-      },
-    },
-    edit: HappypointEditor,
-    save() {
-      return null;
-    },
-    deprecated: [
-      HappypointBlockV1,
-    ],
-  });
+      deprecated: [
+        HappypointBlockV1,
+      ],
+    });
+  }
+};
