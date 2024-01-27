bd6144f3fe4068362d14c6183895669319f3d290
Dan Tovbein <dtovbein@gmail.com>
2024-01-27 12:09:34 -0300

    Follow up Wordpress 6.4
    - Modify the way of adding the layout dropdown options
    - Lock inner blocks when needed
    - Remove unnecesary attributes

--- assets/src/block-editor/QueryBlockExtension/QueryBlockExtension.js
+++ assets/src/block-editor/QueryBlockExtension/QueryBlockExtension.js
@@ -1,154 +1,125 @@
-import {ToolbarButton, RadioControl, PanelBody} from '@wordpress/components';
-import {InspectorControls, BlockControls} from '@wordpress/block-editor';
-import {useCallback, useEffect, useMemo} from '@wordpress/element';
+import {RadioControl, PanelBody} from '@wordpress/components';
+import {InspectorControls} from '@wordpress/block-editor';
+import {useCallback, useEffect, useMemo, useState} from '@wordpress/element';
 
 const {addFilter} = wp.hooks;
 const {__} = wp.i18n;
 
-const targetBlocks = ['core/query'];
+const targetBlocks = ['core/post-template'];
+
+const layoutTypes = [
+  {label: 'List', value: 'default', columnCount: 3},
+  {label: 'Grid', value: 'grid', columnCount: 3},
+  {label: 'Carousel', value: 'flex', columnCount: 6},
+];
 
 export const setupQueryBlockExtension = function() {
   const {createHigherOrderComponent} = wp.compose;
 
   addFilter(
     'editor.BlockEdit',
-    'planet4-blocks/overrides/query-controls',
+    'planet4-blocks/overrides/query-loop-layout',
     createHigherOrderComponent(
       BlockEdit => props => {
-        if (!targetBlocks.includes(props.name)) {
-          return (
-            <BlockEdit {...props} />
-          );
-        }
+        if (props.attributes.namespace === 'planet4-blocks/actions-list' || targetBlocks.includes(props.name)) {
+          const [isPostTemplate, setPostTemplate] = useState();
+          const [selectedBlock, setSelectedBlock] = useState();
+          const {attributes, setAttributes} = props;
 
-        const {attributes, setAttributes} = props;
+          const onChangeHandler = useCallback(type => {
+            const {className, query} = attributes;
 
-        const isSelectedBlock = namespace => {
-          const selectedBlock = wp.data.select('core/block-editor').getSelectedBlock();
-          return (selectedBlock && selectedBlock.attributes.namespace === namespace);
-        };
+            const pattern = 'is-custom-layout-';
+            const layoutType = layoutTypes.find(t => t.value === type);
 
-        const onChangeHandler = useCallback(view => {
-          setAttributes({
-            displayLayout: {
-              type: view,
-              columns: view === 'carousel' ? 6 : 3,
-            },
-            query: {
-              ...attributes.query,
-              perPage: view === 'carousel' ? 6 : 3,
-            },
-          });
-        }, []);
+            if (layoutType) {
+              setAttributes({
+                layout: {
+                  type: layoutType.value,
+                  columnCount: layoutType.columnCount,
+                },
+                query: {
+                  ...query,
+                  perPage: layoutType.columnCount,
+                },
+                className: ((className.includes(pattern)) ?
+                  className.replace(/\is-custom-layout-.*/, `${pattern}${layoutType.label}`) :
+                  `${className} ${pattern}${layoutType.label}`).toLowerCase(),
+              });
+            }
+          }, [attributes]);
 
-        useEffect(() => {
-          const pattern = 'is-custom-layout-';
-          const {className, displayLayout} = attributes;
-
-          if (typeof className !== 'undefined') {
-            setAttributes({
-              className: (className.includes(pattern)) ?
-                className.replace(/\is-custom-layout-.*/, `${pattern}${displayLayout.type}`) :
-                `${className} ${pattern}${displayLayout.type}`,
-            });
-          }
-        }, [
-          attributes,
-          setAttributes,
-        ]);
-
-        useEffect(() => {
-          // Reset in case of retrieving wrong values
-          if (attributes.displayLayout.type === 'flex' && attributes.displayLayout.columns === 6) {
-            setAttributes({
-              displayLayout: {
-                ...attributes.displayLayout,
-                columns: 3,
-              },
-              query: {
-                ...attributes.query,
-                perPage: 3,
-              },
-            });
-          }
-        }, [attributes.displayLayout.type]);
-
-        useEffect(() => {
-          if (props.isSelected && isSelectedBlock('planet4-blocks/actions-list')) {
-            const selectedBlock = wp.data.select('core/block-editor').getSelectedBlock();
-
-            const timeout = setTimeout(() => {
-              const carouselBtn = document.querySelector('#carousel-toolbar-button');
-              let viewsGroup;
-
-              // Find all toolbar groups only when the block is selected
-              for (const group of document.querySelectorAll('.components-toolbar-group')) {
-                // Find the views group from toolbar
-                if (group.innerHTML.includes('List view') || group.innerHTML.includes('Grid view')) {
-                  viewsGroup = group;
-
-                  // Remove the list view for Actions List block
-                  if (selectedBlock.attributes.namespace && selectedBlock.attributes.namespace.includes('planet4-blocks/actions-list')) {
-                    for (const children of group.children) {
-                      if (children.ariaLabel.includes('List view')) {
-                        group.removeChild(children);
-                      }
-                    }
+          useEffect(() => {
+            if (isPostTemplate) {
+              const timeout = setTimeout(() => {
+                // Find all toolbar groups only when the block is selected
+                for (const group of document.querySelectorAll('.components-toolbar-group')) {
+                  // Find the views group from toolbar
+                  if (group.innerHTML.includes('List view') || group.innerHTML.includes('Grid view')) {
+                    group.style.display = 'none';
                   }
                 }
+              }, 10);
 
-                // Move the carousel button to the views group if exists
-                if (carouselBtn && group.contains(carouselBtn) && viewsGroup) {
-                  viewsGroup.append(group);
-                }
+              return () => {
+                clearTimeout(timeout);
+              };
+            }
+          }, [isPostTemplate]);
+
+          useEffect(() => {
+            if (selectedBlock && selectedBlock.name === 'core/post-template') {
+              const parentBlock = wp.data.select('core/block-editor')
+                .getBlock(wp.data.select('core/block-editor')
+                  .getBlockParents(selectedBlock.clientId)[0]);
+
+              if (parentBlock && parentBlock.attributes.namespace === 'planet4-blocks/actions-list') {
+                setPostTemplate(selectedBlock);
               }
-            }, 50);
+            }
+          }, [selectedBlock]);
 
-            return () => {
-              clearTimeout(timeout);
-            };
-          }
-        }, [props.isSelected]);
+          useEffect(() => {
+            // Reset every time a new block is selected
+            setPostTemplate(null);
+            setSelectedBlock(wp.data.select('core/block-editor').getSelectedBlock());
+          }, [
+            wp.data.select('core/block-editor').getSelectedBlock(),
+          ]);
 
-        return useMemo(() => (
-          <>
-            {isSelectedBlock('planet4-blocks/actions-list') && (
-              <>
-                <InspectorControls>
-                  <PanelBody
-                    title={__('Layout view', 'planet4-blocks-backend')}
-                    initialOpen={true}
-                  >
-                    <RadioControl
-                      label={__('Display as', 'planet4-blocks-backend')}
-                      help={__('The layout of the action|post list', 'planet4-blocks-backend')}
-                      selected={attributes.displayLayout.type}
-                      options={[
-                        {label: 'Grid', value: 'flex'},
-                        {label: 'Carousel', value: 'carousel'},
-                      ]}
-                      onChange={value => {
-                        onChangeHandler(value);
-                      }}
-                    />
-                  </PanelBody>
-                </InspectorControls>
-                <BlockControls group="other">
-                  <ToolbarButton
-                    id="carousel-toolbar-button"
-                    icon="slides"
-                    label={__('Carousel view', 'core-block-custom-attributes')}
-                    isActive={attributes.displayLayout.type === 'carousel'}
-                    onClick={() => {
-                      onChangeHandler('carousel');
-                    }}
-                  />
-                </BlockControls>
-              </>
-            )}
-            <BlockEdit {...props} />
-          </>
-        ), [attributes, wp.data.select('core/block-editor').getSelectedBlock()]);
+          return useMemo(() => (
+            <>
+              <InspectorControls>
+                <PanelBody
+                  title={__('Layout view', 'planet4-blocks-backend')}
+                  initialOpen={true}
+                >
+                  {attributes.layout && (
+                    <>
+                      <RadioControl
+                        label={__('Display as', 'planet4-blocks-backend')}
+                        help={__('The layout of the action|post list', 'planet4-blocks-backend')}
+                        selected={attributes.layout.type}
+                        options={layoutTypes}
+                        onChange={type => onChangeHandler(type)}
+                      />
+                    </>
+                  )}
+                </PanelBody>
+              </InspectorControls>
+              <BlockEdit {...props} />
+            </>
+          ), [
+            attributes,
+            isPostTemplate,
+            selectedBlock,
+            wp.data.select('core/block-editor').getSelectedBlock(),
+          ]);
+        }
+
+        return (
+          <BlockEdit {...props} />
+        );
       }
     )
   );
