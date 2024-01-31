a23dee89b41b6e00ca5a387078627b1d2b86c636
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2024-01-31 15:31:08 +0530

    PLANET-7379 Move SEO Sidebar settings to master theme
    Ref. https://jira.greenpeace.org/browse/PLANET-7379

---
+++ assets/src/block-editor/Sidebar/SearchEngineOptimizationsSidebar.js
@@ -0,0 +1,29 @@
+import {PluginDocumentSettingPanel} from '@wordpress/edit-post';
+import {useDispatch, useSelect} from '@wordpress/data';
+import {URLInput} from '../URLInput/URLInput';
+
+const CANONICAL_URL = 'p4_seo_canonical_url';
+
+const {__} = wp.i18n;
+
+export const SearchEngineOptimizationsSidebar = {
+  getId: () => 'planet4-seo-sidebar',
+  render: () => {
+    const meta = useSelect(select => select('core/editor').getEditedPostAttribute('meta'), []);
+    const {editPost} = useDispatch('core/editor', [meta[CANONICAL_URL]]);
+
+    return (
+      <PluginDocumentSettingPanel
+        name="planet4-search-engine-optimizations"
+        title={__('Search Engine Optimizations', 'planet4-blocks-backend')}
+      >
+        <URLInput
+          label={__('Canonical link', 'planet4-blocks-backend')}
+          value={meta[CANONICAL_URL]}
+          onChange={value => editPost({meta: {[CANONICAL_URL]: value}})}
+          help={__('If emtpy a self-reference canonical link will be used', 'planet4-blocks-backend')}
+        />
+      </PluginDocumentSettingPanel>
+    );
+  },
+};
