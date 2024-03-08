9c225c573ddf6d37dd7768abc682cfe8784d0605
Maud Leray <maudleray035@gmail.com>
2024-03-08 11:28:23 +0100

    Fix PHP tests
    These are failing with the new check in place

--- tests/test-category-page.php
+++ tests/test-category-page.php
@@ -29,11 +29,6 @@ class CategoryPageTest extends P4TestCase
 
         $post_data = $this->get_posts()['post_with_category_tag_custom_term'];
         $post = $this->factory->post->create_and_get($post_data);
-        $attachment_id = $this->factory->attachment->create_upload_object(
-            dirname(__DIR__) . '/tests/data/images/pressmedia.jpg',
-            0
-        );
-        set_post_thumbnail($post, $attachment_id);
 
         // Wrap WP_Post around Post.
         $post = new Post($post->ID);
@@ -142,6 +137,10 @@ class CategoryPageTest extends P4TestCase
      */
     public function get_posts(): array
     {
+        $attachment_id = $this->factory->attachment->create_upload_object(
+            dirname(__DIR__) . '/tests/data/images/pressmedia.jpg',
+            0
+        );
 
         return [
 
@@ -159,6 +158,9 @@ class CategoryPageTest extends P4TestCase
                 'tax_input' => [
                     'p4-page-type' => [ 'story' ],
                 ],
+                'meta_input' => [
+                    '_thumbnail_id' => $attachment_id,
+                ]
             ],
             'post_with_category_tag' => [
                 'post_type' => 'post',
@@ -171,6 +173,9 @@ class CategoryPageTest extends P4TestCase
                 'tags_input' => [
                     'arcticsunrise',
                 ],
+                'meta_input' => [
+                    '_thumbnail_id' => $attachment_id,
+                ]
             ],
             'post_with_category' => [
                 'post_type' => 'post',
@@ -180,6 +185,9 @@ class CategoryPageTest extends P4TestCase
                 'post_category' => [
                     get_category_by_slug('nature')->term_id,
                 ],
+                'meta_input' => [
+                    '_thumbnail_id' => $attachment_id,
+                ]
             ],
         ];
     }
