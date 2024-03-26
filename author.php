5e3dac978ec25bcdec4336c47de0b82553a5f3c2
lithrel <lithrel@randomdomainname.net>
2024-03-26 18:42:39 +0100

    PLANET-7431: Author profile page returns all posts by Editor
    Ref: https://jira.greenpeace.org/browse/PLANET-7431

--- author.php
+++ author.php
@@ -17,24 +17,16 @@ use Timber\Timber;
 
 $context = Timber::get_context();
 
-$post_args = [
-    'posts_per_page' => 10,
-    'post_type' => 'post',
-    'paged' => 1,
-    'meta_key' => 'p4_author_override',
-    'meta_compare' => 'NOT EXISTS',
-    'has_password' => false, // Skip password protected content.
-];
-
 if (isset($wp_query->query_vars['author'])) {
     $author = new User($wp_query->query_vars['author']);
     $context['author'] = $author;
     $context['title'] = 'Author Archives: ' . $author->name();
-    $post_args['author'] = $wp_query->query_vars['author'];
 
     $context['page_category'] = 'Author Page';
 
-    $context['social_accounts'] = Post::filter_social_accounts($context['footer_social_menu'] ?: []);
+    $context['social_accounts'] = Post::filter_social_accounts(
+        $context['footer_social_menu'] ?: []
+    );
     $context['og_title'] = $author->name;
     $context['og_description'] = get_the_author_meta('description', $author->ID);
     $context['og_image_data'] = [
@@ -49,12 +41,6 @@ if (isset($wp_query->query_vars['author'])) {
     $author_share_buttons->link = $author->link;
     $context['author_share_buttons'] = $author_share_buttons;
 }
-
-// Adjust global query to exclude author override.
-// We can remove this once we convert author overrides to actual users.
-$wp_query->query_vars['meta_key'] = 'p4_author_override';
-$wp_query->query_vars['meta_compare'] = 'NOT EXISTS';
-$wp_query->query_vars['has_password'] = false;
 
 $templates = ['author.twig', 'archive.twig'];
 $page = new ListingPage($templates, $context);
