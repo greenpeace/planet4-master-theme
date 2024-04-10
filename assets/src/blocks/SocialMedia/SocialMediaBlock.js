f15517b752a8d9f9ab1ed590976f0d0d49938540
Maud Leray <maudleray035@gmail.com>
2024-04-10 08:53:49 +0200

    PLANET-7492 Make sure blocks are not registered twice (#2251)
    The blocks registered via the editorIndex file were sometimes registered twice

--- assets/src/blocks/SocialMedia/SocialMediaBlock.js
+++ assets/src/blocks/SocialMedia/SocialMediaBlock.js
@@ -3,47 +3,55 @@ import {socialMediaV1} from './deprecated/socialMediaV1';
 import {OEMBED_EMBED_TYPE, FACEBOOK_PAGE_TAB_TIMELINE} from './SocialMediaConstants.js';
 import {SocialMediaFrontend} from './SocialMediaFrontend.js';
 
-const {registerBlockType} = wp.blocks;
+const {registerBlockType, getBlockTypes} = wp.blocks;
 
 const BLOCK_NAME = 'planet4-blocks/social-media';
 
-export const registerSocialMediaBlock = () => registerBlockType(BLOCK_NAME, {
-  title: 'Social Media',
-  icon: 'share',
-  category: 'planet4-blocks',
-  attributes: {
-    title: {
-      type: 'string',
-      default: '',
+export const registerSocialMediaBlock = () => {
+  const blockAlreadyExists = getBlockTypes().find(block => block.name === BLOCK_NAME);
+
+  if (blockAlreadyExists) {
+    return;
+  }
+
+  registerBlockType(BLOCK_NAME, {
+    title: 'Social Media',
+    icon: 'share',
+    category: 'planet4-blocks',
+    attributes: {
+      title: {
+        type: 'string',
+        default: '',
+      },
+      description: {
+        type: 'string',
+        default: '',
+      },
+      embed_type: {
+        type: 'string',
+        default: OEMBED_EMBED_TYPE,
+      },
+      facebook_page_tab: {
+        type: 'string',
+        default: FACEBOOK_PAGE_TAB_TIMELINE,
+      },
+      social_media_url: {
+        type: 'string',
+        default: '',
+      },
+      alignment_class: {
+        type: 'string',
+        default: '',
+      },
+      embed_code: {
+        type: 'string',
+        default: '',
+      },
     },
-    description: {
-      type: 'string',
-      default: '',
-    },
-    embed_type: {
-      type: 'string',
-      default: OEMBED_EMBED_TYPE,
-    },
-    facebook_page_tab: {
-      type: 'string',
-      default: FACEBOOK_PAGE_TAB_TIMELINE,
-    },
-    social_media_url: {
-      type: 'string',
-      default: '',
-    },
-    alignment_class: {
-      type: 'string',
-      default: '',
-    },
-    embed_code: {
-      type: 'string',
-      default: '',
-    },
-  },
-  edit: SocialMediaEditor,
-  save: ({attributes}) => <SocialMediaFrontend {...attributes} />,
-  deprecated: [
-    socialMediaV1,
-  ],
-});
+    edit: SocialMediaEditor,
+    save: ({attributes}) => <SocialMediaFrontend {...attributes} />,
+    deprecated: [
+      socialMediaV1,
+    ],
+  });
+};
--- assets/src/blocks/TakeActionBoxout/TakeActionBoxoutBlock.js
+++ assets/src/blocks/TakeActionBoxout/TakeActionBoxoutBlock.js
@@ -1,64 +1,72 @@
 import {TakeActionBoxoutEditor} from './TakeActionBoxoutEditor.js';
 import {takeActionBoxoutV1} from './deprecated/takeActionBoxoutV1';
 
-const {registerBlockType} = wp.blocks;
+const {registerBlockType, getBlockTypes} = wp.blocks;
 
 const BLOCK_NAME = 'planet4-blocks/take-action-boxout';
 
-export const registerTakeActionBoxoutBlock = () => registerBlockType(BLOCK_NAME, {
-  title: 'Take Action Boxout',
-  icon: 'welcome-widgets-menus',
-  category: 'planet4-blocks',
-  supports: {
-    html: false, // Disable "Edit as HTMl" block option.
-  },
-  // This attributes definition mimics the one in the PHP side.
-  attributes: {
-    take_action_page: {
-      type: 'number',
+export const registerTakeActionBoxoutBlock = () => {
+  const blockAlreadyExists = getBlockTypes().find(block => block.name === BLOCK_NAME);
+
+  if (blockAlreadyExists) {
+    return;
+  }
+
+  registerBlockType(BLOCK_NAME, {
+    title: 'Take Action Boxout',
+    icon: 'welcome-widgets-menus',
+    category: 'planet4-blocks',
+    supports: {
+      html: false, // Disable "Edit as HTMl" block option.
     },
-    title: {
-      type: 'string',
+    // This attributes definition mimics the one in the PHP side.
+    attributes: {
+      take_action_page: {
+        type: 'number',
+      },
+      title: {
+        type: 'string',
+      },
+      excerpt: {
+        type: 'string',
+      },
+      link: {
+        type: 'string',
+      },
+      linkText: {
+        type: 'string',
+      },
+      newTab: {
+        type: 'boolean',
+        default: false,
+      },
+      tag_ids: {
+        type: 'array',
+        default: [],
+      },
+      imageId: {
+        type: 'number',
+        default: '',
+      },
+      imageUrl: {
+        type: 'string',
+        default: '',
+      },
+      imageAlt: {
+        type: 'string',
+        default: '',
+      },
+      stickyOnMobile: {
+        type: 'boolean',
+        default: false,
+      },
     },
-    excerpt: {
-      type: 'string',
+    edit: TakeActionBoxoutEditor,
+    save() {
+      return null;
     },
-    link: {
-      type: 'string',
-    },
-    linkText: {
-      type: 'string',
-    },
-    newTab: {
-      type: 'boolean',
-      default: false,
-    },
-    tag_ids: {
-      type: 'array',
-      default: [],
-    },
-    imageId: {
-      type: 'number',
-      default: '',
-    },
-    imageUrl: {
-      type: 'string',
-      default: '',
-    },
-    imageAlt: {
-      type: 'string',
-      default: '',
-    },
-    stickyOnMobile: {
-      type: 'boolean',
-      default: false,
-    },
-  },
-  edit: TakeActionBoxoutEditor,
-  save() {
-    return null;
-  },
-  deprecated: [
-    takeActionBoxoutV1,
-  ],
-});
+    deprecated: [
+      takeActionBoxoutV1,
+    ],
+  });
+};
