1f03a5c20196f9e14de24c016b2d2e3481858bb4
Dan Tovbein <dtovbein@gmail.com>
2024-04-25 07:26:52 -0300

    Include group-stretched-link block variations
    - Also required on this implementation

---
+++ assets/src/block-variations/index.js
@@ -0,0 +1,22 @@
+const {registerBlockVariation} = wp.blocks;
+const {__} = wp.i18n;
+
+/*
+ This file will register general block variations. In case of register a very
+ specific block variation, such as PostsList or ActionsList ,you can create an isolated file.
+*/
+
+export const registerBlockVariations = () => {
+  // Group Stretched Link variation
+  registerBlockVariation('core/group', {
+    name: 'group-stretched-link',
+    title: __('Stretched Link', 'planet4-blocks-backend'),
+    description: __('Make the entire block contents clickable, using the first link inside.', 'planet4-blocks-backend'),
+    attributes: {className: 'group-stretched-link'},
+    scope: ['inserter', 'transform'],
+    isActive: blockAttributes => {
+      return blockAttributes.className === 'group-stretched-link';
+    },
+    icon: 'admin-links',
+  });
+};
