b409c114e965787e8fb94521d1bc0ecb98167ee2
Dan Tovbein <dtovbein@gmail.com>
2024-05-03 12:17:45 -0300

    Fix Analytics Tracking Sidebar component
    - It should be merged before we move to WP6.5

--- assets/src/block-editor/Sidebar/AnalyticsTrackingSidebar.js
+++ assets/src/block-editor/Sidebar/AnalyticsTrackingSidebar.js
@@ -1,3 +1,4 @@
+import {useEffect} from '@wordpress/element';
 import {PluginDocumentSettingPanel} from '@wordpress/edit-post';
 import {dispatch, useSelect} from '@wordpress/data';
 import {SelectSidebarField} from '../SidebarFields/SelectSidebarField';
@@ -6,54 +7,46 @@ import {getSidebarFunctions} from './getSidebarFunctions';
 
 const {__} = wp.i18n;
 
-dispatch('core').addEntities([{
-  baseURL: '/planet4/v1/analytics-values',
-  kind: 'planet4/v1',
-  name: 'analytics-values',
-  label: 'Analytics and tracking values',
-}]);
-
-const GLOBAL_PROJECT = 'p4_campaign_name';
-const LOCAL_PROJECT = 'p4_local_project';
-const BASKET = 'p4_basket_name';
-const DEPARTMENT = 'p4_department';
-
 export const AnalyticsTrackingSidebar = {
   getId: () => 'planet4-analytics-sidebar',
   render: () => {
     const {getParams} = getSidebarFunctions();
     const postId = wp.data.select('core/editor').getCurrentPostId();
     const options = useSelect(select => {
-      return select('core').getEntityRecords('planet4/v1', 'analytics-values', {id: postId});
+      return select('core').getEntityRecord('planet4/v1', 'analytics-values');
     });
 
-    const globalOptions = options ? options[0].global_projects : [];
-    const localOptions = options ? options[0].local_projects : [];
-    const basketOptions = options ? options[0].baskets : [];
+    useEffect(() => {
+      dispatch('core').addEntities([{
+        baseURL: '/planet4/v1/analytics-values',
+        baseURLParams: {post_id: postId},
+        kind: 'planet4/v1',
+        name: 'analytics-values',
+        label: 'Analytics and tracking values',
+      }]);
+    }, [postId]);
 
-    return (
-      <>
-        <PluginDocumentSettingPanel
-          name="analytics-panel"
-          title={__('Analytics & Tracking', 'planet4-blocks-backend')}
-        >
-          <SelectSidebarField
-            label={__('Global Project', 'planet4-master-theme-backend')}
-            options={globalOptions}
-            {...getParams(GLOBAL_PROJECT)} />
-          <SelectSidebarField
-            label={__('Local Projects', 'planet4-master-theme-backend')}
-            options={localOptions}
-            {...getParams(LOCAL_PROJECT)} />
-          <SelectSidebarField
-            label={__('Basket Name', 'planet4-master-theme-backend')}
-            options={basketOptions}
-            {...getParams(BASKET)} />
-          <TextSidebarField
-            label={__('Department', 'planet4-master-theme-backend')}
-            {...getParams(DEPARTMENT)} />
-        </PluginDocumentSettingPanel>
-      </>
+    return options && (
+      <PluginDocumentSettingPanel
+        name="analytics-panel"
+        title={__('Analytics & Tracking', 'planet4-blocks-backend')}
+      >
+        <SelectSidebarField
+          label={__('Global Project', 'planet4-master-theme-backend')}
+          options={options.global_projects || []}
+          {...getParams('p4_campaign_name')} />
+        <SelectSidebarField
+          label={__('Local Projects', 'planet4-master-theme-backend')}
+          options={options.local_projects || []}
+          {...getParams('p4_local_project')} />
+        <SelectSidebarField
+          label={__('Basket Name', 'planet4-master-theme-backend')}
+          options={options.baskets || []}
+          {...getParams('p4_basket_name')} />
+        <TextSidebarField
+          label={__('Department', 'planet4-master-theme-backend')}
+          {...getParams('p4_department')} />
+      </PluginDocumentSettingPanel>
     );
   },
 };
