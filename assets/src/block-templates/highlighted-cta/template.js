3cf783974b8963dd3fcd7dcd07169135d22600f7
Dan Tovbein <dtovbein@gmail.com>
2024-05-07 08:15:13 -0300

    Update template
    - with the recently merged from plugin

--- assets/src/block-templates/highlighted-cta/template.js
+++ assets/src/block-templates/highlighted-cta/template.js
@@ -6,30 +6,40 @@ const template = ({
   titlePlaceholder = __('Enter text', 'planet4-blocks-backend'),
 }) => [
   [
-    'core/columns',
+    'core/group',
     {
       className: 'block',
-      textColor: 'white',
       backgroundColor: 'dark-green-800',
     },
     [
-      ['core/column', {}, [
-        ['core/image', {
-          align: 'center',
-          className: 'force-no-lightbox force-no-caption',
-          url: `${mainThemeUrl}/images/placeholders/placeholder-80x80.jpg`,
-        }],
-        ['core/heading', {
-          textAlign: 'center',
-          level: 3,
-          placeholder: titlePlaceholder,
-        }],
-        ['core/spacer', {height: '16px'}],
-        ['core/buttons', {layout: {type: 'flex', justifyContent: 'center'}}, [
-          ['core/button', {className: 'is-style-transparent'}],
-        ]],
-        ['core/spacer', {height: '16px'}],
+      ['core/spacer', {height: '40px'}],
+      ['core/image', {
+        align: 'center',
+        className: 'force-no-lightbox force-no-caption',
+        url: `${mainThemeUrl}/images/placeholders/placeholder-80x80.jpg`,
+        width: '80px',
+        height: '80px',
+        style: {
+          spacing: {
+            margin: {
+              top: '0',
+              bottom: '0',
+            },
+          },
+        },
+      }],
+      ['core/spacer', {height: '32px'}],
+      ['core/heading', {
+        textAlign: 'center',
+        level: 3,
+        placeholder: titlePlaceholder,
+        textColor: 'white',
+      }],
+      ['core/spacer', {height: '24px'}],
+      ['core/buttons', {layout: {type: 'flex', justifyContent: 'center'}}, [
+        ['core/button', {className: 'is-style-cta'}],
       ]],
+      ['core/spacer', {height: '40px'}],
     ],
   ],
 ];
