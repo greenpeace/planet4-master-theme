5e112b605a49a0739e39770b693ec3ec7aab1add
Osong Agberndifor <38656549+Osong-Michael@users.noreply.github.com>
2024-05-07 10:23:44 +0100

    PLANET-7399 Move Reality Check block pattern into master theme (#2277)
    - Moved all neccessary files for Reality Check Patternn to Master theme

---
+++ assets/src/block-templates/reality-check/template.js
@@ -0,0 +1,33 @@
+import mainThemeUrl from '../main-theme-url';
+
+const {__} = wp.i18n;
+
+const column = ['core/column', {}, [
+  ['core/group', {}, [
+    ['core/image', {
+      align: 'center',
+      className: 'mb-0 force-no-lightbox force-no-caption',
+      url: `${mainThemeUrl}/images/placeholders/placeholder-75x75.jpg`,
+    }],
+    ['core/heading', {
+      style: {typography: {fontSize: '4rem'}},
+      textAlign: 'center',
+      placeholder: __('Enter title', 'planet4-blocks-backend'),
+    }],
+    ['core/paragraph', {
+      align: 'center',
+      placeholder: __('Enter description', 'planet4-blocks-backend'),
+    }],
+    ['core/spacer', {height: '16px'}],
+  ]],
+]];
+
+const template = () => ([
+  ['core/columns', {
+    className: 'block',
+  },
+  [...Array(3).keys()].map(() => column),
+  ],
+]);
+
+export default template;
--- assets/src/block-templates/template-list.js
+++ assets/src/block-templates/template-list.js
@@ -1,7 +1,9 @@
 import * as sideImgTextCta from './side-image-with-text-and-cta';
 import * as issues from './issues';
+import * as realityCheck from './reality-check';
 
 export default [
   sideImgTextCta,
   issues,
+  realityCheck,
 ];
