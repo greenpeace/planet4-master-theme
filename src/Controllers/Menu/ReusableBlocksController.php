1b1e91c16530a067348b41a8bf3397499c257262
Dan Tovbein <dtovbein@gmail.com>
2024-06-26 10:47:24 -0300

    Ref: https://jira.greenpeace.org/browse/PLANET-7528
    Add reusable blocks controller
    
    - Potentially add small changes

--- src/Controllers/Menu/ReusableBlocksController.php
+++ src/Controllers/Menu/ReusableBlocksController.php
@@ -1,43 +1,40 @@
 <?php
-
 /**
  * Reusable Blocks class
+ *
+ * @package P4\MasterTheme\Controllers\Menu;
  */
 
 namespace P4\MasterTheme\Controllers\Menu;
 
-/**
- * Class Reusable_Blocks_Controller
- */
-class ReusableBlocksController extends Controller
-{
-    /**
-     * Post type name.
-     */
-    private const POST_TYPE = 'wp_block';
+if ( ! class_exists( 'ReusableBlocksController' ) ) {
 
-    /**
-     * Create menu/submenu entry.
-     */
-    public function create_admin_menu(): void
-    {
+	/**
+	 * Class ReusableBlocksController
+	 */
+	class ReusableBlocksController extends Controller {
 
-        $current_user = wp_get_current_user();
+		/**
+		 * Post type name.
+		 */
+		const POST_TYPE = 'wp_block';
 
-        if (
-            ( !in_array('administrator', $current_user->roles, true) &&
-            !in_array('editor', $current_user->roles, true) ) ||
-            !current_user_can('edit_posts')
-        ) {
-            return;
-        }
+		/**
+		 * Create menu/submenu entry.
+		 */
+		public function create_admin_menu() {
 
-        add_submenu_page(
-            BlocksReportController::P4BKS_REPORTS_SLUG_NAME,
-            __('All Reusable blocks', 'planet4-blocks-backend'),
-            __('All Reusable blocks', 'planet4-blocks-backend'),
-            'edit_posts',
-            'edit.php?post_type=' . self::POST_TYPE
-        );
-    }
+			$current_user = wp_get_current_user();
+
+			if ( ( in_array( 'administrator', $current_user->roles, true ) || in_array( 'editor', $current_user->roles, true ) ) && current_user_can( 'edit_posts' ) ) {
+				add_submenu_page(
+					P4GBKS_PLUGIN_SLUG_NAME,
+					__( 'All Reusable blocks', 'planet4-blocks-backend' ),
+					__( 'All Reusable blocks', 'planet4-blocks-backend' ),
+					'edit_posts',
+					'edit.php?post_type=' . self::POST_TYPE
+				);
+			}
+		}
+	}
 }
