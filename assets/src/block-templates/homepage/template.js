302ec9b61d00481992aef480615af9c9802b0c35
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2024-07-12 14:07:49 +0530

    PLANET-7553 Move Homepage block pattern layout into master theme
    Ref. https://jira.greenpeace.org/browse/PLANET-7553

---
+++ assets/src/block-templates/homepage/template.js
@@ -0,0 +1,33 @@
+import gravityFormWithText from '../templates/gravity-form-with-text';
+
+const {__} = wp.i18n;
+
+const template = () => (
+  [
+    ['core/group', {}, [
+      ['planet4-blocks/carousel-header'],
+      ['planet4-block-templates/issues', {
+        title: __('The issues we work on', 'planet4-blocks'),
+      }],
+      ['core/spacer', {height: '88px'}],
+      ['planet4-blocks/articles', {
+        article_heading: __('Read our Stories', 'planet4-blocks'),
+      }],
+      ['core/spacer', {height: '56px'}],
+      ['planet4-block-templates/side-image-with-text-and-cta', {
+        title: __('Get to know us', 'planet4-blocks'),
+      }],
+      ['core/spacer', {height: '30px'}],
+      ['planet4-block-templates/side-image-with-text-and-cta', {
+        title: __('We win campaigns', 'planet4-blocks'),
+        mediaPosition: 'right',
+      }],
+      ['core/spacer', {height: '56px'}],
+      ['planet4-blocks/covers'],
+      ['core/spacer', {height: '72px'}],
+      gravityFormWithText(),
+    ]],
+  ]
+);
+
+export default template;
