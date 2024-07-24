2eb06c5f427a641a2056e2826716994b4b6b04c3
Maud Leray <maudleray035@gmail.com>
2024-07-24 11:39:20 +0200

    Simplify code after PR review
    This is much shorter

--- assets/src/js/query-no-posts.js
+++ assets/src/js/query-no-posts.js
@@ -1,11 +1,11 @@
 export const removeNoPostText = () => {
   const postTitle = document.querySelector('.p4-query-loop .wp-block-heading');
   const postDescription = document.querySelector('.p4-query-loop p');
+  const noResultsText = document.querySelector('.p4-query-loop .wp-block-query-no-results');
 
-  if (!postTitle || !postDescription || postTitle.innerHTML !== '' || postDescription.innerHTML !== '') {
+  if (!noResultsText || postTitle?.innerHTML !== '' || postDescription?.innerHTML !== '') {
     return;
   }
 
-  const noResultsText = document.querySelector('.p4-query-loop .wp-block-query-no-results');
   noResultsText.style.display = 'none';
 };
