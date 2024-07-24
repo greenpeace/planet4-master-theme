4ba3818743ee42fe87b32d5ad59afe71bd68383a
Maud Leray <maudleray035@gmail.com>
2024-07-24 13:08:39 +0200

    Make sure 'no post text' exists before manipulating it (#2329)
    Otherwise we get a console error that it is undefined

--- assets/src/js/query-no-posts.js
+++ assets/src/js/query-no-posts.js
@@ -1,9 +1,11 @@
 export const removeNoPostText = () => {
   const postTitle = document.querySelector('.p4-query-loop .wp-block-heading');
   const postDescription = document.querySelector('.p4-query-loop p');
+  const noResultsText = document.querySelector('.p4-query-loop .wp-block-query-no-results');
 
-  if (postTitle.innerHTML === '' && postDescription.innerHTML === '') {
-    const noResultsText = document.querySelector('.p4-query-loop .wp-block-query-no-results');
-    noResultsText.style.display = 'none';
+  if (!noResultsText || postTitle?.innerHTML !== '' || postDescription?.innerHTML !== '') {
+    return;
   }
+
+  noResultsText.style.display = 'none';
 };
