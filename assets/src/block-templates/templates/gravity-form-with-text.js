e1f3afdc0940c4787f0c736f2350d78505fd4d19
Maud Leray <maudleray035@gmail.com>
2024-07-24 15:32:12 +0200

    PLANET-7552 Move High Level Topic block pattern layout into master theme
    We also remove it from the plugin

---
+++ assets/src/block-templates/templates/gravity-form-with-text.js
@@ -0,0 +1,36 @@
+const {__} = wp.i18n;
+
+const gravityFormWithText = backgroundColor => (
+  ['core/group', {
+    backgroundColor: backgroundColor || 'beige-100',
+    align: 'full',
+    style: {
+      spacing: {
+        padding: {
+          top: '80px',
+          bottom: '50px',
+        },
+      },
+    },
+  }, [
+    ['core/group', {className: 'container'}, [
+      ['core/columns', {}, [
+        ['core/column', {}, [
+          ['core/heading', {
+            level: 2,
+            style: {typography: {fontSize: '40px'}},
+            placeholder: __('Enter title', 'planet4-blocks-backend'),
+          }],
+          ['core/paragraph', {
+            placeholder: __('Enter description', 'planet4-blocks-backend'),
+          }],
+        ]],
+        ['core/column', {}, [
+          ['gravityforms/form', {title: false, description: false}],
+        ]],
+      ]],
+    ]],
+  ]]
+);
+
+export default gravityFormWithText;
