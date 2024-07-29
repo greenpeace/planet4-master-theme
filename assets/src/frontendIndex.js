2ba193a94f99d42b69b2f9817472d5da373629e2
Maud Leray <maudleray035@gmail.com>
2024-07-29 14:36:57 +0200

    Disable ESLint rule for 'regenerator-runtime/runtime'
    Otherwise frontend tests fail

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -1,3 +1,6 @@
+// This ESLint error is disabled since 'regenerator-runtime/runtime' has already been added by another package.
+// There is no need to explicitly include it in the list of dependencies in the package.json file.
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'regenerator-runtime/runtime';
 
 import {createRoot} from 'react-dom/client';
