10c0ff5730cb2870af35e3d1ddfeb4594b0c5d11
Pedro Figueroa <figueroash@gmail.com>
2024-08-02 14:45:25 +0200

    PLANET-7550: Fix php lint errors
    - PHP lint errors were fixed

--- src/Patterns/DeepDiveTopic.php
+++ src/Patterns/DeepDiveTopic.php
@@ -14,24 +14,29 @@ namespace P4\MasterTheme\Patterns;
  *
  * @package P4\MasterTheme\Patterns
  */
-class DeepDiveTopic extends BlockPattern {
-
-	/**
+class DeepDiveTopic extends BlockPattern
+{
+    /**
      * @inheritDoc
      */
-	public static function get_name(): string {
-		return 'p4/deep-dive-topic-pattern-layout';
-	}
+    public static function get_name(): string
+    {
+        return 'p4/deep-dive-topic-pattern-layout';
+    }
 
-	/**
+    /**
      * @inheritDoc
      */
-	public static function get_config( $params = [] ): array {
-		return [
-			'title'      => 'Deep Dive Topic',
-			'categories' => [ 'layouts' ],
-			'blockTypes' => [ 'core/post-content' ],
-			'content'    => '<!-- wp:planet4-block-templates/deep-dive-topic ' . wp_json_encode( $params, \JSON_FORCE_OBJECT ) . ' /-->',
-		];
-	}
+    public static function get_config($params = []): array
+    {
+        return [
+            'title' => 'Deep Dive Topic',
+            'categories' => [ 'layouts' ],
+            'blockTypes' => [ 'core/post-content' ],
+            'content' => '
+				<!-- wp:planet4-block-templates/deep-dive-topic ' .
+                wp_json_encode($params, \JSON_FORCE_OBJECT) .
+                ' /-->',
+        ];
+    }
 }
