5771924b172dc0c7f64aa8dce1fe9eb1f22ab99b
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2024-08-05 12:00:49 +0200

    PLANET-7550: Move Deep Dive Topic pattern block to theme (#2339)
    * PLANET-7550: Add Deep Dive Topic pattern block
    
    - The Deep Dive Topic pattern was moved to the theme
    
    * PLANET-7550: Fix php lint errors
    
    - PHP lint errors were fixed
    
    * PLANET-7550: Fix file format
    
    - Identation was fixed and an empty line was added at the end of the file

---
+++ assets/src/block-templates/deep-dive-topic/template.js
@@ -0,0 +1,34 @@
+const {__} = wp.i18n;
+
+const template = () => ([
+  ['core/group', {}, [
+    ['planet4-block-templates/page-header', {
+      titlePlaceholder: __('Page header title', 'planet4-blocks'),
+      mediaPosition: 'right',
+    }],
+    ['core/spacer', {height: '64px'}],
+    ['planet4-block-templates/side-image-with-text-and-cta', {
+      title: __('The problem', 'planet4-blocks'),
+    }],
+    ['planet4-block-templates/side-image-with-text-and-cta', {
+      title: __('What can be done', 'planet4-blocks'),
+      mediaPosition: 'right',
+    }],
+    ['planet4-blocks/covers', {
+      title: __('How you can help', 'planet4-blocks'),
+      cover_type: 'take-action',
+    }],
+    ['planet4-blocks/articles', {
+      article_heading: __('Latest news & stories', 'planet4-blocks'),
+    }],
+    ['planet4-block-templates/deep-dive', {
+      title: __('Keep learning about', 'planet4-blocks'),
+    }],
+    ['planet4-block-templates/quick-links', {
+      title: __('Explore other topics', 'planet4-blocks'),
+      backgroundColor: 'white',
+    }],
+  ]],
+]);
+
+export default template;
