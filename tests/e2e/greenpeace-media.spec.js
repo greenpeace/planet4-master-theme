609608708c17e5b55a9ba098d5b11662921c84c5
Maud Leray <maudleray035@gmail.com>
2024-08-12 14:15:54 +0200

    PLANET-7362 Write a Greenpeace Media e2e test for importing images (#2301)
    This includes testing for the bulk upload, and searching for images either by id or by term.

---
+++ tests/e2e/greenpeace-media.spec.js
@@ -0,0 +1,77 @@
+import {test, expect} from './tools/lib/test-utils.js';
+import {
+  searchImages,
+  deleteImageFromLibrary,
+  waitForLibraryLoad,
+} from './tools/lib/media-library.js';
+
+const TEST_IMAGES = ['GP0STXWME', 'GP0STXWZH'];
+const MEDIA_LIBRARY_PAGE = './wp-admin/admin.php?page=media-picker';
+
+test.useAdminLoggedIn();
+
+test.describe('Greenpeace Media tests', () => {
+  test('import an image to the Library using a search term', async ({page}) => {
+    // Make sure to close any alerts.
+    page.on('dialog', dialog => dialog.accept());
+
+    // Go to the Media Library and wait for the page to load.
+    await page.goto(MEDIA_LIBRARY_PAGE);
+    await waitForLibraryLoad(page);
+
+    // Search for an image to import.
+    await searchImages(page, 'Rainbow Warrior');
+    const image = page.locator('.picker-list > li')
+      .filter({hasNotText: 'Added to Media Library'})
+      .first();
+    await image.click();
+    await expect(image).toHaveClass('is-selected');
+    const sidebar = page.locator('.archive-picker-sidebar');
+    await expect(sidebar).toBeVisible();
+
+    // Import the image.
+    await sidebar.getByRole('button', {name: 'Import to Library'}).click();
+    await expect(sidebar.getByText('Processing...')).toBeVisible();
+    await expect(sidebar.locator('.sidebar-action')).toBeVisible();
+
+    // Delete the image from the library.
+    await deleteImageFromLibrary(page);
+  });
+
+  test('bulk select and upload two images using identifiers', async ({page}) => {
+    // Make sure to close any alerts.
+    page.on('dialog', dialog => dialog.accept());
+
+    // Go to the Media Library and wait for the page to load.
+    await page.goto(MEDIA_LIBRARY_PAGE);
+    await waitForLibraryLoad(page);
+
+    // Search for 2 specific images.
+    const images = page.locator('.picker-list > li');
+    for (let index = 0; index < 2; index++) {
+      await searchImages(page, TEST_IMAGES[index]);
+      await expect(images).toHaveCount(index + 1);
+    }
+
+    // Select the 2 images.
+    await page.getByRole('button', {name: 'Bulk Select'}).click();
+    for (let index = 0; index < 2; index++) {
+      const image = images.nth(index);
+      await image.click();
+      await expect(image).toHaveClass('is-selected');
+    }
+
+    // Bulk upload them.
+    await page.getByRole('button', {name: 'Bulk Upload'}).click();
+    await expect(page.getByText('Processing 2 images')).toBeVisible();
+    await expect(page.getByPlaceholder('Search')).toBeVisible();
+
+    // Delete the images from the library.
+    await page.reload();
+    await waitForLibraryLoad(page);
+    await deleteImageFromLibrary(page, TEST_IMAGES[0]);
+    await page.goto(MEDIA_LIBRARY_PAGE);
+    await waitForLibraryLoad(page);
+    await deleteImageFromLibrary(page, TEST_IMAGES[1]);
+  });
+});
---
+++ tests/e2e/tools/lib/media-library.js
@@ -0,0 +1,57 @@
+import {expect} from './test-utils.js';
+
+/**
+ * Searches for images in the Library based on a search term or image id.
+ *
+ * @param {Object} page       - The page object for interacting with the browser.
+ * @param {string} searchTerm - The term to be searched, can be an image id.
+ */
+async function searchImages(page, searchTerm) {
+  const searchInput = page.getByPlaceholder('Search');
+
+  await searchInput.fill(searchTerm);
+  await page.keyboard.press('Enter');
+  await waitForLibraryLoad(page);
+}
+
+/**
+ * Deletes an image from the Library.
+ * The image id is optional, it's not needed if the image is already selected.
+ *
+ * @param {Object} page      - The page object for interacting with the browser.
+ * @param {string} [imageId] - The image to be deleted (optional).
+ */
+async function deleteImageFromLibrary(page, imageId) {
+  const sidebar = page.locator('.archive-picker-sidebar');
+  if (imageId) {
+    // Search for the image.
+    await searchImages(page, imageId);
+    const image = page.locator('.picker-list > li');
+
+    // Select the image.
+    await image.click();
+  }
+
+  // Open image link via the sidebar.
+  await expect(sidebar).toBeVisible();
+  await sidebar.locator('.sidebar-action').click();
+
+  // Delete the image and wait for confirmation.
+  const deleteLink = page.getByText('Delete permanently');
+  await expect(deleteLink).toBeVisible();
+  await deleteLink.click();
+  await expect(page.getByText('Media file permanently deleted.')).toBeVisible();
+}
+
+/**
+ * Waits for the Library page to be fully loaded, based on the spinner.
+ *
+ * @param {Object} page - The page object for interacting with the browser.
+ */
+async function waitForLibraryLoad(page) {
+  const spinner = page.locator('.archive-picker-loading');
+  await expect(spinner).toBeVisible();
+  await expect(spinner).toBeHidden();
+}
+
+export {searchImages, deleteImageFromLibrary, waitForLibraryLoad};
