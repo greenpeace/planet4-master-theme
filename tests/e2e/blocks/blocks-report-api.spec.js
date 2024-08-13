216129017424815101542617cb0248a571d994f0
Dan Tovbein <dtovbein@gmail.com>
2024-08-13 15:26:31 -0300

    Add new specs
    - Rebuild

---
+++ tests/e2e/blocks/blocks-report-api.spec.js
@@ -0,0 +1,22 @@
+import {test, expect} from '../tools/lib/test-utils.js';
+
+test('Test Blocks report API', async ({page, requestUtils}) => {
+  await page.goto('./wp-admin/');
+
+  const apiRoute = './wp-json/plugin_blocks/v3/plugin_blocks_report';
+  const response = await requestUtils.request.fetch(apiRoute, {
+    failOnStatusCode: true,
+  });
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.headers()['content-type']).toBe('application/json; charset=UTF-8');
+
+  const json = await response.json();
+  expect(json).toHaveProperty('block_types');
+  expect(json).toHaveProperty('block_patterns');
+  expect(json).toHaveProperty('post_types');
+
+  expect(Object.keys(json.block_types)).not.toBe([]);
+  expect(Object.keys(json.block_patterns)).not.toBe([]);
+  expect(Object.keys(json.post_types)).not.toBe([]);
+});
---
+++ tests/e2e/blocks/page-layouts-modal.spec.js
@@ -0,0 +1,17 @@
+import {test, expect} from '../tools/lib/test-utils.js';
+
+test.useAdminLoggedIn();
+
+test('checks if the welcome modal on the editor is present and closed when the button is clicked', async ({page, admin, editor}) => {
+  await admin.createNewPost({postType: 'page', legacyCanvas: true});
+  const modal = editor.canvas.getByRole('dialog', {name: 'Choose a pattern'});
+  await expect(modal).toBeVisible();
+
+  for (const frame of await modal.getByRole('option', {name: 'Editor canvas'}).all()) {
+    await frame.waitForLoadState('domcontentloaded');
+  }
+  await page.waitForLoadState('domcontentloaded');
+
+  await modal.getByRole('button', {name: 'Close'}).click({timeout: 15000});
+  await expect(modal).toBeHidden();
+});
