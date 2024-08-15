cc76f0e33645341e2b8393b7eac93d302e7b3ea9
Maud Leray <maudleray035@gmail.com>
2024-08-15 13:45:55 +0200

    Fix Greenpeace Media tests
    They are a bit flaky sometimes

--- tests/e2e/greenpeace-media.spec.js
+++ tests/e2e/greenpeace-media.spec.js
@@ -70,6 +70,7 @@ test.describe('Greenpeace Media tests', () => {
     await page.getByRole('button', {name: 'Bulk Upload'}).click();
     await expect(page.getByText('Processing 2 images')).toBeVisible();
     await expect(page.getByPlaceholder('Search')).toBeVisible();
+    await waitForLibraryLoad(page);
 
     // Delete the images from the library.
     await deleteImageFromLibrary(page, selectedImages[0]);
