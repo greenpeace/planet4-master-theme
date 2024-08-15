0dcfd40c6a5116bd04cc80077baacb7c0c7ef971
Maud Leray <maudleray035@gmail.com>
2024-08-15 11:16:46 +0200

    Use default WP config again
    I'm guessing this was removed because before the WP upgrade the config was not adapted to webpack 5?

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const dashDash = require('@greenpeace/dashdash');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 const mediaQueryAliases = {
   '(max-width: 576px)': 'mobile-only',
@@ -18,6 +19,7 @@ module.exports = (env, argv) => {
 
   return {
     //stats: 'verbose',
+    ...defaultConfig,
     mode: argv.mode || 'development',
     devtool: isProduction ? false : 'source-map',
     entry: {
@@ -132,6 +134,7 @@ module.exports = (env, argv) => {
       ],
     },
     plugins: [
+      ...defaultConfig.plugins,
       // extract css into dedicated file
       new MiniCssExtractPlugin({
         chunkFilename: '[id].css',
@@ -152,9 +155,6 @@ module.exports = (env, argv) => {
         }),
         new CssMinimizerPlugin(),
       ],
-    },
-    stats: {
-      children: false,
     },
   };
 };
