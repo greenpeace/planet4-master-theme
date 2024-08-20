75dcd197a8c059fb3869306070b256a191f8727f
Pedro Figueroa <figueroash@gmail.com>
2024-08-20 10:20:09 +0200

    PLANET-7573: Fix selector
    - Wrong selector in e2e test was fixed

--- tests/e2e/search.spec.js
+++ tests/e2e/search.spec.js
@@ -4,15 +4,37 @@ test.useAdminLoggedIn();
 
 test.describe('Greenpeace Search tests', () => {
   test('re sync elasticpress', async ({page}) => {
+    // Go to ElasticSearch sync page
     await page.goto('./wp-admin/admin.php?page=elasticpress-sync');
 
-    const checkBox = await page.getByRole('checkbox', {name: 'Delete all data and start fresh sync'});
-    const startButton = await page.getByRole('button', {name: 'Start sync'});
+    const mainButton = page.locator('.ep-sync-controls button.is-primary');
+    const checkBox = page.locator('#inspector-checkbox-control-0');
+    const syncProgress = page.locator('.ep-sync-progress__details');
 
+    let mainButtonText = await mainButton.allInnerTexts();
+    let syncProgressText = await syncProgress.allInnerTexts();
+
+    // If the sync was already executed, stop it
+    if (mainButtonText.includes('Stop sync')) {
+      expect(syncProgressText.includes('Sync paused'));
+      await mainButton.click();
+      mainButtonText = await mainButton.allInnerTexts();
+    }
+
+    // Click on the checkbox and execute the sync process
     await checkBox.check();
-    await startButton.click();
+    await mainButton.click();
 
-    expect(page.getByText('Sync complete.')).toBeVisible();
+    expect(mainButtonText.includes('Start sync'));
+    expect(syncProgressText.includes('Sync in progress'));
+
+    // Wait for the sync to complete
+    await page.waitForFunction(
+      () => document.querySelector('.ep-sync-progress__details').innerText.includes('Sync complete')
+    );
+
+    syncProgressText = await syncProgress.allInnerTexts();
+    expect(syncProgressText.includes('Sync complete'));
   });
 
   test('check search works', async ({page, requestUtils}) => {
