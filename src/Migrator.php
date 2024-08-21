7a353a3f8b88d5c8ff26985dec6a899eaaa0988c
Pedro Figueroa <figueroash@gmail.com>
2024-08-21 12:21:25 +0200

    PLANET-7521: Migrate Media Blocks
    - Media blocks were migrated to audio/video default blocks

--- src/Migrator.php
+++ src/Migrator.php
@@ -33,7 +33,7 @@ use P4\MasterTheme\Migrations\M028MovePageHeaderSideBarOptions;
 use P4\MasterTheme\Migrations\M029RemoveTemplateEditorOption;
 use P4\MasterTheme\Migrations\M030RemovePurgeOnFeatureChangeOption;
 use P4\MasterTheme\Migrations\M031MigrateMediaBlockToAudioVideoBlock;
-use P4\MasterTheme\Migrations\M032MigrateSplit2ColumnBlock;
+use P4\MasterTheme\Migrations\M033MigrateMediaBlockToAudioVideoBlock;
 
 /**
  * Run any new migration scripts and record results in the log.
@@ -84,7 +84,7 @@ class Migrator
             M029RemoveTemplateEditorOption::class,
             M030RemovePurgeOnFeatureChangeOption::class,
             M031MigrateMediaBlockToAudioVideoBlock::class,
-            M032MigrateSplit2ColumnBlock::class,
+            M033MigrateMediaBlockToAudioVideoBlock::class,
         ];
 
         // Loop migrations and run those that haven't run yet.
