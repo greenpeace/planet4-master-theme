7d561ed2995defa204e2e13883410a7252089041
Maud Leray <maudleray035@gmail.com>
2024-09-03 12:24:56 +0200

    PLANET-7512 Update Take Action Boxout styles
    This is to follow the design system

--- assets/src/blocks/TakeActionBoxout/TakeActionBoxoutFrontend.js
+++ assets/src/blocks/TakeActionBoxout/TakeActionBoxoutFrontend.js
@@ -38,18 +38,18 @@ export const TakeActionBoxoutFrontend = ({
       {excerpt &&
         <p className="boxout-excerpt" dangerouslySetInnerHTML={{__html: excerpt}} />
       }
+      {link && linkText &&
+        <a
+          className="btn btn-primary"
+          data-ga-category="Take Action Boxout"
+          data-ga-action="Call to Action"
+          data-ga-label="n/a"
+          href={link}
+          {...newTab && {rel: 'noreferrer', target: '_blank'}}
+        >
+          {linkText}
+        </a>
+      }
     </div>
-    {link && linkText &&
-      <a
-        className="btn btn-primary"
-        data-ga-category="Take Action Boxout"
-        data-ga-action="Call to Action"
-        data-ga-label="n/a"
-        href={link}
-        {...newTab && {rel: 'noreferrer', target: '_blank'}}
-      >
-        {linkText}
-      </a>
-    }
   </section>
 );
