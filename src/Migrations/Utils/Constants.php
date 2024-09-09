4b0aa2235cead269805d11e437e8db525571a7da
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2024-09-09 12:33:51 +0530

    PLANET-7489 Update PR as per base branch utility constants and functions
    Use a reusable code from base branch

--- src/Migrations/Utils/Constants.php
+++ src/Migrations/Utils/Constants.php
@@ -14,6 +14,7 @@ class Constants
     public const BLOCK_SPLIT_TWO_COLUMNS = self::PREFIX_P4_BLOCKS . '/split-two-columns';
     public const BLOCK_COVERS = self::PREFIX_P4_BLOCKS . '/covers';
     public const BLOCK_P4_COLUMNS = self::PREFIX_P4_BLOCKS . '/columns';
+    public const BLOCK_COLUMNS = self::PREFIX_P4_BLOCKS . '/columns';
 
     public const BLOCK_EMBED = self::PREFIX_CORE_BLOCKS . '/embed';
     public const BLOCK_AUDIO = self::PREFIX_CORE_BLOCKS . '/audio';
