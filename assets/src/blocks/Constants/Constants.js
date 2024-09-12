097d60c628d2f1bf6083f0f6eff43621498ee687
Pedro Figueroa <figueroash@gmail.com>
2024-09-12 09:08:45 +0200

    PLANET-7611: Rename Happy Point
    - The block name Happy Point is now used consistently

--- assets/src/blocks/Constants/Constants.js
+++
@@ -1,8 +0,0 @@
-export const P4_BLOCK_PREFIX = 'planet4-blocks';
-
-export const BLOCK_NAME = {
-  tableOfContents: {
-    name: P4_BLOCK_PREFIX + '/submenu', //TO DO: Change the name of this block to 'table-of-contents' when migrating it.
-    title: 'Table of Contents',
-  },
-};
