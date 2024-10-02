7d99dbe7a902f65734a34cdec0daf3b75b9021de
Pedro Figueroa <figueroash@gmail.com>
2024-10-02 10:00:33 +0200

    PLANET 7521: Remove unused import
    - Remove unused import

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -3,14 +3,12 @@ import 'regenerator-runtime/runtime';
 import {createRoot} from 'react-dom/client';
 import {TableOfContentsFrontend} from './blocks/TableOfContents/TableOfContentsFrontend';
 import {HappyPointFrontend} from './blocks/HappyPoint/HappyPointFrontend';
-import {MediaFrontend} from './blocks/Media/MediaFrontend';
 import {ColumnsFrontend} from './blocks/Columns/ColumnsFrontend';
 
 // Render React components
 const COMPONENTS = {
   'planet4-blocks/submenu': TableOfContentsFrontend,
   'planet4-blocks/happypoint': HappyPointFrontend,
-  'planet4-blocks/media-video': MediaFrontend,
   'planet4-blocks/columns': ColumnsFrontend,
 };
 
