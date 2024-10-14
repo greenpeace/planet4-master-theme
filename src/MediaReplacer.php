64f827605620072cdae323238ebb3dd6d03df7f7
Pedro Figueroa <figueroash@gmail.com>
2024-10-14 14:04:13 +0200

    PLANET-7564: media replacement feature
    - Add ajax call for the replacement

--- src/MediaReplacer.php
+++ src/MediaReplacer.php
@@ -16,7 +16,18 @@ class MediaReplacer
     {
         add_action('admin_footer', [$this, 'enqueue_media_modal_script']);
         add_filter('attachment_fields_to_edit', [$this, 'add_replace_media_button'], 10, 2);
-        // add_action('init', [$this, 'replace_media_image']);
+        add_action('wp_ajax_replace_media', [$this, 'ajax_replace_media']); // AJAX action for replacing media
+    }
+
+    public function ajax_replace_media() {
+        // Check if the attachment ID is set
+        if (isset($_POST['attachment_id'])) {
+            $attachment_id = intval($_POST['attachment_id']);
+            $this->replace_media_image($attachment_id); // Call the function with the attachment ID
+            wp_send_json_success(); // Send a success response
+        } else {
+            wp_send_json_error('Attachment ID is missing.'); // Error response
+        }
     }
 
     function enqueue_media_modal_script() {
@@ -26,12 +37,27 @@ class MediaReplacer
                 $(document).on('click', '.custom-button', function(e) {
                     e.preventDefault();
                     var attachmentId = $(this).data('attachment-id');
-                    alert('Button clicked for attachment ID: ' + attachmentId); // Replace with your action
+    
+                    // Send AJAX request to replace the media
+                    $.ajax({
+                        url: ajaxurl, // WordPress AJAX URL
+                        type: 'POST',
+                        data: {
+                            action: 'replace_media', // Custom action name
+                            attachment_id: attachmentId // Pass the attachment ID
+                        },
+                        success: function(response) {
+                            location.reload(true);
+                        },
+                        error: function(xhr, status, error) {
+                            alert('Error: ' + error); // Error message
+                        }
+                    });
                 });
             });
         </script>
         <?php
-    }
+    }    
     
     function add_replace_media_button($form_fields, $post) {
         // Check if the post type is 'attachment'
@@ -44,9 +70,8 @@ class MediaReplacer
         return $form_fields;
     }
 
-    function replace_media_image() {
-        // Use the attachment ID instead of the URL
-        $old_image_id = 225; // Replace with the old image ID
+    function replace_media_image($old_image_id) {
+        // Assuming $new_image_id is defined or fetched appropriately
         $new_image_id = 224; // Replace with the new image ID
         
         // Get file paths
@@ -85,5 +110,5 @@ class MediaReplacer
         } else {
             error_log('New image does not exist at ' . $new_image_path); // Log an error if the new image is not found
         }
-    }    
+    }  
 }
