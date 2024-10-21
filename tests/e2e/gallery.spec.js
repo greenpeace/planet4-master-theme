ccd253d5b1ffde15897045ef2cd9fae952e6d48d
Dan Tovbein <dtovbein@greenpeace.org>
2024-10-21 09:46:24 -0300

    Fix e2e specs
    - Fix editor.canvas issue and replaced with page

--- tests/e2e/gallery.spec.js
+++ tests/e2e/gallery.spec.js
@@ -1,38 +1,38 @@
 import {test, expect} from './tools/lib/test-utils.js';
 import {publishPostAndVisit, createPostWithFeaturedImage} from './tools/lib/post.js';
+import {searchAndInsertBlock} from './tools/lib/editor.js';
 
 test.useAdminLoggedIn();
 
 test('Test Gallery basic functionalities', async ({page, admin, editor}) => {
-  await createPostWithFeaturedImage({admin, editor}, {
+  await createPostWithFeaturedImage({page, admin, editor}, {
     title: 'Test page for Gallery',
     postType: 'page',
   });
-  await editor.canvas.getByRole('button', {name: 'Add default block'}).click();
-  await page.keyboard.type('This is a test Page for gallery.');
-  await page.keyboard.press('Enter');
-  await page.keyboard.type('/gallery');
-  await page.getByRole('option', {name: 'Gallery'}).click();
+
+  // Search block
+  await searchAndInsertBlock(page, {blockName: 'Planet 4 Gallery', namespace: 'planet4-blocks-gallery'});
 
   await page.route('./wp-json/planet4/v1/gallery/images/*', async route => {
-    const request = await route.request();
+    const request = route.request();
     const response = await request.response();
 
-    await expect(response.status()).toEqual(200);
+    expect(response.status()).toEqual(200);
     await route.continue();
   });
 
-  await editor.canvas.getByRole('button', {name: 'Media Library'}).click();
-  const imageModal = await editor.canvas.getByRole('dialog', {name: 'Create gallery'});
-  const mediaLibTab = await imageModal.getByRole('tab', {name: 'Media Library'});
-  await mediaLibTab.click();
-  await imageModal.getByRole('tabpanel', {name: 'Media Library'});
+  await page.getByRole('button', {name: 'Media Library'}).click();
+
+  const imageModal = page.getByRole('dialog', {name: 'Create gallery'});
+  await imageModal.getByRole('tab', {name: 'Media Library'}).click();
+  imageModal.getByRole('tabpanel', {name: 'Media Library'});
 
   await imageModal.locator('[data-id="357"]').click();
   await imageModal.locator('[data-id="354"]').click();
   await imageModal.locator('[data-id="350"]').click();
-  await imageModal.getByRole('button', {name: 'Create a new gallery'}).click();
-  await editor.canvas.getByRole('dialog', {name: 'Edit gallery'}).getByRole('button', {name: 'Insert gallery'}).click();
+
+  await page.getByRole('button', {name: 'Create a new gallery'}).click();
+  await page.getByRole('dialog', {name: 'Edit gallery'}).getByRole('button', {name: 'Insert gallery'}).click();
 
   // Publish post
   await publishPostAndVisit({page, editor});
