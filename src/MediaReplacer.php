8826f7a7524b7a194534f51d245d36d8f32a46fa
Pedro Figueroa <figueroash@gmail.com>
2024-10-21 15:23:38 +0200

    PLANET-7564: Add Gloud Media Replacement
    - Remove CloudflarePurger

--- src/MediaReplacer.php
+++ src/MediaReplacer.php
@@ -2,7 +2,7 @@
 
 namespace P4\MasterTheme;
 
-use P4\MasterTheme\CloudflarePurger;
+use WP_Post;
 
 /**
  * Class MediaReplacer.
@@ -11,17 +11,11 @@ use P4\MasterTheme\CloudflarePurger;
  */
 class MediaReplacer
 {
-    protected $cloud_flare_purger;
-
     /**
      * MediaReplacer constructor.
      */
     public function __construct()
     {
-        if (class_exists('P4\MasterTheme\CloudflarePurger')) {
-            $this->cloud_flare_purger = new CloudflarePurger();
-        }
-
         add_action('admin_enqueue_scripts', [$this, 'enqueue_media_modal_script']);
         add_filter('attachment_fields_to_edit', [$this, 'add_replace_media_button'], 10, 2);
         add_action('wp_ajax_replace_media', [$this, 'ajax_replace_media']);
@@ -31,7 +25,8 @@ class MediaReplacer
     /**
      * Enqueues the JavaScript required for the media replacement modal.
      */
-    public function enqueue_media_modal_script() {
+    public function enqueue_media_modal_script(): void
+    {
         wp_enqueue_script(
             'custom-media-replacer',
             get_template_directory_uri() . '/admin/js/media_replacer.js',
@@ -48,7 +43,8 @@ class MediaReplacer
      * @param WP_Post $post The current attachment post object.
      * @return array Modified form fields with the replace media button added.
      */
-    public function add_replace_media_button($form_fields, $post) {
+    public function add_replace_media_button(array $form_fields, WP_Post $post): array
+    {
         // Check if the post type is 'attachment' and exclude image mime types
         if ($post->post_type !== 'attachment') {
             return $form_fields;
@@ -61,7 +57,7 @@ class MediaReplacer
             'image/bmp',
             'image/webp',
             'image/tiff',
-            'image/svg+xml'
+            'image/svg+xml',
         ];
 
         // Check if the post excludes image mime types
@@ -88,16 +84,17 @@ class MediaReplacer
                 />
             ',
         );
-        
+
         return $form_fields;
     }
 
     /**
      * Handles the AJAX request for replacing media files.
-     * Checks for the attachment ID and uploaded file, uploads the file, 
+     * Checks for the attachment ID and uploaded file, uploads the file,
      * and replaces the old media file with the new one.
      */
-    public function ajax_replace_media() {
+    public function ajax_replace_media(): void
+    {
         // Check if the attachment ID is set
         if (!isset($_POST['attachment_id'])) {
             set_transient('media_replacement_error', 'Attachment ID is missing.', 5);
@@ -113,7 +110,7 @@ class MediaReplacer
         }
 
         $attachment_id = intval($_POST['attachment_id']);
-    
+
         // Handle the uploaded file
         $file = $_FILES['file'];
         $upload_overrides = array('test_form' => false);
@@ -127,16 +124,9 @@ class MediaReplacer
             wp_send_json_error($error_message);
             return;
         }
-        
+
         // Replace the media file
         $this->replace_media_file($attachment_id, $movefile['file']);
-
-        // Purge Cloudflare with the attachment URL
-        if ($this->cloud_flare_purger && method_exists($this->cloud_flare_purger, 'purge')) {
-            $attachment_url = [wp_get_attachment_url($attachment_id)];
-            $this->cloud_flare_purger->purge($attachment_url);
-        }
-
         set_transient('media_replacement_message', 'Media replaced successfully!', 5);
         wp_send_json_success();
     }
@@ -144,7 +134,8 @@ class MediaReplacer
     /**
      * Displays admin notices for media replacement messages.
      */
-    public function display_admin_notices() {
+    public function display_admin_notices(): void
+    {
         // Check for success message
         if ($message = get_transient('media_replacement_message')) {
             echo '<div class="notice notice-success is-dismissible"><p>' . esc_html($message) . '</p></div>';
@@ -152,20 +143,23 @@ class MediaReplacer
         }
 
         // Check for error message
-        if ($error = get_transient('media_replacement_error')) {
-            echo '<div class="notice notice-error is-dismissible"><p>' . esc_html($error) . '</p></div>';
-            delete_transient('media_replacement_error');
+        if (!$error = get_transient('media_replacement_error')) {
+            return;
         }
+
+        echo '<div class="notice notice-error is-dismissible"><p>' . esc_html($error) . '</p></div>';
+        delete_transient('media_replacement_error');
     }
 
     /**
-     * Replaces the media file associated with the old attachment ID 
+     * Replaces the media file associated with the old attachment ID
      * with the new file located at the specified path.
      *
      * @param int $old_file_id The ID of the old attachment.
      * @param string $new_file_path The path to the new file.
      */
-    private function replace_media_file($old_file_id, $new_file_path) {
+    private function replace_media_file(int $old_file_id, string $new_file_path): void
+    {
         // Get the old file path
         $old_file_path = get_attached_file($old_file_id);
 
@@ -184,14 +178,15 @@ class MediaReplacer
             'post_mime_type' => $filetype['type'],
             'post_title' => preg_replace('/\.[^.]+$/', '', basename($old_file_path)),
             'post_content' => '',
-            'post_status' => 'inherit'
+            'post_status' => 'inherit',
         );
 
         // Update the database record for the file
         wp_update_post($attachment_data);
 
         // Update file metadata
-        // By calling the "wp_update_attachment_metadata" function, the WP Stateless plugin syncs the file with Google Storage.
+        // By calling the "wp_update_attachment_metadata" function,
+        // the WP Stateless plugin syncs the file with Google Storage.
         // https://github.com/udx/wp-stateless/blob/0871da645453240007178f4a5f243ceab6a188ea/lib/classes/class-bootstrap.php#L376
         $attach_data = wp_generate_attachment_metadata($old_file_id, $old_file_path);
         wp_update_attachment_metadata($old_file_id, $attach_data);
