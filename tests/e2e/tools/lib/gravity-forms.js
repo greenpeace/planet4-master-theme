062838f502c31b75693bd04bc11f8001da940a42
Maud Leray <maudleray035@gmail.com>
2024-10-22 09:46:12 +0200

    Add e2e tests for GF redirect and page confirmation types (#2412)
    See tickets PLANET-7638 and PLANET-7639. We used to have a test only for text confirmation type.

--- tests/e2e/tools/lib/gravity-forms.js
+++ tests/e2e/tools/lib/gravity-forms.js
@@ -1,5 +1,16 @@
 import {expect} from '@playwright/test';
 
+const TEST_FIRST_NAME = 'Jon';
+const TEST_LAST_NAME = 'Snow';
+const TEST_EMAIL = 'jon.snow@gmail.com';
+
+/**
+ * Toggle the Gravity Forms rest API to use it for tests.
+ * It is disabled by default.
+ *
+ * @param {Object} page     - The page object for interacting with the browser.
+ * @param {boolean} enabled - Whether it should be enabled or disabled.
+ */
 const toggleRestAPI = async ({page}, enabled) => {
   await page.goto('./wp-admin/admin.php?page=gf_settings&subview=gravityformswebapi');
   await page.getByRole('checkbox', {label: 'Enabled'}).setChecked(enabled);
@@ -13,6 +24,12 @@ const toggleRestAPI = async ({page}, enabled) => {
   await expect(page.locator('.gforms_note_success')).toBeVisible();
 };
 
+/**
+ * Create a new Gravity Forms form.
+ *
+ * @param {Object} page  - The page object for interacting with the browser.
+ * @param {Object} title - The form title.
+ */
 const createForm = async ({page}, {title}) => {
   const response = await page.request.post('./wp-json/gf/v2/forms', {
     data: {
@@ -45,4 +62,48 @@ const createForm = async ({page}, {title}) => {
   return createdForm;
 };
 
-export {toggleRestAPI, createForm};
+/**
+ * Fill and submit a Gravity Forms form.
+ *
+ * @param {Object} page   - The page object for interacting with the browser.
+ * @param {number} formId - The form id.
+ */
+const fillAndSubmitForm = async ({page}, formId) => {
+  const form = page.locator(`#gform_${formId}`);
+  await form.getByLabel('First name').fill(TEST_FIRST_NAME);
+  await form.getByLabel('Last name').fill(TEST_LAST_NAME);
+  await form.getByLabel('Email').fill(TEST_EMAIL);
+  const submitButton = form.getByRole('button', {name: 'Submit'});
+  await submitButton.click();
+};
+
+/**
+ * Check the latest entry for a Gravity Forms form.
+ *
+ * @param {Object} page   - The page object for interacting with the browser.
+ * @param {number} formId - The form id.
+ */
+const checkEntry = async ({page}, formId) => {
+  await page.goto(`./wp-admin/admin.php?page=gf_entries&id=${formId}`);
+  const latestEntry = page.locator('#the-list > tr.entry_row').first();
+  await expect(latestEntry).toBeVisible();
+  await expect(latestEntry.locator('td[data-colname="First name"]')).toContainText(TEST_FIRST_NAME);
+  await expect(latestEntry.locator('td[data-colname="Last name"]')).toContainText(TEST_LAST_NAME);
+  await expect(latestEntry.locator('td[data-colname="Email"]')).toContainText(TEST_EMAIL);
+};
+
+/**
+ * Change the confirmation type for a Gravity Forms form.
+ *
+ * @param {Object} page   - The page object for interacting with the browser.
+ * @param {number} formId - The form id.
+ * @param {string} label  - The confirmation type label.
+ */
+const changeConfirmationType = async ({page}, formId, label) => {
+  await page.goto(`./wp-admin/admin.php?page=gf_edit_forms&view=settings&subview=confirmation&id=${formId}`);
+  await page.locator('#the-list > tr:first-child').hover();
+  await page.getByRole('link', {name: 'Edit', exact: true}).click();
+  await page.getByLabel(label, {exact: true}).check();
+};
+
+export {toggleRestAPI, createForm, fillAndSubmitForm, checkEntry, changeConfirmationType};
