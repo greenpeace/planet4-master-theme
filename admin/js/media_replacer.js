e31995149b6cefd6b25120bceba366ebcfb522ed
Pedro Figueroa <figueroash@gmail.com>
2024-10-23 10:14:12 +0200

    PLANET-7564: Add Gloud Media Replacement
    - Refactor code to use guard clauses in the js file

--- admin/js/media_replacer.js
+++ admin/js/media_replacer.js
@@ -5,56 +5,59 @@
  */
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', e => {
-    if (e.target.classList.contains('media-replacer-button')) {
-      e.preventDefault();
-      const attachmentId = e.target.dataset.attachmentId;
-      const fileInput = e.target.nextElementSibling;
+    const replaceButtonClass = 'media-replacer-button';
 
-      // Show the file input
-      fileInput.click();
+    if (!e.target.classList.contains(replaceButtonClass)) {
+      return;
+    }
 
-      // When a file is selected
-      fileInput.addEventListener('change', () => {
-        const file = fileInput.files[0];
+    e.preventDefault();
 
-        if (file) {
-          const formData = new FormData();
-          formData.append('action', 'replace_media');
-          formData.append('attachment_id', attachmentId);
-          formData.append('file', file);
+    const attachmentId = e.target.dataset.attachmentId;
+    const fileInput = e.target.nextElementSibling;
 
-          const replaceMediaButton = document.querySelector('.media-replacer-button');
+    // Show the file input
+    fileInput.click();
 
-          if (replaceMediaButton) {
-            replaceMediaButton.disabled = true;
-            replaceMediaButton.innerText = 'Replacing media, please wait...';
+    // When a file is selected
+    fileInput.addEventListener('change', () => {
+      const file = fileInput.files[0];
+      if (!file) {
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('action', 'replace_media');
+      formData.append('attachment_id', attachmentId);
+      formData.append('file', file);
+
+      const replaceMediaButton = document.querySelector(`.${replaceButtonClass}`);
+      if (replaceMediaButton) {
+        replaceMediaButton.disabled = true;
+        replaceMediaButton.innerText = 'Replacing media, please wait...';
+      }
+
+      // Send AJAX request to replace the media
+      fetch(ajaxurl, {
+        method: 'POST',
+        body: formData,
+      })
+        .then(response => response.json())
+        .then(response => {
+          if (!response.success) {
+            alert('Error: ' + response.data); // eslint-disable-line no-alert
+            return;
           }
 
-          // Send AJAX request to replace the media
-          fetch(ajaxurl, {
-            method: 'POST',
-            body: formData,
-          })
-            .then(response => response.json())
-            .then(response => {
-              if (response.success) {
-                location.reload(true);
-              } else {
-                // eslint-disable-next-line no-alert
-                alert('Error: ' + response.data);
-              }
-            })
-            .catch(error => {
-              if (replaceMediaButton) {
-                replaceMediaButton.disabled = false;
-                replaceMediaButton.innerText = 'Replace Media';
-              }
-
-              // eslint-disable-next-line no-alert
-              alert('Error: ' + error);
-            });
-        }
-      }, {once: true}); // Use the 'once' option to ensure the event is only handled once
-    }
+          location.reload(true);
+        })
+        .catch(error => {
+          if (replaceMediaButton) {
+            replaceMediaButton.disabled = false;
+            replaceMediaButton.innerText = 'Replace Media';
+          }
+          alert('Error: ' + error); // eslint-disable-line no-alert
+        });
+    }, {once: true}); // Use the 'once' option to ensure the event is only handled once
   });
 });
