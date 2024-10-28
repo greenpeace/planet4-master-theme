d9b1b34247807b88db1bc9225c725bd7faca682c
Dan Tovbein <dtovbein@greenpeace.org>
2024-10-28 05:45:51 -0300

    Fix eslint issues
    - Missing declaration
    - Indent issues

--- assets/src/blocks/Covers/CoversConstants.js
+++ assets/src/blocks/Covers/CoversConstants.js
@@ -1,3 +1,5 @@
+const {__} = wp.i18n;
+
 export const COVERS_TYPES = {
   takeAction: 'take-action',
   campaign: 'campaign',
@@ -14,51 +16,51 @@ export const CAROUSEL_LAYOUT_COVERS_LIMIT = 12;
 export const BLOCK_NAME = 'planet4-blocks/covers';
 
 export const attributes = {
-	cover_type: {
-		type: 'string',
-		default: 'content',
-	},
-	initialRowsLimit: {
-		type: 'integer',
-		default: 1,
-	},
-	title: {
-		type: 'string',
-		default: '',
-	},
-	description: {
-		type: 'string',
-		default: '',
-	},
-	tags: {
-		type: 'array',
-		default: [],
-	},
-	post_types: {
-		type: 'array',
-		default: [],
-	},
-	posts: {
-		type: 'array',
-		default: [],
-	},
-	version: {
-		type: 'integer',
-		default: 2,
-	},
-	layout: {
-		type: 'string',
-		default: COVERS_LAYOUTS.grid,
-	},
-	isExample: {
-		type: 'boolean',
-		default: false,
-	},
-	exampleCovers: { // Used for the block's preview, which can't extract items from anything.
-		type: 'object',
-	},
-	readMoreText: {
-		type: 'string',
-		default: __('Load more', 'planet4-blocks'),
-	},
+  cover_type: {
+    type: 'string',
+    default: 'content',
+  },
+  initialRowsLimit: {
+    type: 'integer',
+    default: 1,
+  },
+  title: {
+    type: 'string',
+    default: '',
+  },
+  description: {
+    type: 'string',
+    default: '',
+  },
+  tags: {
+    type: 'array',
+    default: [],
+  },
+  post_types: {
+    type: 'array',
+    default: [],
+  },
+  posts: {
+    type: 'array',
+    default: [],
+  },
+  version: {
+    type: 'integer',
+    default: 2,
+  },
+  layout: {
+    type: 'string',
+    default: COVERS_LAYOUTS.grid,
+  },
+  isExample: {
+    type: 'boolean',
+    default: false,
+  },
+  exampleCovers: { // Used for the block's preview, which can't extract items from anything.
+    type: 'object',
+  },
+  readMoreText: {
+    type: 'string',
+    default: __('Load more', 'planet4-blocks'),
+  },
 };
