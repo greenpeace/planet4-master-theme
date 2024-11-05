d8ac834a34d5ea5ab9d1817d37993563a3538852
Pedro Figueroa <figueroash@gmail.com>
2024-11-05 12:10:56 +0100

    PLANET-7445: Migrate split 2 columns block
    - Implement minor adjustments and extra conditions

--- src/Migrations/Utils/Constants.php
+++ src/Migrations/Utils/Constants.php
@@ -16,7 +16,6 @@ class Constants
     public const BLOCK_SOCIAL_MEDIA = self::PREFIX_P4_BLOCKS . '/social-media';
     public const BLOCK_P4_COLUMNS = self::PREFIX_P4_BLOCKS . '/columns';
 
-
     public const BLOCK_EMBED = self::PREFIX_CORE_BLOCKS . '/embed';
     public const BLOCK_AUDIO = self::PREFIX_CORE_BLOCKS . '/audio';
     public const BLOCK_VIDEO = self::PREFIX_CORE_BLOCKS . '/video';
