cd2ef8df9ba308355343f85cf9cf587511405dda
Pedro Figueroa <figueroash@gmail.com>
2024-11-08 10:33:49 +0100

    PLANET-7489: Migrate Campaign Covers
    - Remove unnecessary code

--- assets/src/blocks/Covers/CoversEditor.js
+++ assets/src/blocks/Covers/CoversEditor.js
@@ -11,7 +11,7 @@ import {InspectorControls, RichText} from '@wordpress/block-editor';
 import TagSelector from '../../block-editor/TagSelector';
 import PostTypeSelector from '../../block-editor/PostTypeSelector';
 import {Covers} from './Covers';
-import {COVERS_TYPES, COVERS_LAYOUTS, CAROUSEL_LAYOUT_COVERS_LIMIT} from './CoversConstants';
+import {COVERS_TYPES, COVERS_LAYOUTS} from './CoversConstants';
 import {useCovers} from './useCovers';
 import {getStyleFromClassName} from '../../functions/getStyleFromClassName';
 import {CoversCarouselLayout} from './CoversCarouselLayout';
