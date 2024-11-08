c85249ec04b8462b8b8265ade033a19eb829ab5a
Maud Leray <maudleray035@gmail.com>
2024-11-08 11:02:25 +0100

    Make sure to migrate all post statuses
    Otherwise only published ones get updated

--- src/Migrations/M033MigrateSocialMediaTwitterBlockToEmbedBlock.php
+++ src/Migrations/M033MigrateSocialMediaTwitterBlockToEmbedBlock.php
@@ -37,7 +37,8 @@ class M033MigrateSocialMediaTwitterBlockToEmbedBlock extends MigrationScript
             // Get the list of posts using Social Media blocks.
             $posts = Utils\Functions::get_posts_using_specific_block(
                 Utils\Constants::BLOCK_SOCIAL_MEDIA,
-                Utils\Constants::ALL_POST_TYPES
+                Utils\Constants::ALL_POST_TYPES,
+                Utils\Constants::POST_STATUS_LIST,
             );
 
             // If there are no posts, abort.
--- src/Migrations/Utils/Constants.php
+++ src/Migrations/Utils/Constants.php
@@ -44,4 +44,12 @@ class Constants
         'future',
         'private',
     ];
+
+    public const POST_STATUS_LIST = [
+        'publish',
+        'pending',
+        'draft',
+        'future',
+        'private',
+    ];
 }
