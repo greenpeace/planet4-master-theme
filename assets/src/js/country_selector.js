9fc70901e51f0eb3631f3ab4343c1c53e12f2f53
Dan Tovbein <dtovbein@greenpeace.org>
2024-11-11 07:11:31 -0300

    Follow up review
    - Fix focus tab
    - Change the order of elements programatically

--- assets/src/js/country_selector.js
+++ assets/src/js/country_selector.js
@@ -1,31 +1,50 @@
 // The script basically add/remove the tabindex attribute within the country selector
 // It helps to improve SEO
 export const setupCountrySelector = () => {
-  const countries = document.querySelectorAll('.countries-list .countries > li');
+  const targetNode = document.querySelector('#country-selector');
+  const countriesList = document.querySelector('.countries-list');
+  const footerMenu = document.querySelector('.footer-menu');
+  const footerSocialMedia = document.querySelector('.footer-social-media');
+  const largeAndUpScreens = '(min-width: 992px)';
 
-  // Create a new MutationObserver with a country selector
-  new MutationObserver(
+  const switchOrders = (evt = null) => {
+    const event = evt ? evt : window.matchMedia(largeAndUpScreens);
+    if (event.matches) {
+      footerMenu.parentNode.insertBefore(footerMenu, footerMenu.parentNode.firstChild);
+    } else {
+      footerSocialMedia.parentNode.insertBefore(footerSocialMedia, footerSocialMedia.parentNode.firstChild);
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(
+    // Callback function to execute when mutations are observed
     mutationList => {
+      // Set default visibility
       for (const mutation of mutationList) {
-        if (mutation.target.className.includes('footer-country-selector') && mutation.attributeName === 'class') {
-          const isOpen = mutation.target.className.includes('open');
-
-          for (const countryItem of countries) {
-            // This check applies not only to the parent link but also to children
-            // In some cases, the country has also different link's languages
-            if (countryItem.classList.contains('country-group')) {
-              countryItem.setAttribute('tabIndex', -1);
-
-              for (const link of countryItem.querySelectorAll('li')) {
-                link.setAttribute('tabIndex', isOpen ? 0 : -1);
-              }
-            }
+        if (mutation.attributeName === 'class') {
+          if (targetNode.classList.contains('open')) {
+            countriesList.style.visibility = 'visible';
+          } else {
+            setTimeout(() => {
+              countriesList.style.visibility = 'hidden';
+            }, 1000); // Follow the .countries-list transition time
           }
         }
       }
     }
-  ).observe(
-    document.querySelector('.footer-country-selector'),
+  );
+
+  // Start observing the target node for configured mutations
+  observer.observe(
+    targetNode,
     {attributes: true, childList: true, subtree: true}
   );
+
+  window.matchMedia(largeAndUpScreens).addEventListener('change', event => {
+    switchOrders(event);
+  });
+
+  // Set default order
+  switchOrders();
 };
