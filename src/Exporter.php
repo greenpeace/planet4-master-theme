510bb8eed9dabec4137ace38b558ed6c231c1954
Nikos Roussos <nikos.roussos@greenpeace.org>
2024-11-15 09:51:56 +0200

    Show media import button only on Media Library
    We are injecting an Import button, in order to guide people on import media files while on Media Library.
    But currently that button show up everywhere, on all post types.
    
    This small change makes the button visible only in the Media Library.

--- src/Exporter.php
+++ src/Exporter.php
@@ -115,7 +115,7 @@ class Exporter
         ?>
         <script>
             jQuery(function(){
-                jQuery(".wrap .page-title-action")
+                jQuery(".upload-php .wrap .page-title-action")
                     .after('<a href="upload.php?page=media-picker" class="add-new-h2"><?php esc_html_e('Import Greenpeace Media', 'planet4-master-theme-backend'); ?></a>');
             });
 
