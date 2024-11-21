f488bba2386355e0405fca09de78e58de0fea49b
Dan Tovbein <dtovbein@greenpeace.org>
2024-11-21 06:24:11 -0300

    Follow up review
    - Apply transition duration dynamic variable
    - Check if selectors exist
    - Update JSDoc

--- assets/src/js/country_selector.js
+++ assets/src/js/country_selector.js
@@ -1,12 +1,27 @@
-// The script basically add/remove the tabindex attribute within the country selector
-// It helps to improve SEO
+/**
+ * The script basically add/remove the tabindex attribute within the country selector
+ * It helps to improve SEO
+ * @return {void}
+ */
 export const setupCountrySelector = () => {
   const targetNode = document.querySelector('#country-selector');
   const countriesList = document.querySelector('.countries-list');
   const footerMenu = document.querySelector('.footer-menu');
   const footerSocialMedia = document.querySelector('.footer-social-media');
-  const largeAndUpScreens = '(min-width: 992px)';
 
+  if (!targetNode || !countriesList || !footerMenu || !footerSocialMedia) {
+    return;
+  }
+
+  const largeAndUpScreens = '(min-width: 992px)';
+  const countriesListTransitionDuration = (
+    parseFloat(getComputedStyle(document.querySelector('.countries-list')).transitionDuration) * 1000
+  ) || 500;
+
+  /**
+   * This funtion re-order the elements from the footer
+   * @param {*} evt
+   */
   const switchOrders = (evt = null) => {
     const event = evt ? evt : window.matchMedia(largeAndUpScreens);
     if (event.matches) {
@@ -28,7 +43,7 @@ export const setupCountrySelector = () => {
           } else {
             setTimeout(() => {
               countriesList.style.visibility = 'hidden';
-            }, 1000); // Follow the .countries-list transition time
+            }, countriesListTransitionDuration);
           }
         }
       }
@@ -36,10 +51,7 @@ export const setupCountrySelector = () => {
   );
 
   // Start observing the target node for configured mutations
-  observer.observe(
-    targetNode,
-    {attributes: true, childList: true, subtree: true}
-  );
+  observer.observe(targetNode, {attributes: true, childList: true, subtree: true});
 
   window.matchMedia(largeAndUpScreens).addEventListener('change', event => {
     switchOrders(event);
