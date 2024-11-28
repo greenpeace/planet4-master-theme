c921e7aeb1c72c81f2cd37ef53d498acf4775363
Pedro Figueroa <figueroash@gmail.com>
2024-11-28 12:52:38 +0100

    PLANET-7578: Migrate Covers blocks
    -  Migrate Covers blocks

--- assets/src/blocks/Covers/Covers.js
+++ assets/src/blocks/Covers/Covers.js
@@ -1,5 +1,11 @@
 import {TakeActionCovers} from './TakeActionCovers';
+import {COVERS_TYPES} from './CoversConstants';
 
-export const Covers = ({...props}) => {
-  return <TakeActionCovers {...props} />;
+export const Covers = ({cover_type, ...props}) => {
+  switch (cover_type) {
+  case COVERS_TYPES.takeAction:
+    return <TakeActionCovers {...props} />;
+  default:
+    return null;
+  }
 };
