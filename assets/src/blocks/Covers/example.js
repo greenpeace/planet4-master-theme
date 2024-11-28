f6dca05a7218d59f069a67abe6f141017f9b3b03
Pedro Figueroa <figueroash@gmail.com>
2024-11-28 09:40:47 +0100

    PLANET-7578: Migrate Covers blocks
    - Fix feedback:
    - https://github.com/greenpeace/planet4-master-theme/pull/2363#discussion_r1860851961

--- assets/src/blocks/Covers/example.js
+++ assets/src/blocks/Covers/example.js
@@ -4,40 +4,36 @@ export const example = {
     isExample: true,
     title: 'Covers block',
     exampleCovers: {
-      content: [
+      'take-action': [
         {
-          'take-action': [
-            {
-              link: 'Page1',
-              button_link: 'Page1',
-              title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-              excerpt: 'Etiam et turpis et tortor congue interdum quis in leo. Donec vel eros eget mauris aliquam commodo.',
-              button_text: 'Take action',
-              tags: [{
-                name: 'Tag1',
-              }],
-            },
-            {
-              link: 'Page2',
-              button_link: 'Page2',
-              title: 'Vivamus ornare varius neque at posuere',
-              excerpt: 'Cras suscipit velit nec gravida auctor. Suspendisse et enim a ex feugiat interdum laoreet vel lorem.',
-              button_text: 'Take action',
-              tags: [{
-                name: 'Tag2',
-              }],
-            },
-            {
-              link: 'Page3',
-              button_link: 'Page3',
-              title: 'Vitae purus neque',
-              excerpt: 'In egestas mollis leo. Suspendisse in iaculis mauris. Duis sagittis arcu vel sodales bibendum.',
-              button_text: 'Take action',
-              tags: [{
-                name: 'Tag3',
-              }],
-            },
-          ],
+          link: 'Page1',
+          button_link: 'Page1',
+          title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          excerpt: 'Etiam et turpis et tortor congue interdum quis in leo. Donec vel eros eget mauris aliquam commodo.',
+          button_text: 'Take action',
+          tags: [{
+            name: 'Tag1',
+          }],
+        },
+        {
+          link: 'Page2',
+          button_link: 'Page2',
+          title: 'Vivamus ornare varius neque at posuere',
+          excerpt: 'Cras suscipit velit nec gravida auctor. Suspendisse et enim a ex feugiat interdum laoreet vel lorem.',
+          button_text: 'Take action',
+          tags: [{
+            name: 'Tag2',
+          }],
+        },
+        {
+          link: 'Page3',
+          button_link: 'Page3',
+          title: 'Vitae purus neque',
+          excerpt: 'In egestas mollis leo. Suspendisse in iaculis mauris. Duis sagittis arcu vel sodales bibendum.',
+          button_text: 'Take action',
+          tags: [{
+            name: 'Tag3',
+          }],
         },
       ],
     },
