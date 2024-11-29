62d253cd3ba772825943048e947bf076e5904afb
Pedro Figueroa <figueroash@gmail.com>
2024-11-29 10:59:59 +0100

    PLANET-7578: Migrate Covers blocks
    - Fixes based on feedback:
    - https://github.com/greenpeace/planet4-master-theme/pull/2461#discussion_r1862155279
    - https://github.com/greenpeace/planet4-master-theme/pull/2461#discussion_r1862153757

--- assets/src/blocks/Covers/Covers.js
+++ assets/src/blocks/Covers/Covers.js
@@ -1,11 +1,5 @@
 import {TakeActionCovers} from './TakeActionCovers';
-import {COVERS_TYPES} from './CoversConstants';
 
-export const Covers = ({cover_type, ...props}) => {
-  switch (cover_type) {
-  case COVERS_TYPES.takeAction:
-    return <TakeActionCovers {...props} />;
-  default:
-    return null;
-  }
+export const Covers = ({...props}) => {
+  return <TakeActionCovers {...props} />;
 };
