b24a79069952a60812980b88df3db2ad147b9180
Pedro Figueroa <figueroash@gmail.com>
2024-12-10 22:17:51 +0100

    PLANET-6692: Create Topic Link Block
    - Add js docs

--- assets/src/blocks/TopicLink/TopicLinkFrontend.js
+++ assets/src/blocks/TopicLink/TopicLinkFrontend.js
@@ -1,3 +1,14 @@
+/**
+ * TopicLinkFrontend component for rendering the Topic Link block on the frontend.
+ *
+ * @param {Object} props - Component properties.
+ * @param {string} props.imageUrl - The URL of the background image.
+ * @param {string} props.imageAlt - The alt text for the background image.
+ * @param {string} props.selectedCategory - The name of the selected category.
+ * @param {string} props.categoryLink - The URL link to the selected category.
+ * @param {string} props.focal_points - CSS-compatible object position string (e.g., "50% 50%").
+ * @return {JSX.Element} The Topic Link Frontend component.
+ */
 export const TopicLinkFrontend = ({
   imageUrl,
   imageAlt,
@@ -9,16 +20,17 @@ export const TopicLinkFrontend = ({
     <section className="topic-link-block">
       <a href={categoryLink} target="_self">
         <div className="background-image">
-          {imageUrl &&
-              <img
-                src={imageUrl}
-                alt={imageAlt}
-                style={{objectPosition: focal_points}}
-              />}
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt={imageAlt}
+              style={{objectPosition: focal_points}}
+            />
+          )}
         </div>
         <div className="topic-link-content">
           <p>
-              Learn more about {selectedCategory}
+            Learn more about {selectedCategory}
           </p>
           <div className="chevron-icon"></div>
         </div>
