4e0fe4c2b68e8062840ca4c75c711dd13c9e3b22
Pedro Figueroa <figueroash@gmail.com>
2025-01-10 10:07:46 +0100

    PLANET-6692: Create Topic Link Block
    - Fix issue after rebase

--- assets/src/editorIndex.js
+++ assets/src/editorIndex.js
@@ -24,6 +24,7 @@ wp.domReady(() => {
   registerSocialMediaBlock();
   registerTimelineBlock();
   registerPostsListBlock();
+  registerTopicLinkBlock();
 
   // Block Templates
   registerBlockTemplates();
