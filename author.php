6412476b25b0977b078c9116072d5796fc493bec
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2025-01-13 19:15:41 +0530

    PLANET-7646 Add default avatar image to Gravator function
    Ref. https://jira.greenpeace.org/browse/PLANET-7646

--- author.php
+++ author.php
@@ -30,7 +30,7 @@ if (isset($wp_query->query_vars['author'])) {
     $context['og_title'] = $author->name;
     $context['og_description'] = get_the_author_meta('description', $author->ID);
     $context['og_image_data'] = [
-        'url' => get_avatar_url($author->ID, [ 'size' => 300, 'default' => 'mm']),
+        'url' => get_avatar_url($author->ID, [ 'size' => 300, 'default' => $context['default_avatar']]),
         'width' => '300',
         'height' => '300',
     ];
