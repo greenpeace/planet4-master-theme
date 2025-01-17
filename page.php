aa52250c7c2ae10db70ce5304131bc3376d63a2c
Pedro Figueroa <figueroash@gmail.com>
2025-01-17 12:34:18 +0100

    PLANET-7625: Move scripts from twig templates
    - Fix php lint issues

--- page.php
+++ page.php
@@ -113,7 +113,6 @@ if (post_password_required($post->ID)) {
     do_action('pass_gtm_data', $context);
     Timber::render('single-page.twig', $context);
 } else {
-
     do_action('pass_gtm_data', $context);
     Timber::render([ 'page-' . $post->post_name . '.twig', 'page.twig' ], $context);
 }
