8f37496a5149d3ab10ea66d6f7263dfe7df03ffc
Pedro Figueroa <figueroash@gmail.com>
2025-02-18 11:08:38 +0100

    Fix Main Navigation Menu Accessibility Issues
    - Fix the donate button

--- assets/src/js/accessible_nav_menu.js
+++ assets/src/js/accessible_nav_menu.js
@@ -2,69 +2,85 @@
  * Class to handle keyboard accessibility in the navigation menu.
  */
 class AccessibleNavMenu {
-  static EVENT = {
-    dom: 'DOMContentLoaded',
-    keydown: 'keydown',
-    focus: 'focus',
-    focusout: 'focusout',
-  };
-  static EVENT_KEY = {
-    enter: 'Enter',
-    tab: 'Tab',
-  };
-  static DISPLAY = {
-    flex: 'flex',
-    none: 'none',
-  };
-  static CLASSNAME = {
-    rotate: 'rotate',
-    children: 'menu-item-has-children',
-  };
-  static SELECTORS = {
-    accessible: '.accessible-nav-link',
-    submenu: '.nav-submenu',
-    submenu_link: '.nav-submenu a',
-    nav_item: '.nav-item',
-    last_link: 'a:last-of-type',
-  };
-
   /**
    * Initializes the event listeners.
    */
   constructor() {
-    document.addEventListener(AccessibleNavMenu.EVENT.dom, () => {
-      this.handleNavMenuItem();
-      this.handleNavMenuSub();
-      this.handleNavMenuSubItems();
+    document.addEventListener('DOMContentLoaded', () => {
+      const mainNav = document.querySelector('#nav-main-desktop');
+
+      if (!mainNav) {
+        return;
+      }
+
+      this.handleNavMenuItem(mainNav);
+      this.handleAccessibleNavLink(mainNav);
+      this.handleNavMenuSub(mainNav);
+      this.handleNavMenuSubItems(mainNav);
+    });
+  }
+
+  /**
+   * Adds event listeners to handle submenu toggle on mouse hover.
+   * @param {HTMLElement} mainNav The main nav selector.
+   */
+  handleNavMenuItem(mainNav) {
+    mainNav.querySelectorAll('.nav-item, .nav-donate').forEach(button => {
+      button.addEventListener('mouseenter', () => {
+        const submenu = button.querySelector('.nav-submenu');
+        if (submenu) {
+          submenu.style.display = 'flex';
+        }
+
+        const accessibleLink = button.querySelector('.accessible-nav-link');
+        if (accessibleLink) {
+          accessibleLink.classList.add('rotate');
+        }
+      });
+      button.addEventListener('mouseleave', () => {
+        const submenu = button.querySelector('.nav-submenu');
+        if (submenu) {
+          submenu.style.display = 'none';
+        }
+
+        const accessibleLink = button.querySelector('.accessible-nav-link');
+        if (accessibleLink) {
+          accessibleLink.classList.remove('rotate');
+        }
+      });
     });
   }
 
   /**
    * Adds event listeners to handle submenu toggle.
+   * @param {HTMLElement} mainNav The main nav selector.
    */
-  handleNavMenuItem() {
-    document.querySelectorAll(AccessibleNavMenu.SELECTORS.accessible).forEach(button => {
-      button.addEventListener(AccessibleNavMenu.EVENT.keydown, event => {
-        if (event.key === AccessibleNavMenu.EVENT_KEY.enter) {
-          const submenu = button.closest(AccessibleNavMenu.SELECTORS.nav_item).querySelector(AccessibleNavMenu.SELECTORS.submenu);
+  handleAccessibleNavLink(mainNav) {
+    mainNav.querySelectorAll('.accessible-nav-link').forEach(button => {
+      button.addEventListener('keydown', event => {
+        if (event.key === 'Enter') {
+          const submenu = button.closest('.nav-item, .nav-donate')?.querySelector('.nav-submenu');
           if (submenu) {
             const display = window.getComputedStyle(submenu).display;
-            if (display === AccessibleNavMenu.DISPLAY.none || display === '') {
-              submenu.style.display = AccessibleNavMenu.DISPLAY.flex;
-              button.classList.add(AccessibleNavMenu.CLASSNAME.rotate);
+            if (display === 'none' || display === '') {
+              submenu.style.display = 'flex';
+              button.classList.add('rotate');
             } else {
-              submenu.style.display = AccessibleNavMenu.DISPLAY.none;
-              button.classList.remove(AccessibleNavMenu.CLASSNAME.rotate);
+              submenu.style.display = 'none';
+              button.classList.remove('rotate');
             }
           }
         }
       });
-      button.addEventListener(AccessibleNavMenu.EVENT.focusout, event => {
-        if (event.relatedTarget.parentElement.classList.contains(AccessibleNavMenu.CLASSNAME.children)) {
-          button.classList.remove(AccessibleNavMenu.CLASSNAME.rotate);
-          const submenu = button.closest(AccessibleNavMenu.SELECTORS.nav_item).querySelector(AccessibleNavMenu.SELECTORS.submenu);
+      button.addEventListener('focusout', event => {
+        const parentIsDonateBtn = event.relatedTarget?.parentElement?.classList?.contains('nav-donate');
+        const parentHasChildren = event.relatedTarget?.parentElement?.classList?.contains('menu-item-has-children');
+
+        if (parentIsDonateBtn || parentHasChildren) {
+          button.classList.remove('rotate');
+          const submenu = button.closest('.nav-item, .nav-donate').querySelector('.nav-submenu');
           if (submenu) {
-            submenu.style.display = AccessibleNavMenu.DISPLAY.none;
+            submenu.style.display = 'none';
           }
         }
       });
@@ -73,18 +89,19 @@ class AccessibleNavMenu {
 
   /**
    * Adds event listeners to hide submenu when focus leaves it, unless it moves to the associated toggle button.
+   * @param {HTMLElement} mainNav The main nav selector.
    */
-  handleNavMenuSub() {
-    document.querySelectorAll(AccessibleNavMenu.SELECTORS.submenu).forEach(submenu => {
-      submenu.addEventListener(AccessibleNavMenu.EVENT.focusout, event => {
-        const button = submenu.closest(AccessibleNavMenu.SELECTORS.nav_item).querySelector(AccessibleNavMenu.SELECTORS.accessible);
+  handleNavMenuSub(mainNav) {
+    mainNav.querySelectorAll('.nav-submenu').forEach(submenu => {
+      submenu.addEventListener('focusout', event => {
+        const button = submenu.closest('.nav-item, .nav-donate')?.querySelector('.accessible-nav-link');
         const newFocusElement = event.relatedTarget; // The next focused element
 
         // Only hide if focus is completely outside submenu and not back to .accessible-nav-link
         if (!submenu.contains(newFocusElement) && newFocusElement !== button) {
-          submenu.style.display = AccessibleNavMenu.DISPLAY.none;
+          submenu.style.display = 'none';
           if (button) {
-            button.classList.remove(AccessibleNavMenu.CLASSNAME.rotate);
+            button.classList.remove('rotate');
           }
         }
       });
@@ -93,18 +110,19 @@ class AccessibleNavMenu {
 
   /**
    * Adds event listeners to handle submenu keyboard navigation.
+   * @param {HTMLElement} mainNav The main nav selector.
    */
-  handleNavMenuSubItems() {
-    document.querySelectorAll(AccessibleNavMenu.SELECTORS.submenu_link).forEach(submenuLink => {
-      submenuLink.addEventListener(AccessibleNavMenu.EVENT.focus, event => {
-        const submenu = submenuLink.closest(AccessibleNavMenu.SELECTORS.submenu);
+  handleNavMenuSubItems(mainNav) {
+    mainNav.querySelectorAll('.nav-submenu a').forEach(submenuLink => {
+      submenuLink.addEventListener('focus', event => {
+        const submenu = submenuLink.closest('.nav-submenu');
         const nextLink = submenuLink.nextElementSibling;
-        const lastLink = submenu.querySelector(AccessibleNavMenu.SELECTORS.last_link);
+        const lastLink = submenu.querySelector('a:last-of-type');
 
-        if (nextLink && event.key === AccessibleNavMenu.EVENT_KEY.tab && !event.shiftKey) {
+        if (nextLink && event.key === 'Tab' && !event.shiftKey) {
           nextLink.focus();
         } else if (submenuLink === lastLink && !event.shiftKey) {
-          submenu.querySelector(AccessibleNavMenu.SELECTORS.accessible)?.focus();
+          submenu.querySelector('.accessible-nav-link')?.focus();
         }
       });
     });
