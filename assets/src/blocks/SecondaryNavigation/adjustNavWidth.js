955b8a722d7ef6cb1c06bffb331ea15c44cb77f3
Osong Agberndifor <osongagberndifor@gmail.com>
2025-02-25 00:15:58 +0100

    PLANET-6530 Review changes
    - Merged sticky function with frontend code
    - Refactord active class code to use React state
    - Added new function to handle content position based on number of items

---
+++ assets/src/blocks/SecondaryNavigation/adjustNavWidth.js
@@ -0,0 +1,19 @@
+function adjustJustifyContent() {
+  const container = document.querySelector('.secondary-navigation-item');
+  if (!container) {return;}
+
+  const children = Array.from(container.children);
+  const containerWidth = container.clientWidth;
+  let totalChildrenWidth = (children.length - 1) * 48;
+
+  children.forEach(child => {
+    totalChildrenWidth += child.offsetWidth;
+  });
+
+  container.style.justifyContent = totalChildrenWidth <= containerWidth ? 'center' : 'flex-start';
+}
+
+export const initializeJustifyContentAdjustment = () => {
+  window.addEventListener('load', adjustJustifyContent);
+  window.addEventListener('resize', adjustJustifyContent);
+};
