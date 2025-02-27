062ab30d8ac9b71c87f7259e641caecce2086446
Maud Leray <maudleray035@gmail.com>
2025-02-27 11:00:39 +0100

    PLANET-7700 Fix Actions List meaningful sequence
    This is for better accessibility

--- assets/src/js/actions_list_clickable_cards.js
+++ assets/src/js/actions_list_clickable_cards.js
@@ -2,19 +2,30 @@ export const setupClickabelActionsListCards = () => {
   const liElements = document.querySelectorAll('.actions-list ul li:not(.carousel-li)');
 
   liElements.forEach(li => {
-    const linkElement = li.querySelector('.wp-block-post-title > a');
-    if (linkElement) {
-      const url = linkElement.getAttribute('href');
-      li.tabIndex = 0;
-      const anchor = document.createElement('a');
-      anchor.setAttribute('href', url);
-      anchor.classList.add('actions-list-links');
+    const titleLink = li.querySelector('.wp-block-post-title > a');
 
-      while (li.firstChild) {
-        anchor.appendChild(li.firstChild);
-      }
-
-      li.appendChild(anchor);
+    if (!titleLink) {
+      return;
     }
+
+    // Add overlay to make whole card clickable
+    const url = titleLink.getAttribute('href');
+    li.tabIndex = 0;
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', url);
+    anchor.classList.add('actions-list-links');
+
+    while (li.firstChild) {
+      anchor.appendChild(li.firstChild);
+    }
+
+    li.appendChild(anchor);
+
+    // Remove keyboard access for the image, tag, title, and the card itself.
+    const imageLink = li.querySelector('.wp-block-post-featured-image > a');
+    const tagLink = li.querySelector('.taxonomy-post_tag > a');
+    const NO_KEYBOARD_ACCESS_ELEMENTS = [titleLink, imageLink, tagLink, li];
+
+    NO_KEYBOARD_ACCESS_ELEMENTS.forEach(element => element.setAttribute('tabindex', -1));
   });
 };
