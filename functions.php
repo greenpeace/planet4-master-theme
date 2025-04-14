b1a8103bd2e276b94b056322c05ad7a58d7724b5
Pedro Figueroa <figueroash@gmail.com>
2025-04-14 09:31:26 +0200

    PLANET-7745: Adjust taxonomy breadcrumbs
    - Revert new class

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 use P4\MasterTheme\Api;
 use P4\MasterTheme\Loader;
 use P4\MasterTheme\MediaArchive\Rest;
+use P4\MasterTheme\Post;
 use Timber\Timber;
 
 // This theme vendor dir
@@ -212,6 +213,126 @@ add_filter(
     2
 );
 
+/**
+ * I'll move this somewhere else in master theme.
+ *
+ */
+function register_more_blocks(): void
+{
+    register_block_type(
+        'p4/reading-time',
+        [
+            'render_callback' => [ Post::class, 'reading_time_block' ],
+            'uses_context' => [ 'postId' ],
+        ]
+    );
+    register_block_type(
+        'p4/post-author-name',
+        [
+            // phpcs:ignore SlevomatCodingStandard.Functions.UnusedParameter -- register_block_type callback
+            'render_callback' => function (array $attributes, $content, $block) {
+                $author_override = get_post_meta($block->context['postId'], 'p4_author_override', true);
+                $post_author_id = get_post_field('post_author', $block->context['postId']);
+
+                $is_override = ! empty($author_override);
+
+                $name = $is_override ? $author_override : get_the_author_meta('display_name', $post_author_id);
+                $link = $is_override ? '#' : get_author_posts_url($post_author_id);
+
+                $block_content = $author_override ? $name : "<a href='$link'>$name</a>";
+
+                return "<span class='article-list-item-author'>$block_content</span>";
+            },
+            'uses_context' => [ 'postId' ],
+        ]
+    );
+    // Like the core block but with an appropriate sizes attribute.
+     // phpcs:disable Generic.Files.LineLength.MaxExceeded
+    register_block_type(
+        'p4/post-featured-image',
+        [
+            // phpcs:ignore SlevomatCodingStandard.Functions.UnusedParameter -- register_block_type callback
+            'render_callback' => function (array $attributes, $content, $block) {
+                $post_id = $block->context['postId'];
+                $post_link = get_permalink($post_id);
+                $featured_image = get_the_post_thumbnail(
+                    $post_id,
+                    null,
+                    // For now hard coded sizes to the ones from Articles, as it's the single usage.
+                    // This can be made a block attribute, or even construct a sizes attr with math based on context.
+                    // For example, it could already access displayLayout from Query block to know how many columns are
+                    // being rendered. If it then also knows the flex gap and container width, it should have all needed
+                    // info to support a large amount of cases.
+                    [ 'sizes' => '(min-width: 1600px) 389px, (min-width: 1200px) 335px, (min-width: 1000px) 281px, (min-width: 780px) 209px, (min-width: 580px) 516px, calc(100vw - 24px)' ]
+                );
+
+                return "<a href='$post_link'>$featured_image</a>";
+            },
+            'uses_context' => [ 'postId' ],
+        ]
+    );
+    // Block displays related posts using the Query Loop block
+    register_block_type(
+        'p4/related-posts',
+        [
+            'attributes' => [
+                'query_attributes' => [
+                    'type' => 'object',
+                    'default' => [],
+                ],
+            ],
+            'render_callback' => [ Post::class, 'render_related_posts_block' ],
+        ]
+    );
+
+    register_block_type(
+        'p4/bottom-page-navigation-block',
+        [
+            'render_callback' => [ Post::class, 'render_navigation_block' ],
+        ]
+    );
+
+    register_block_type(
+        'p4/taxonomy-breadcrumb',
+        [
+            'api_version'     => 2,
+            'render_callback' => function ($attributes, $content, $block) {
+                $post_id = $block->context['postId'] ?? get_the_ID();
+                $taxonomy = $attributes['taxonomy'] ?? 'category';
+
+                $terms = get_the_terms($post_id, $taxonomy);
+                if (is_wp_error($terms) || empty($terms)) {
+                    return '';
+                }
+
+                $first = $terms[0];
+                $term_link = get_term_link($first);
+
+                return sprintf('
+                    <div class="wp-block-post-terms">
+                        <a href="%s">%s</a>
+                    </div>',
+                    esc_url($term_link),
+                    esc_html($first->name)
+                );
+            },
+            'uses_context'    => ['postId'],
+            'attributes'      => [
+                'taxonomy' => [
+                    'type'    => 'string',
+                    'default' => 'category',
+                ],
+                'taxonomy' => [
+                    'type'    => 'string',
+                    'default' => 'category',
+                ],
+            ],
+        ]
+    );
+}
+
+add_action('init', 'register_more_blocks');
+
 add_filter(
     'cloudflare_purge_by_url',
     function ($urls, $post_id) {
@@ -306,6 +427,7 @@ add_action(
             'field' => 'term_id',
             'terms' => [$post_type->term_id],
         ]]);
+        $query->set('post__not_in', get_option('sticky_posts'));
     }
 );
 
