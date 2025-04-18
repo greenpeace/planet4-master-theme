24cabe374742ac4080ca4ac3d879661776e35fa1
Maud Leray <maudleray035@gmail.com>
2025-04-18 11:54:32 +0200

    PLANET-7798 Upgrade WordPress to 6.8
    This is to stay up to date

--- assets/src/block-editor/SidebarFields/TextareaSidebarField.js
+++ assets/src/block-editor/SidebarFields/TextareaSidebarField.js
@@ -2,6 +2,7 @@ const {TextareaControl} = wp.components;
 
 export const TextareaSidebarField = ({value, setValue, label}) => (
   <TextareaControl
+    __nextHasNoMarginBottom
     label={label}
     value={value}
     onChange={setValue}
