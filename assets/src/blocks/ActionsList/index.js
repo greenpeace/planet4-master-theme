2903b8df8cd964110e131c56079507804bf5347a
Pedro Figueroa <figueroash@gmail.com>
2025-05-12 10:59:15 +0200

    PLANET-7579: Migrate covers take action block
    - Change block category

--- assets/src/blocks/ActionsList/index.js
+++ assets/src/blocks/ActionsList/index.js
@@ -25,7 +25,7 @@ export const registerActionsListBlock = () => {
     icon: 'list-view',
     scope: ['inserter'],
     allowedControls: ['taxQuery', 'pages', 'offset'],
-    category: 'planet4-blocks-beta',
+    category: 'planet4-blocks',
     isActive: ({namespace, query}) => namespace === ACTIONS_LIST_BLOCK_NAME && query.postType === queryPostType,
     attributes: {
       namespace: ACTIONS_LIST_BLOCK_NAME,
