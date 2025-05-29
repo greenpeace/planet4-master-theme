3ad8981d24166f875350ae45824118b742b08260
Pedro Figueroa <figueroash@gmail.com>
2025-05-29 10:04:29 +0200

    PLANET-7579: Remove Covers block
    - Remove code related to the block

--- assets/src/blocks/Covers/CoversGridLoadMoreButton.js
+++
@@ -1,5 +0,0 @@
-export const CoversGridLoadMoreButton = ({showMoreCovers, readMoreText}) => (
-  <button onClick={showMoreCovers} className="btn btn-secondary load-more-btn">
-    {readMoreText}
-  </button>
-);
