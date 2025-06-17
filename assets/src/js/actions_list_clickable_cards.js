fec16d9ffd2d3be7e5db909a080f89dff15da051
Osong Agberndifor <osongagberndifor@gmail.com>
2025-06-17 09:26:58 +0100

    PLANET-7719 Fixed screen reader issue and Carousel style accessibility
    - Removed role presentation attribute from Gallery images
    - Changed next and prev buttons for carousel style from a tags to buttons
    - Added meaningful tabbing sequence for Actions List block
    - Added a ghost element used to reset the focus for Carousel elements
    - Added styling for the buttons
    - Added a skip link to go back to the Carousel list

--- assets/src/js/actions_list_clickable_cards.js
+++ assets/src/js/actions_list_clickable_cards.js
@@ -1,20 +1,38 @@
+/**
+ * Update the tabbing functionality of the Actions list block by making only the
+ * card and the take action button focusable.
+ *
+ * @function setupClickabelActionsListCards
+ */
+
 export const setupClickabelActionsListCards = () => {
   const liElements = document.querySelectorAll('.actions-list ul li:not(.carousel-li)');
 
   liElements.forEach(li => {
-    const linkElement = li.querySelector('.wp-block-post-title > a');
-    if (linkElement) {
-      const url = linkElement.getAttribute('href');
-      li.tabIndex = 0;
-      const anchor = document.createElement('a');
-      anchor.setAttribute('href', url);
-      anchor.classList.add('actions-list-links');
+    const titleLink = li.querySelector('.wp-block-post-title > a');
 
-      while (li.firstChild) {
-        anchor.appendChild(li.firstChild);
-      }
-
-      li.appendChild(anchor);
+    if (!titleLink) {
+      return;
     }
+
+    // Add overlay to make whole card clickable
+    const url = titleLink.getAttribute('href');
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', url);
+    anchor.classList.add('actions-list-links');
+
+    while (li.firstChild) {
+      anchor.appendChild(li.firstChild);
+    }
+
+    li.appendChild(anchor);
+
+    // Remove keyboard access for the image, tag, title, and the card itself.
+    const imageLink = li.querySelector('.wp-block-post-featured-image > a');
+    const tagLink = li.querySelector('.taxonomy-post_tag > a');
+    const postTerms = li.querySelector('.wp-block-post-terms > a');
+    const NO_KEYBOARD_ACCESS_ELEMENTS = [titleLink, imageLink, tagLink, li, postTerms];
+
+    NO_KEYBOARD_ACCESS_ELEMENTS.forEach(element => element?.setAttribute('tabindex', -1));
   });
 };
