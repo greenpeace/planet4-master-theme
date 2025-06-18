08b31e99f0cde8d494d6075164291aa136f4da28
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2025-06-18 14:18:41 +0200

    PLANET-7579: Remove Covers block (#2638)
    - Add basic migration implementation
    - Execute migration
    - Replace old post type
    - Fix elements per page
    - Fix docs and comments
    - Fix lint issues
    - Replace blocks in post templates
    - Change migration file name

--- assets/src/blocks/TakeActionBoxout/TakeActionBoxoutEditor.js
+++ assets/src/blocks/TakeActionBoxout/TakeActionBoxoutEditor.js
@@ -18,7 +18,7 @@ const {
 } = wp.components;
 const {__} = wp.i18n;
 
-// Planet 4 settings (Planet 4 >> Defaults content >> Take Action Covers default button text).
+// Planet 4 settings (Planet 4 >> Defaults content >> Actions default button text).
 const DEFAULT_BUTTON_TEXT = window.p4_vars.options.take_action_covers_button_text || __('Take action', 'planet4-blocks');
 
 export const TakeActionBoxoutEditor = ({
