4eac79493a9819b99ac8e267ddcf0765493d1627
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-06-20 17:16:44 +0300

    Add a custom Action template
    This adds a body class with the name `p4_action-template-resistance-hub` that we can use
    to apply the campaign's custom design.

---
+++ single-p4_action-resistance_hub.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Template Name: Resistance Hub Campaign
+ * The template for displaying Resistance Hub campaign actions.
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+include get_template_directory() . '/single-p4_action.php';
