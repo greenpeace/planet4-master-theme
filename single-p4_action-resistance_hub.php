39242bac9b121aa804f2756b7e45eb3b9afed8ea
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-07-11 15:25:27 +0300

    Add a custom Action template (#2656)
    This adds a body class with the name `p4_action-template-resistance-hub` that we can use
    to apply the campaign's custom design.
    
    Ref: https://jira.greenpeace.org/browse/PLANET-7828
    
    - Add a Task Type field to show on Actions List block
    - Apply only to Resistance Hub campaign theme
    - Add Actions Options feature
    - If the feature is enabled the editor can set
    - the Task Type and Deadline through actions
    - Display the template from list only when the feature is enabled
    - Apply custom styles
    - Apply logic code
    - Apply logic to Take Action Boxout
    - Takes the TaskType from action and include the information to boxout

---
+++ single-p4_action-resistance_hub.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Template Name: Resistance Hub Campaign
+ * Template Post Type: p4_action
+ * The template for displaying Resistance Hub campaign actions.
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$features = get_option('planet4_features');
+
+// Enqueue Resistance Hub styles and script only for this template
+add_action('wp_enqueue_scripts', function (): void {
+    $template = get_page_template();
+    if (!$template) {
+        return;
+    }
+
+    $template_data = get_file_data($template, array('Template Name' => 'Template Name'));
+    if ($template_data['Template Name'] !== 'Resistance Hub Campaign') {
+        return;
+    }
+
+    wp_enqueue_style(
+        'p4-action-resistance-hub-styles',
+        get_template_directory_uri() . '/assets/build/resistance-hub-campaign-styles.css',
+        [],
+    );
+
+    wp_enqueue_script(
+        'p4-action-resistance-hub-script',
+        get_template_directory_uri() . '/assets/build/resistance-hub-campaign.js',
+        [],
+    );
+});
+
+add_filter(
+    'render_block',
+    function ($block_content, $block) {
+        global $post;
+        $post_id = $post->ID;
+
+        if (!isset($block['blockName'])) {
+            return $block_content;
+        }
+
+        $block_name = $block['blockName'];
+
+        if (
+            $block_name === "planet4-blocks/take-action-boxout" &&
+            isset($block['attrs']['take_action_page'])
+        ) {
+            $post_id = (int) $block['attrs']['take_action_page'];
+        }
+
+        if (
+            $block_name === 'planet4-blocks/action-button-text' ||
+            $block_name === "planet4-blocks/take-action-boxout"
+        ) {
+            $actions_task_type_value = get_post_meta($post_id, 'actions_task_type', true);
+            $actions_deadline_value = get_post_meta($post_id, 'actions_deadline', true);
+
+            if ($actions_task_type_value && $actions_deadline_value) {
+                $block_content = preg_replace(
+                    '/(<a[^>]*class="[^"]*btn[^"]*"[^>]*)/',
+                    // phpcs:disable Generic.Files.LineLength.MaxExceeded
+                    '$1 data-tasktype="' . esc_attr($actions_task_type_value) . '"' . ' data-deadline="' . esc_attr($actions_deadline_value) . '"',
+                    $block_content,
+                    1
+                );
+            }
+        }
+        return $block_content;
+    },
+    10,
+    2
+);
+
+include_once get_template_directory() . '/single-p4_action.php';
