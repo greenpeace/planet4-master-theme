b9dd24bb8d1fc949fe27f6badf4dd9334b35f146
Maud Leray <maudleray035@gmail.com>
2025-07-18 13:20:48 +0200

    PLANET-7829 Enable Footnotes block (#2660)
    This feature was added with WordPress 6.3. For it to be rendered in the frontend, we need to use the global WP post ID to construct the Timber post.

--- single-p4_action.php
+++ single-p4_action.php
@@ -1,5 +1,7 @@
 <?php
 
+global $post;
+
 /**
  * The Template for displaying all action pages
  *
@@ -12,38 +14,42 @@
  * @since    Timber 0.1
  */
 
+if (!$post) {
+    return;
+}
+
 use P4\MasterTheme\Context;
 use P4\MasterTheme\Post;
 use Timber\Timber;
 
 $context = Timber::get_context();
-$post = new Post(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-$page_meta_data = get_post_meta($post->ID);
+$timber_post = new Post($post->ID);
+$page_meta_data = get_post_meta($timber_post->ID);
 $page_meta_data = array_map(fn ($v) => reset($v), $page_meta_data);
 
 // Set GTM Data Layer values.
-$post->set_data_layer();
-$data_layer = $post->get_data_layer();
+$timber_post->set_data_layer();
+$data_layer = $timber_post->get_data_layer();
 
-Context::set_header($context, $page_meta_data, $post->title);
+Context::set_header($context, $page_meta_data, $timber_post->title);
 Context::set_background_image($context);
-Context::set_og_meta_fields($context, $post);
+Context::set_og_meta_fields($context, $timber_post);
 Context::set_campaign_datalayer($context, $page_meta_data);
-Context::set_utm_params($context, $post);
+Context::set_utm_params($context, $timber_post);
 Context::set_custom_styles($context, $page_meta_data);
 
-$context['post'] = $post;
-$context['social_accounts'] = $post->get_social_accounts($context['footer_social_menu'] ?: []);
+$context['post'] = $timber_post;
+$context['social_accounts'] = $timber_post->get_social_accounts($context['footer_social_menu'] ?: []);
 $context['page_category'] = 'Actions';
-$context['post_tags'] = implode(', ', $post->tags());
-$context['post_categories'] = implode(', ', $post->categories());
+$context['post_tags'] = implode(', ', $timber_post->tags());
+$context['post_categories'] = implode(', ', $timber_post->categories());
 $context['custom_body_classes'] = 'brown-bg ';
 
-Context::set_p4_blocks_datalayer($context, $post);
+Context::set_p4_blocks_datalayer($context, $timber_post);
 
-if (post_password_required($post->ID)) {
+if (post_password_required($timber_post->ID)) {
     // Password protected form validation.
-    $context['is_password_valid'] = $post->is_password_valid();
+    $context['is_password_valid'] = $timber_post->is_password_valid();
 
     // Hide the page title from links to the extra feeds.
     remove_action('wp_head', 'feed_links_extra', 3);
@@ -54,5 +60,5 @@ if (post_password_required($post->ID)) {
     Timber::render('single-page.twig', $context);
 } else {
     do_action('enqueue_google_tag_manager_script', $context);
-    Timber::render([ 'page-' . $post->post_name . '.twig', 'page.twig' ], $context);
+    Timber::render([ 'page-' . $timber_post->post_name . '.twig', 'page.twig' ], $context);
 }
--- single.php
+++ single.php
@@ -1,5 +1,7 @@
 <?php
 
+global $post;
+
 /**
  * The Template for displaying all single posts
  *
@@ -10,6 +12,10 @@
  * @since    Timber 0.1
  */
 
+if (!$post) {
+    return;
+}
+
 use P4\MasterTheme\Context;
 use P4\MasterTheme\Post;
 use P4\MasterTheme\Settings\CommentsGdpr;
@@ -17,24 +23,19 @@ use Timber\Timber;
 
 // Initializing variables.
 $context = Timber::get_context();
-/**
- * P4 Post Object
- *
- * @var Post $post
- */
-$post = Timber::query_post(false, Post::class); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-$context['post'] = $post;
+$timber_post = new Post($post->ID);
+$context['post'] = $timber_post;
 
 // Set Navigation Issues links.
-$post->set_issues_links();
+$timber_post->set_issues_links();
 
 // Get the cmb2 custom fields data
 // Articles block parameters to populate the related-posts block(previously known as articles block)
 // p4_take_action_page parameter to populate the take action boxout block
 // Author override parameter. If this is set then the author profile section will not be displayed.
-$page_meta_data = get_post_meta($post->ID);
+$page_meta_data = get_post_meta($timber_post->ID);
 $page_meta_data = array_map(fn($v) => reset($v), $page_meta_data);
-$page_terms_data = get_the_terms($post, 'p4-page-type');
+$page_terms_data = get_the_terms($timber_post, 'p4-page-type');
 $page_terms_data = is_array($page_terms_data) ? reset($page_terms_data) : null;
 $context['background_image'] = $page_meta_data['p4_background_image_override'] ?? '';
 $take_action_page = $page_meta_data['p4_take_action_page'] ?? '';
@@ -42,18 +43,18 @@ $context['page_type'] = $page_terms_data->name ?? '';
 $context['page_term_id'] = $page_terms_data->term_id ?? '';
 $context['custom_body_classes'] = 'white-bg';
 $context['page_type_slug'] = $page_terms_data->slug ?? '';
-$context['social_accounts'] = $post->get_social_accounts($context['footer_social_menu'] ?: []);
+$context['social_accounts'] = $timber_post->get_social_accounts($context['footer_social_menu'] ?: []);
 $context['page_category'] = 'Post Page';
-$context['post_tags'] = implode(', ', $post->tags());
-$context['post_categories'] = implode(', ', $post->categories());
+$context['post_tags'] = implode(', ', $timber_post->tags());
+$context['post_categories'] = implode(', ', $timber_post->categories());
 // We need the explode because we want to remove "+00:00" at the end of the string.
-$context['page_date'] = explode('+', get_the_date('c', $post->ID))[0];
-$context['old_posts_archive_notice'] = $post->get_old_posts_archive_notice();
+$context['page_date'] = explode('+', get_the_date('c', $timber_post->ID))[0];
+$context['old_posts_archive_notice'] = $timber_post->get_old_posts_archive_notice();
 
-Context::set_og_meta_fields($context, $post);
+Context::set_og_meta_fields($context, $timber_post);
 Context::set_campaign_datalayer($context, $page_meta_data);
-Context::set_utm_params($context, $post);
-Context::set_reading_time_datalayer($context, $post);
+Context::set_utm_params($context, $timber_post);
+Context::set_reading_time_datalayer($context, $timber_post);
 
 $context['filter_url'] = add_query_arg(
     [
@@ -65,13 +66,13 @@ $context['filter_url'] = add_query_arg(
 );
 
 // Build the shortcode for related-posts block.
-if ('yes' === $post->include_articles) {
+if ('yes' === $timber_post->include_articles) {
     $tag_id_array = [];
-    foreach ($post->tags() as $post_tag) {
-        $tag_id_array[] = $post_tag->id;
+    foreach ($timber_post->tags() as $timber_post_tag) {
+        $tag_id_array[] = $timber_post_tag->id;
     }
     $category_id_array = [];
-    foreach ($post->terms('category') as $category) {
+    foreach ($timber_post->terms('category') as $category) {
         $category_id_array[] = $category->id;
     }
 
@@ -83,7 +84,7 @@ if ('yes' === $post->include_articles) {
                 'post_tag' => $tag_id_array,
                 'category' => $category_id_array,
             ],
-            'exclude' => [$post->ID],
+            'exclude' => [$timber_post->ID],
         ],
         'className' => 'posts-list p4-query-loop is-custom-layout-list',
         'layout' => [
@@ -93,17 +94,19 @@ if ('yes' === $post->include_articles) {
         'namespace' => 'planet4-blocks/posts-list',
     ];
 
-    $post->articles = '<!-- wp:p4/related-posts {"query_attributes" : ' . wp_json_encode($block_attributes) . '} /-->';
+    $timber_post->articles = '<!-- wp:p4/related-posts {"query_attributes" : '
+        . wp_json_encode($block_attributes) .
+    '} /-->';
 }
 
 if (! empty($take_action_page) && ! has_block('planet4-blocks/take-action-boxout')) {
-    $post->take_action_page = $take_action_page;
+    $timber_post->take_action_page = $take_action_page;
 
     $block_attributes = [
         'take_action_page' => $take_action_page,
     ];
 
-    $post->take_action_boxout = '<!-- wp:planet4-blocks/take-action-boxout '
+    $timber_post->take_action_boxout = '<!-- wp:planet4-blocks/take-action-boxout '
     . wp_json_encode($block_attributes, JSON_UNESCAPED_SLASHES)
     . ' /-->';
 }
@@ -128,21 +131,21 @@ $comments_args = [
 ];
 
 $context['comments_args'] = $comments_args;
-$context['show_comments'] = comments_open($post->ID);
+$context['show_comments'] = comments_open($timber_post->ID);
 $context['post_comments_count'] = get_comments(
     [
-        'post_id' => $post->ID,
+        'post_id' => $timber_post->ID,
         'status' => 'approve',
         'type' => 'comment',
         'count' => true,
     ]
 );
 
-Context::set_p4_blocks_datalayer($context, $post);
+Context::set_p4_blocks_datalayer($context, $timber_post);
 
-if (post_password_required($post->ID)) {
+if (post_password_required($timber_post->ID)) {
     // Password protected form validation.
-    $context['is_password_valid'] = $post->is_password_valid();
+    $context['is_password_valid'] = $timber_post->is_password_valid();
 
     // Hide the post title from links to the extra feeds.
     remove_action('wp_head', 'feed_links_extra', 3);
@@ -154,7 +157,7 @@ if (post_password_required($post->ID)) {
 } else {
     do_action('enqueue_google_tag_manager_script', $context);
     Timber::render(
-        [ 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ],
+        [ 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single.twig' ],
         $context
     );
 }
