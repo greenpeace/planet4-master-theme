37aa2e7ededca42c60060e7c802517f667b2e7a8
sagarsdeshmukh <sagarsdeshmukh91@gmail.com>
2025-07-22 16:03:51 +0530

    PLANET-7390 Update PHPDoc block & add comments
    Ref. https://jira.greenpeace.org/browse/PLANET-7390

--- src/TimberSettings.php
+++ src/TimberSettings.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace P4\MasterTheme;
 
+/**
+ * Class TimberSettings
+ *
+ * Configures Timber-related settings such as class maps, Twig environment options, etc.
+ */
 class TimberSettings
 {
+    /**
+     * Registers filters to customize Timber behavior.
+     */
     public static function hooks(): void
     {
+        // Map post types to custom Timber post class.
         add_filter('timber/post/classmap', function ($classmap) {
             $custom_classmap = [
                 'page' => Post::class,
@@ -20,8 +29,10 @@ class TimberSettings
             return array_merge($classmap, $custom_classmap);
         });
 
+        // Assign a custom Timber user class.
         add_filter('timber/user/class', fn() => User::class);
 
+        // Customize Twig environment options.
         add_filter('timber/twig/environment/options', static function (array $options): array {
             $options['autoescape'] = 'html';
             $options['cache'] = defined('WP_DEBUG') ? !WP_DEBUG : true;
