1f8f857cac3327f78d1a05414d748106e51053b8
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2025-08-25 09:46:33 +0200

    PLANET-7849: Refactor `header.js` file II (#2710)
    - Rename the accessibleNavMenu.js file
    - Rename and move the homepage_transparent_nav.js file
    - Move elements to another file

--- assets/src/js/header.js
+++ assets/src/js/header.js
@@ -1,30 +1,10 @@
-/* global hj */
-
-import {setupAccessibleNavMenu} from './header/accessibleNavMenu';
+import {setupAccessibleNavMenu} from './header/setupAccessibleNavMenu';
 import setupMobileTabsMenuScroll from './header/setupMobileTabsMenuScroll';
 import {setupCloseNavMenuButton, setupDocumentClick, toggleNavElements} from './header/setupNavMenu';
+import {setupTransparentNavHomepage} from './header/setupTransparentNavHomepage';
+import {setupNavInteractions} from './header/setupNavInteractions';
 
 export const setupHeader = () => {
-  // Close all menus on escape pressed
-  document.onkeyup = event => {
-    if (event.key === 'Escape') {
-      document.body.click();
-    }
-  };
-
-  let searchFocused = false;
-  const searchInput = document.getElementById('search_input');
-  if (searchInput) {
-    searchInput.addEventListener('focus', () => {
-      if (!searchFocused) {
-        if (hj) {
-          hj('event', 'search');
-        }
-        searchFocused = true;
-      }
-    });
-  }
-
   // Set the mobile tabs menu behavior on scroll.
   setupMobileTabsMenuScroll();
 
@@ -39,5 +19,11 @@ export const setupHeader = () => {
 
   // Handle clicking on navigation elements.
   toggleNavElements();
+
+  // Handles the transition to the transparent styles for the Navigation Menu.
+  setupTransparentNavHomepage();
+
+  // Set up other nav interactions and events.
+  setupNavInteractions();
 };
 
