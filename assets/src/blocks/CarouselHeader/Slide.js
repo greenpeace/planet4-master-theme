9c69861182d2b9911d575986c277bc35bfcb0698
Dan Tovbein <dtovbein@greenpeace.org>
2025-09-10 14:14:20 -0300

    Follow up UAT
    - Implement focusable order elements through carousel
    - Also turn into ul the divs that behave as list items

--- assets/src/blocks/CarouselHeader/Slide.js
+++ assets/src/blocks/CarouselHeader/Slide.js
@@ -1,17 +1,22 @@
-const {forwardRef} = wp.element;
+const {forwardRef, useMemo} = wp.element;
 
 export const SlideWithRef = ({
   children,
   active,
-}, ref) => (
-  <div
+  focusable,
+}, ref) => useMemo(() => (
+  <li
     className={`carousel-item ${active ? 'active' : ''}`}
+    tabIndex={focusable ? 0 : -1}
     ref={ref}
+    role="tabpanel"
+    // aria-hidden={!active}
+    aria-label="Text goes here!"
   >
     <div className="carousel-item-mask">
       {children}
     </div>
-  </div>
-);
+  </li>
+), [children, active, focusable, ref]);
 
 export const Slide = forwardRef(SlideWithRef);
