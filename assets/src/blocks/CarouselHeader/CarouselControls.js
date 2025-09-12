9764ab526b3a53b7e37e1852a46a43fa3e756824
Pedro Figueroa <figueroash@gmail.com>
2025-09-12 11:02:21 +0200

    Fix play/pause button
    - Fix play/pause button

--- assets/src/blocks/CarouselHeader/CarouselControls.js
+++ assets/src/blocks/CarouselHeader/CarouselControls.js
@@ -1,4 +1,4 @@
-const {useMemo} = wp.element;
+const {useState, useEffect, useMemo} = wp.element;
 const {__} = wp.i18n;
 
 export const CarouselControls = ({
@@ -7,59 +7,106 @@ export const CarouselControls = ({
   goToSlide = () => {},
   handleAutoplay = () => {},
   currentSlide = null,
-  slides = null,
+  slides = [],
   autoplay,
-}) => useMemo(() => (
-  <>
-    {/* Arrows */}
-    <button className="carousel-control-prev" onClick={goToPrevSlide} aria-label="Go to previous slide">
-      <span className="carousel-control-prev-icon" aria-hidden="true"><i></i></span>
-      <span className="visually-hidden">{__('Previous', 'planet4-blocks')}</span>
-    </button>
-    <button className="carousel-control-next" onClick={goToNextSlide} aria-label="Go to next slide">
-      <span className="carousel-control-next-icon" aria-hidden="true"><i></i></span>
-      <span className="visually-hidden">{__('Next', 'planet4-blocks')}</span>
-    </button>
-    {/* Indicators */}
-    <div className="carousel-indicators-wrapper">
-      <div className="container">
-        <ol className="carousel-indicators" tabIndex={-1}>
-          {
-            slides.map((_, index) =>
-              <li
-                key={index}
-                {...(index === currentSlide) ? {
-                  className: 'active',
-                } : null}
-              >
-                <button
-                  onClick={() => {
-                    if(index !== currentSlide) {
-                      goToSlide(index);
-                    }
-                  }}
-                  tabIndex={0}
-                  onKeyDown={e => {
-                    if ((e.key === 'Enter' || e.key === ' ') && index !== currentSlide) {
-                      e.preventDefault();
-                      goToSlide(index);
-                    }
-                  }}
-                  aria-label={__('Go to slide', 'planet4-blocks') + ` ${index + 1}`}
-                  aria-current={index === currentSlide ? 'true' : undefined}
-                />
-              </li>
-            )
-          }
-        </ol>
-        {autoplay && (
-          <button
-            aria-label={autoplay ? __('Stop Slider', 'planet4-blocks') : __('Play Slider', 'planet4-blocks')}
-            className={`carousel-autoplay-control ${autoplay ? 'stop' : 'play'}`}
-            onClick={handleAutoplay}
-          />
-        )}
-      </div>
-    </div>
-  </>
-), [currentSlide, autoplay, slides, goToPrevSlide, goToNextSlide, goToSlide, handleAutoplay]);
+}) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const [hasAutoplay, setHasAutoplay] = useState(false);
+
+  useEffect(() => {
+    if (autoplay) {
+      setHasAutoplay(true);
+    }
+  }, [autoplay]);
+
+  const handlePausePlayButton = () => {
+    setIsPaused(prev => !prev);
+    handleAutoplay();
+  };
+
+  return useMemo(
+    () => (
+      <>
+        {/* Arrows */}
+        <button
+          className="carousel-control-prev"
+          onClick={goToPrevSlide}
+          aria-label="Go to previous slide"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true">
+            <i></i>
+          </span>
+          <span className="visually-hidden">
+            {__('Previous', 'planet4-blocks')}
+          </span>
+        </button>
+        <button
+          className="carousel-control-next"
+          onClick={goToNextSlide}
+          aria-label="Go to next slide"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true">
+            <i></i>
+          </span>
+          <span className="visually-hidden">
+            {__('Next', 'planet4-blocks')}
+          </span>
+        </button>
+
+        {/* Indicators */}
+        <div className="carousel-indicators-wrapper">
+          <div className="container">
+            <ol className="carousel-indicators" tabIndex={-1}>
+              {slides.map((_, index) => (
+                <li
+                  key={index}
+                  {...(index === currentSlide ? {className: 'active'} : {})}
+                >
+                  <button
+                    onClick={() => {
+                      if (index !== currentSlide) {
+                        goToSlide(index);
+                      }
+                    }}
+                    tabIndex={0}
+                    onKeyDown={e => {
+                      if (
+                        (e.key === 'Enter' || e.key === ' ') &&
+                        index !== currentSlide
+                      ) {
+                        e.preventDefault();
+                        goToSlide(index);
+                      }
+                    }}
+                    aria-label={__('Go to slide', 'planet4-blocks') + ` ${index + 1}`}
+                    aria-current={index === currentSlide ? 'true' : undefined}
+                  />
+                </li>
+              ))}
+            </ol>
+            {hasAutoplay && (
+              <button
+                aria-label={isPaused ?
+                  __('Play Slider', 'planet4-blocks') :
+                  __('Stop Slider', 'planet4-blocks')}
+                className={`carousel-autoplay-control ${
+                  isPaused ? 'play' : 'stop'
+                }`}
+                onClick={handlePausePlayButton}
+              />
+            )}
+          </div>
+        </div>
+      </>
+    ),
+    [
+      currentSlide,
+      slides,
+      goToPrevSlide,
+      goToNextSlide,
+      goToSlide,
+      isPaused,
+      hasAutoplay,
+    ]
+  );
+};
