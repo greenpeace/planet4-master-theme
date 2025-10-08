97d37fb8ba87d8da3c8cb46ccef35f32893282e0
Maud Leray <maudleray035@gmail.com>
2025-10-08 13:49:12 +0200

    Fix PHP unit tests
    The functionality should be working as expected

--- src/Loader.php
+++ src/Loader.php
@@ -93,6 +93,7 @@ final class Loader
             ImageHandler::class,
             CloudflareTurnstileHandler::class,
             Sentry::class,
+            PostFunctionsHandler::class,
         ];
 
         if (is_admin()) {
@@ -104,7 +105,6 @@ final class Loader
             $this->default_services[] = MediaArchive\Rest::class;
             $this->default_services[] = MigrationStatus::class;
             $this->default_services[] = MediaReplacer::class;
-            $this->default_services[] = PostFunctionsHandler::class;
 
             foreach (Features::external_settings() as $setting_class) {
                 $this->default_services[] = $setting_class;
