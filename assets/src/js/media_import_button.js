b674a21647c19df77e7888ebb5829c3065a7540b
Dan Tovbein <dtovbein@greenpeace.org>
2025-10-20 13:32:56 -0300

    Update Jquery to Vanila JS
    - This is applied to admin notices
    - Export posts within select options
    - Media import button

--- assets/src/js/media_import_button.js
+++ assets/src/js/media_import_button.js
@@ -1,6 +1,13 @@
 /* global mediaImportLabel */
 
-jQuery(() =>{
-  jQuery('.upload-php .wrap .page-title-action')
-    .after(`<a href="upload.php?page=media-picker" class="add-new-h2">${mediaImportLabel}</a>`);
+document.addEventListener('DOMContentLoaded', () => {
+  const pageTitleAction = document.querySelector('.upload-php .wrap .page-title-action');
+
+  if(pageTitleAction) {
+    const mediaImportButton = document.createElement('a');
+    mediaImportButton.href = 'upload.php?page=media-picker';
+    mediaImportButton.className = 'add-new-h2';
+    mediaImportButton.textContent = mediaImportLabel;
+    pageTitleAction.insertAdjacentElement('afterend', mediaImportButton);
+  }
 });
