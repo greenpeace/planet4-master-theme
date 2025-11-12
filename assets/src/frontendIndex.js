4b0ed5b6fbc480b40211a7ef0b8312f90455008a
Pedro Figueroa <figueroash@gmail.com>
2025-11-12 16:41:31 +0100

    PLANET-7727: Clean up the frontendIndex.js file
    - Clean up the frontendIndex.js file.
    - Move elements to a new JS file.

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -3,42 +3,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'regenerator-runtime/runtime';
 
-import {createRoot} from 'react-dom/client';
-import {TableOfContentsFrontend} from './blocks/TableOfContents/TableOfContentsFrontend';
-import {HappyPointFrontend} from './blocks/HappyPoint/HappyPointFrontend';
-import {ColumnsFrontend} from './blocks/Columns/ColumnsFrontend';
-import {TopicLinkFrontend} from './blocks/TopicLink/TopicLinkFrontend';
 import {setupLightboxForImages} from './blocks/components/Lightbox/setupLightboxForImages';
 import {setupParallax} from './blocks/components/Parallax/setupParallax';
-import {SecondaryNavigationFrontend} from './blocks/SecondaryNavigation/SecondaryNavigationFrontend';
-
-// Render React components
-const COMPONENTS = {
-  'planet4-blocks/submenu': TableOfContentsFrontend,
-  'planet4-blocks/happypoint': HappyPointFrontend,
-  'planet4-blocks/columns': ColumnsFrontend,
-  'planet4-blocks/topic-link': TopicLinkFrontend,
-  'planet4-blocks/secondary-navigation': SecondaryNavigationFrontend,
-};
+import {setupBlockFrontend} from './blocks/components/BlockFrontend/setupBlockFrontend';
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-render]').forEach(
-    blockNode => {
-      const blockName = blockNode.dataset.render;
-      if (!COMPONENTS[blockName]) {
-        return;
-      }
-
-      const BlockFrontend = COMPONENTS[blockName];
-      if (!BlockFrontend) {
-        return;
-      }
-      const attributes = JSON.parse(blockNode.dataset.attributes);
-      const rootElement = createRoot(blockNode);
-      rootElement.render(<BlockFrontend {...attributes.attributes} />);
-    }
-  );
-
+  setupBlockFrontend();
   setupLightboxForImages();
   setupParallax();
 });
