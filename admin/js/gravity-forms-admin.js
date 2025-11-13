08684e2a8d2b5f2fe31686a68eaad4d155e90411
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2025-11-13 15:01:10 +0100

    PLANET-7992: Disable Personal Data > General Settings in GF and add a custom message (#2810)
    -  Add a message to the disabled personal data settings in Gravity Forms.

--- admin/js/gravity-forms-admin.js
+++ admin/js/gravity-forms-admin.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Select all retention policy radio buttons
-  const retentionButtons = document.querySelectorAll('#gform_setting_retention_policy input');
-  const retentionDays = document.querySelector('#retention\\[retain_entries_days\\]');
+  const personalDataTab = document.querySelector('#tab_personal-data');
+
+  if (!personalDataTab) {
+    return;
+  }
+
+  const retentionButtons = personalDataTab.querySelectorAll('#gform_setting_retention_policy input');
+  const settingsPanel = personalDataTab.querySelector('.gform-settings-panel__content');
+  const retentionDays = personalDataTab.querySelector('#retention\\[retain_entries_days\\]');
+  const preventIp = personalDataTab.querySelector('#_gform_setting_preventIP');
+
+  // Add a custom message to the "Personal Data > General Settings" panel
+  if (settingsPanel) {
+    const infoDiv = document.createElement('div');
+    infoDiv.style.color = '#e54c3b';
+    infoDiv.style.marginBottom = '20px';
+    infoDiv.textContent = `In order to minimize the storage of personal data in Planet 4, these
+      settings are managed automatically and cannot be changed manually. Ideally all submission
+      data should be synced or exported to other systems.`;
+
+    settingsPanel.prepend(infoDiv);
+  }
 
   // Disable "Retention Policy" buttons if they exist
   if (retentionButtons?.length) {
@@ -11,5 +30,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // Disable "Number of days to retain entries" input if it exists
   if (retentionDays) {
     retentionDays.disabled = true;
+  }
+
+  // Disable "Prevent the storage of IP addresses during form submission" input if it exists
+  if (preventIp) {
+    preventIp.disabled = true;
   }
 });
