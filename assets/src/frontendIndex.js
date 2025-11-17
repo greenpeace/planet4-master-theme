978e03ef1f61e3a6ae2d05340de1f24727c42183
Dan Tovbein <dtovbein@greenpeace.org>
2025-11-17 14:20:46 -0300

    Set up lightbox where it is needed
    - Lazy loading script

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -3,27 +3,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'regenerator-runtime/runtime';
 
-<<<<<<< HEAD
 import {setupLightboxForImages} from './blocks/components/Lightbox/setupLightboxForImages';
 import {setupParallax} from './blocks/components/Parallax/setupParallax';
 import {setupBlockFrontend} from './blocks/components/BlockFrontend/setupBlockFrontend';
-=======
-import {createRoot} from 'react-dom/client';
-import {TableOfContentsFrontend} from './blocks/TableOfContents/TableOfContentsFrontend';
-import {HappyPointFrontend} from './blocks/HappyPoint/HappyPointFrontend';
-import {ColumnsFrontend} from './blocks/Columns/ColumnsFrontend';
-import {TopicLinkFrontend} from './blocks/TopicLink/TopicLinkFrontend';
-import {SecondaryNavigationFrontend} from './blocks/SecondaryNavigation/SecondaryNavigationFrontend';
-
-// Render React components
-const COMPONENTS = {
-  'planet4-blocks/submenu': TableOfContentsFrontend,
-  'planet4-blocks/happypoint': HappyPointFrontend,
-  'planet4-blocks/columns': ColumnsFrontend,
-  'planet4-blocks/topic-link': TopicLinkFrontend,
-  'planet4-blocks/secondary-navigation': SecondaryNavigationFrontend,
-};
->>>>>>> 8468564d (PLANET-7794: Load javascript code only where they are needed)
 
 document.addEventListener('DOMContentLoaded', () => {
   setupBlockFrontend();
