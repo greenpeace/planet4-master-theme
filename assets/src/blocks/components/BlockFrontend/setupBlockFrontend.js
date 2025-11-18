64938271fce9214f0301bee2fd22ea188301be3b
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2025-11-18 09:34:27 +0100

    PLANET-7727: Clean up the `frontendIndex.js` file (#2811)
    - Clean up the frontendIndex.js file.
    - Move elements to a new JS file.

---
+++ assets/src/blocks/components/BlockFrontend/setupBlockFrontend.js
@@ -0,0 +1,39 @@
+import {createRoot} from 'react-dom/client';
+
+import {TableOfContentsFrontend} from '../../TableOfContents/TableOfContentsFrontend.js';
+import {HappyPointFrontend} from '../../HappyPoint/HappyPointFrontend.js';
+import {ColumnsFrontend} from '../../Columns/ColumnsFrontend.js';
+import {TopicLinkFrontend} from '../../TopicLink/TopicLinkFrontend.js';
+import {SecondaryNavigationFrontend} from '../../SecondaryNavigation/SecondaryNavigationFrontend.js';
+
+// Render React components
+const COMPONENTS = {
+  'planet4-blocks/submenu': TableOfContentsFrontend,
+  'planet4-blocks/happypoint': HappyPointFrontend,
+  'planet4-blocks/columns': ColumnsFrontend,
+  'planet4-blocks/topic-link': TopicLinkFrontend,
+  'planet4-blocks/secondary-navigation': SecondaryNavigationFrontend,
+};
+
+export const setupBlockFrontend = () => {
+  document.querySelectorAll('[data-render]').forEach(
+    blockNode => {
+      const blockName = blockNode.dataset.render;
+      if (!COMPONENTS[blockName]) {
+        return;
+      }
+
+      const BlockFrontend = COMPONENTS[blockName];
+      if (!BlockFrontend) {
+        return;
+      }
+      const attributes = JSON.parse(blockNode.dataset.attributes);
+
+      if (!blockNode._reactRoot) {
+        blockNode._reactRoot = createRoot(blockNode);
+      }
+      blockNode._reactRoot.render(<BlockFrontend {...attributes.attributes} />);
+    }
+  );
+
+};
