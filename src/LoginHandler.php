dd01474d7befaeabd38fd122fcc6f787c34c3f9e
Dan Tovbein <dtovbein@greenpeace.org>
2026-02-06 12:31:38 -0300

    Follow up redirection
    - Rollback changes

--- src/LoginHandler.php
+++ src/LoginHandler.php
@@ -53,7 +53,7 @@ class LoginHandler
                 ) {
                     $google_link = html_entity_decode($matches[1], ENT_QUOTES | ENT_HTML5);
 
-                    if (!empty($google_link) && empty($_GET['loggedout']) || $_GET['loggedout'] !== 'true') {
+                    if (!empty($google_link)) {
                         // Clean up the HTML by removing the login form itself
                         $html = preg_replace('/<form[^>]*id=["\']loginform["\'][^>]*>.*?<\/form>/is', '', $html);
 
