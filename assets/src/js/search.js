e8d4d37b101f25713393ab89ad0672951d3e16b6
Pedro Figueroa <figueroash@gmail.com>
2026-02-11 16:10:15 +0100

    PLANET-8110: Investigate dynamic rendering in search page
    - Improve the load more button and the search title

--- assets/src/js/search.js
+++ assets/src/js/search.js
@@ -1,12 +1,10 @@
-/* global localizations */
-
-const showHiddenRow = row => {
-  if (!row) {
-    return;
-  }
-  row.classList.remove('row-hidden');
-  row.style.display = 'block';
-};
+// const showHiddenRow = row => {
+//   if (!row) {
+//     return;
+//   }
+//   row.classList.remove('row-hidden');
+//   row.style.display = 'block';
+// };
 
 const isModalSearch = () => {
   return document.getElementById('filtermodal') &&
@@ -91,77 +89,78 @@ export const setupSearch = () => {
   }
 
   // Add click event for load more button in blocks.
-  const navSearchInput = document.getElementById('search_input');
-  const loadMoreButton = document.querySelector('.btn-load-more-click-scroll');
-  if (loadMoreButton) {
-    loadMoreButton.onclick = () => {
-      const {total_posts, posts_per_load, current_page} = loadMoreButton.dataset;
-      const nextPage = parseInt(current_page) + 1;
-      loadMoreButton.dataset.current_page = nextPage;
+  // const navSearchInput = document.getElementById('search_input');
 
-      const url = new URL(document.documentElement.dataset.base + '/wp-json/planet4/v1/search/');
-      url.searchParams.append('s', navSearchInput.value.trim());
-      url.searchParams.append('paged', nextPage);
-      url.searchParams.append('orderby', orderInput.value);
-      getSelectedFilters(isModalSearch()).forEach(f => {
-        url.searchParams.append(f.name, f.value);
-      });
+  // const loadMoreButton = document.querySelector('.btn-load-more-click-scroll');
+  // if (loadMoreButton) {
+  //   loadMoreButton.onclick = () => {
+  //     const {total_posts, posts_per_load, current_page} = loadMoreButton.dataset;
+  //     const nextPage = parseInt(current_page) + 1;
+  //     loadMoreButton.dataset.current_page = nextPage;
 
-      fetch(url)
-        .then(response => response.text())
-        .then(html => {
-          // Append the response at the bottom of the results and then show it.
-          const searchResults = document.querySelector('.multiple-search-result .list-unstyled');
-          searchResults.innerHTML += html;
+  //     const url = new URL(document.documentElement.dataset.base + '/wp-json/planet4/v1/search/');
+  //     url.searchParams.append('s', navSearchInput.value.trim());
+  //     url.searchParams.append('paged', nextPage);
+  //     url.searchParams.append('orderby', orderInput.value);
+  //     getSelectedFilters(isModalSearch()).forEach(f => {
+  //       url.searchParams.append(f.name, f.value);
+  //     });
 
-          const hiddenRow = document.querySelector('.row-hidden:last-child');
-          showHiddenRow(hiddenRow);
+  //     fetch(url)
+  //       .then(response => response.text())
+  //       .then(html => {
+  //         // Append the response at the bottom of the results and then show it.
+  //         const searchResults = document.querySelector('.multiple-search-result .list-unstyled');
+  //         searchResults.innerHTML += html;
 
-          if (posts_per_load * nextPage > total_posts) {
-            loadMoreButton.style.display = 'none';
-          }
-        }).catch(error => {
-          console.log(error); //eslint-disable-line no-console
-        });
-    };
-  }
+  //         const hiddenRow = document.querySelector('.row-hidden:last-child');
+  //         showHiddenRow(hiddenRow);
+
+  //         if (posts_per_load * nextPage > total_posts) {
+  //           loadMoreButton.style.display = 'none';
+  //         }
+  //       }).catch(error => {
+  //         console.log(error); //eslint-disable-line no-console
+  //       });
+  //   };
+  // }
 
   // Reveal more results just by scrolling down the first 'show_scroll_times' times.
-  let loadMoreCount = 0;
-  let loadedMore = false;
-  window.onscroll = () => {
-    if (!loadMoreButton) {
-      return;
-    }
+  // let loadMoreCount = 0;
+  // let loadedMore = false;
+  // window.onscroll = () => {
+  //   if (!loadMoreButton) {
+  //     return;
+  //   }
 
-    const elementTop = loadMoreButton.offsetTop;
-    const elementHeight = loadMoreButton.clientHeight;
-    const windowHeight = window.innerHeight;
-    const windowScroll = window.scrollY;
-    const loadEarlierOffset = 250;
+  //   const elementTop = loadMoreButton.offsetTop;
+  //   const elementHeight = loadMoreButton.clientHeight;
+  //   const windowHeight = window.innerHeight;
+  //   const windowScroll = window.scrollY;
+  //   const loadEarlierOffset = 250;
 
-    const {posts_per_load, total_posts} = loadMoreButton.dataset;
+  //   const {posts_per_load, total_posts} = loadMoreButton.dataset;
 
-    if (loadMoreCount < localizations.show_scroll_times) {
-      // If next page has not loaded then load next page as soon as scrolling
-      // reaches 'loadEarlierOffset' pixels before the Load more button.
-      if (!loadedMore &&
-        windowScroll > (elementTop + elementHeight - windowHeight - loadEarlierOffset) &&
-        (loadMoreCount + 1) * posts_per_load < total_posts) {
-        loadMoreCount += 1;
-        loadMoreButton.click();
-        loadedMore = true;
+  //   if (loadMoreCount < localizations.show_scroll_times) {
+  //     // If next page has not loaded then load next page as soon as scrolling
+  //     // reaches 'loadEarlierOffset' pixels before the Load more button.
+  //     if (!loadedMore &&
+  //       windowScroll > (elementTop + elementHeight - windowHeight - loadEarlierOffset) &&
+  //       (loadMoreCount + 1) * posts_per_load < total_posts) {
+  //       loadMoreCount += 1;
+  //       loadMoreButton.click();
+  //       loadedMore = true;
 
-        // Add a throttle to avoid multiple scroll events from firing together.
-        setTimeout(() => {
-          loadedMore = false;
-        }, 500);
-      }
-      if (windowScroll > (elementTop + elementHeight - windowHeight)) {
-        const hiddenRows = [...document.querySelectorAll('.row-hidden')];
-        hiddenRows.forEach(showHiddenRow);
-      }
-    }
-    return false;
-  };
+  //       // Add a throttle to avoid multiple scroll events from firing together.
+  //       setTimeout(() => {
+  //         loadedMore = false;
+  //       }, 500);
+  //     }
+  //     if (windowScroll > (elementTop + elementHeight - windowHeight)) {
+  //       const hiddenRows = [...document.querySelectorAll('.row-hidden')];
+  //       hiddenRows.forEach(showHiddenRow);
+  //     }
+  //   }
+  //   return false;
+  // };
 };
--- assets/src/js/search_results.js
+++ assets/src/js/search_results.js
@@ -1,46 +1,109 @@
 import {createRoot} from 'react-dom/client';
 
-const {useEffect} = wp.element;
+const {useEffect, useState} = wp.element;
 
 export default function SearchController({restUrl}) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [foundPosts, setFoundPosts] = useState(0);
+  const [postsPerLoad, setPostsPerLoad] = useState(5);
+  const [loading, setLoading] = useState(false);
+
+  // Fetch search results from the API:
+  const fetchResults = async (page = 1, append = false) => {
+    const wrapper = document.getElementById('search-results-wrapper');
+    const resultsContainer = document.querySelector('#search-results .list-unstyled');
+    const input = document.getElementById('search-page-input');
+
+    if (!wrapper || !resultsContainer || !input) {return;}
+
+    setLoading(true);
+
+    const params = new URLSearchParams();
+    params.set('s', input.value);
+    params.set('paged', page);
+
+    const url = `${restUrl}?${params.toString()}`;
+    const res = await fetch(url, {
+      headers: {'X-Requested-With': 'XMLHttpRequest'},
+    });
+    const data = await res.json();
+
+    const html = data.html.replace(/\n/g, '');
+    resultsContainer.innerHTML = append ? resultsContainer.innerHTML + html : html;
+
+    // Update state
+    setSearchTerm(input.value);
+    setResults(resultsContainer.innerHTML);
+    setCurrentPage(data.current_page);
+    setFoundPosts(data.found_posts);
+    setPostsPerLoad(data.posts_per_load || 5);
+
+    history.pushState({}, '', `?${params.toString()}`);
+    setLoading(false);
+  };
+
+  // Show more results when the Load More button is clicked:
+  const onLoadMore = () => {
+    if (loading) {return;}
+    const nextPage = currentPage + 1;
+    fetchResults(nextPage, true);
+  };
+
+  // Populate the search results list when the Search button is clicked:
+  const onSubmit = e => {
+    e.preventDefault();
+    fetchResults(1, false);
+  };
+
+  // Populate the search results list on component mount:
+  useEffect(() => {
+    fetchResults(1, false);
+  }, []);
+
+  // Update the title:
+  useEffect(() => {
+    const title = document.querySelector('.result-statement');
+    title.innerHTML = `${foundPosts} results for '${searchTerm}'`;
+  }, [results]);
+
+  // Add and remove listeners:
   useEffect(() => {
     const form = document.getElementById('search_form_inner');
-    const results = document.getElementById('search-results');
+    const loadMoreButton = document.querySelector('.btn-load-more-click-scroll');
 
-    if (!form || !results) {return;}
-
-    const wrapper = document.getElementById('search-results-wrapper');
-
-    const onSubmit = async e => {
-      e.preventDefault();
-
-      const params = new URLSearchParams(new FormData(form));
-      const url = `${restUrl}planet4/v1/search?${params.toString()}`;
-
-      wrapper.setAttribute('aria-busy', 'true');
-
-      const res = await fetch(url, {
-        headers: {'X-Requested-With': 'XMLHttpRequest'},
-      });
-
-      const html = await res.text();
-      results.innerHTML = html;
-
-      history.pushState({}, '', `?${params.toString()}`);
-      wrapper.setAttribute('aria-busy', 'false');
+    if (form) {
+      form.addEventListener('submit', onSubmit);
+    }
+    if (loadMoreButton) {
+      loadMoreButton.addEventListener('click', onLoadMore);
     };
 
-    form.addEventListener('submit', onSubmit);
+    return () => {
+      if (form) {
+        form.removeEventListener('submit', onSubmit);
+      }
+      if (loadMoreButton) {
+        loadMoreButton.removeEventListener('click', onLoadMore);
+      }
+    };
+  });
 
-    return () => form.removeEventListener('submit', onSubmit);
-  }, [restUrl]);
+  // Hide the Load More button:
+  useEffect(() => {
+    const loadMoreButton = document.querySelector('.btn-load-more-click-scroll');
+    const showMore = currentPage * postsPerLoad < foundPosts;
+
+    loadMoreButton.style.display = showMore ? 'block' : 'none';
+
+  }, [currentPage, foundPosts, postsPerLoad]);
 
   return null;
 }
 
-
+// Mount the component
 const el = document.getElementById('search-controller');
-
 if (el) {
   createRoot(el).render(
     <SearchController restUrl={el.dataset.restUrl} />
