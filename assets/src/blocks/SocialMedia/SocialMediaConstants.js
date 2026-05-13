6db69aa97d52699368a3880b94e3c98669daf873
Pedro Figueroa <figueroash@gmail.com>
2026-05-13 12:50:24 +0200

    PLANET-8129: Meta block - Fix all of its functions and embed options
    - Meta block - Fix all of its functions and embed options

--- assets/src/blocks/SocialMedia/SocialMediaConstants.js
+++ assets/src/blocks/SocialMedia/SocialMediaConstants.js
@@ -1,9 +1,12 @@
-export const OEMBED_EMBED_TYPE = 'oembed';
-export const FACEBOOK_EMBED_TYPE = 'facebook_page';
+export const INSTAGRAM_EMBED_TYPE = 'instagramPost';
+export const FACEBOOK_PAGE_EMBED_TYPE = 'facebookPage';
+export const FACEBOOK_POST_EMBED_TYPE = 'facebookPost';
 
 export const FACEBOOK_PAGE_TAB_TIMELINE = 'timeline';
 export const FACEBOOK_PAGE_TAB_EVENTS = 'events';
 export const FACEBOOK_PAGE_TAB_MESSAGES = 'messages';
+
+export const INSTAGRAM_JS = 'https://www.instagram.com/embed.js';
 
 export const ALLOWED_OEMBED_PROVIDERS = [
   'facebook',
