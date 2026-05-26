165e74a80c9139ea5d4fe253034045ecd758815b
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2026-05-26 14:09:56 +0200

    PLANET-8129: Meta block - Fix all of its functions and embed options
    - Migrate Instagram

--- src/Migrations/M065ReplaceMetaBlock.php
+++ src/Migrations/M065ReplaceMetaBlock.php
@@ -53,15 +53,38 @@ class M065ReplaceMetaBlock extends MigrationScript
      */
     private static function transform_block(array $block): array
     {
+        if (isset($block['attrs']['social_media_url'])) {
+            $media = $block['attrs']['social_media_url'];
+            $title = $block['attrs']['title'] ?? '';
+            $description = $block['attrs']['description'] ?? '';
+            $alignment = $block['attrs']['alignment_class'] ?? '';
+
+            if (str_contains($media, 'facebook')) {
+                $block_data = self::migrate_facebook($block, $title, $description, $alignment);
+            } else {
+                $block_data = self::migrate_instagram($block, $title, $description, $alignment);
+            }
+
+            $block['attrs']['embed_type'] = $block_data['embed_type'];
+            $block['attrs']['embed_code'] = $block_data['embed_code'];
+
+            $block['innerHTML'] = $block_data['html'];
+            $block['innerContent'][0] = $block_data['html'];
+        }
+
+        var_dump($block);
+        return $block;
+    }
+
+    private static function migrate_facebook(
+        array $block, string $title, string $description, string $alignment
+    ): array
+    {
         $is_facebook_page = isset($block['attrs']['embed_type']) && $block['attrs']['embed_type'] === 'facebook_page';
 
         $fb_plugin = $is_facebook_page ? 'page.php' : 'post.php';
         $fb_url = $block['attrs']['social_media_url'] ?? '';
         $fb_src_base = 'https://www.facebook.com/plugins/' . $fb_plugin . '?href=' . esc_html($fb_url);
-
-        $title = $block['attrs']['title'] ?? '';
-        $description = $block['attrs']['description'] ?? '';
-        $alignment = $block['attrs']['alignment_class'] ?? '';
 
         if ($is_facebook_page) {
             $fb_page_tab = $block['attrs']['facebook_page_tab'] ?? 'timeline';
@@ -77,14 +100,31 @@ class M065ReplaceMetaBlock extends MigrationScript
         '<iframe class="social-media-embed-facebook" src="' . esc_html($fb_src) . '" width="500" height="500" scrolling="no" frameborder="0" allow="encrypted-media" title="Social Media"></iframe>' .
         '</div></section>';
 
-        $block['attrs']['embed_type'] = $is_facebook_page ? 'facebookPage' : 'facebookPost';
-        $block['attrs']['embed_code'] = $fb_url;
+        return [
+            'embed_type' => $is_facebook_page ? 'facebookPage' : 'facebookPost',
+            'embed_code' => $fb_url,
+            'html' => $html,
+        ];
+    }
 
-        $block['innerHTML'] = $html;
-        $block['innerContent'][0] = $html;
+    private static function migrate_instagram(
+        array $block, string $title, string $description, string $alignment
+    ): array
+    {
+        $ig_url = $block['attrs']['social_media_url'] ?? '';
+        $ig_src = basename(rtrim($ig_url, '/'));
 
-        var_dump($block);
+        $html = '<section class="block social-media-block">' .
+        '<header><h2 class="page-section-header">' . esc_html($title) . '</h2></header>' .
+        '<p class="page-section-description">' . esc_html($description) . '</p>' .
+        '<div class="social-media-embed ' . esc_html($alignment) . '">' .
+        '<blockquote class="instagram-media" data-instgrm-captioned="true" data-instgrm-permalink="https://www.instagram.com/reel/' . esc_html($ig_src) . '/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>' .
+        '</div></section>';
 
-        return $block;
+        return [
+            'embed_type' => 'instagramPost',
+            'embed_code' => $ig_src,
+            'html' => $html,
+        ];
     }
 }
