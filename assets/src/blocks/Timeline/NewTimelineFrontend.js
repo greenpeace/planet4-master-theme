4fe840f64bb9ea13c76fef4539db9358330625c3
Maud Leray <maudleray035@gmail.com>
2026-05-28 17:07:01 +0200

    Make sure image url check is case sensitive
    Otherwise some images might be skipped when they shouldn't

--- assets/src/blocks/Timeline/NewTimelineFrontend.js
+++ assets/src/blocks/Timeline/NewTimelineFrontend.js
@@ -57,7 +57,7 @@ export const NewTimelineFrontend = ({attributes}) => {
     const contentId = `timeline-content-${event.day}-${event.month}`;
 
     // Check if the event has a media, and if that media is an image.
-    const hasImage = event.media && event.media.match(/\.(jpeg|jpg|gif|png)$/) !== null;
+    const hasImage = event.media && event.media.match(/\.(jpeg|jpg|gif|png)$/i) !== null;
 
     return (
       <li className="timeline-block-event">
