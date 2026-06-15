43e8a4546bc34175a40ef015cec9015f55476ea1
Maud Leray <maudleray035@gmail.com>
2026-06-15 15:04:33 +0200

    Use metadata.name instead of hardcoded block name
    This was suggested during code review and it's better for consistency with other blocks

--- assets/src/blocks/TableOfContents/view-script.js
+++ assets/src/blocks/TableOfContents/view-script.js
@@ -1,7 +1,8 @@
 import {TableOfContentsFrontend} from './TableOfContentsFrontend';
 import {createRoot} from 'react-dom/client';
+import metadata from './block.json';
 
-document.querySelectorAll('[data-render="planet4-blocks/submenu"]').forEach(
+document.querySelectorAll(`[data-render="${metadata.name}"]`).forEach(
   blockNode => {
     const attributes = JSON.parse(blockNode.dataset.attributes);
     const rootElement = createRoot(blockNode);
