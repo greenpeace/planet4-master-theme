39b7e6c48d53aaee10ae963c888ae95b96f77e34
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2026-06-19 11:23:36 +0200

    PLANET-8197: Improve file organization in the src/ folder
    - Improve file organization in the src/ folder

---
+++ src/Migration/Migrations/M002EnableLazyYoutube.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace P4\MasterTheme\Migration\Migrations;
+
+use P4\MasterTheme\Migration\MigrationRecord;
+use P4\MasterTheme\Migration\MigrationScript;
+
+/**
+ * Turn on the lazy youtube feature everywhere.
+ */
+class M002EnableLazyYoutube extends MigrationScript
+{
+    /**
+     * Perform the actual migration.
+     *
+     * @param MigrationRecord $record Information on the execution, can be used to add logs.
+     * phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter -- interface implementation
+     */
+    protected static function execute(MigrationRecord $record): void
+    {
+        $features = get_option('planet4_features');
+        $features['lazy_youtube_player'] = true;
+        update_option('planet4_features', $features);
+    }
+    // phpcs:enable SlevomatCodingStandard.Functions.UnusedParameter
+}
---
+++ src/Migration/Migrations/M007RemoveEnhancedDonateButtonOption.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace P4\MasterTheme\Migration\Migrations;
+
+use P4\MasterTheme\Migration\MigrationRecord;
+use P4\MasterTheme\Migration\MigrationScript;
+use P4\MasterTheme\Settings;
+
+/**
+ * Remove the "enchanced donate button" option from Planet 4 settings.
+ */
+class M007RemoveEnhancedDonateButtonOption extends MigrationScript
+{
+    /**
+     * Perform the actual migration.
+     *
+     * @param MigrationRecord $record Information on the execution, can be used to add logs.
+     * phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter -- interface implementation
+     */
+    protected static function execute(MigrationRecord $record): void
+    {
+        $options = get_option(Settings::KEY);
+        unset($options['donate_btn_visible_on_mobile']);
+        update_option(Settings::KEY, $options);
+    }
+    // phpcs:enable SlevomatCodingStandard.Functions.UnusedParameter
+}
