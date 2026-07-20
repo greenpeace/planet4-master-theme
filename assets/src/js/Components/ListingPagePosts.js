f4e4c573bddae5c83bdbb0fd94a3d36f0e868c04
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2026-07-20 11:03:18 +0200

    PLANET-8181: Dynamically reload posts on filters selection
    - Add filter component

--- assets/src/js/Components/ListingPagePosts.js
+++ assets/src/js/Components/ListingPagePosts.js
@@ -1,9 +1,18 @@
-import {useCallback, useState, useEffect} from '@wordpress/element';
+import {useCallback, useState, useEffect, createPortal} from '@wordpress/element';
 import {fetchJson} from '../../functions/fetchJson';
 import {addQueryArgs} from '../../functions/addQueryArgs';
+import ListingPageFilters from './ListingPageFilters';
 
-const ListingPagePosts = () => {
+const ListingPagePosts = ({filtersContainer}) => {
   const [posts, setPosts] = useState([]);
+  const [postTypes, setPostTypes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [filters, setFilters] = useState({
+    postType: '',
+    category: '',
+    tag: '',
+  });
 
   function PostItem({post}) {
     return (
@@ -14,6 +23,25 @@ const ListingPagePosts = () => {
     );
   }
 
+  const getTaxonomies = useCallback(async () => {
+    try {
+      const baseUrl = document.body.dataset.nro;
+
+      const [postTypesData, categoriesData, tagsData] = await Promise.all([
+        fetchJson(`${baseUrl}/wp-json/${addQueryArgs('wp/v2/p4-page-type', {per_page: 100, hide_empty: true})}`),
+        fetchJson(`${baseUrl}/wp-json/${addQueryArgs('wp/v2/categories', {per_page: 100, hide_empty: true})}`),
+        fetchJson(`${baseUrl}/wp-json/${addQueryArgs('wp/v2/tags', {per_page: 100, hide_empty: true})}`),
+      ]);
+
+      setPostTypes(Array.isArray(postTypesData) ? postTypesData : []);
+      setCategories(Array.isArray(categoriesData) ? categoriesData : []);
+      setTags(Array.isArray(tagsData) ? tagsData : []);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+  }, []);
+
   const getPosts = useCallback(async () => {
     try {
       const args = {
@@ -22,28 +50,58 @@ const ListingPagePosts = () => {
         offset: 0,
       };
 
+      if (filters.postType) {
+        args['p4-page-type'] = filters.postType; // already a number from FilterSelect
+      }
+      if (filters.category) {
+        args.categories = filters.category;
+      }
+      if (filters.tag) {
+        args.tags = filters.tag;
+      }
+
       const baseUrl = document.body.dataset.nro;
 
       const data = await fetchJson(`${baseUrl}/wp-json/${addQueryArgs('wp/v2/posts', args)}`);
 
-      setPosts(data);
-
+      setPosts(Array.isArray(data) ? data : []);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e);
     }
-  }, []);
+  }, [filters]);
+
+  useEffect(() => {
+    getTaxonomies();
+  }, [getTaxonomies]);
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [getPosts]);
 
   return (
-    <ul>
-      {posts.map(post => (
-        <PostItem key={post.id} post={post} />
-      ))}
-    </ul>);
+    <>
+      { filtersContainer &&
+        createPortal(
+          <ListingPageFilters
+            postTypes={postTypes}
+            categories={categories}
+            tags={tags}
+            currentPostType={filters.postType}
+            currentCategory={filters.category}
+            currentTag={filters.tag}
+            onApply={setFilters}
+          />,
+          filtersContainer
+        ) }
+
+      <ul>
+        { posts.map(post => (
+          <PostItem key={post.id} post={post} />
+        )) }
+      </ul>
+    </>
+  );
 };
 
 export default ListingPagePosts;
