860e3c933088c7dd5efce5a983bd8f9563fdd12b
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2026-07-22 08:58:56 +0200

    PLANET-8181: Dynamically reload posts on filters selection
    - Fix paginator layout

--- assets/src/js/Components/Paginator.js
+++ assets/src/js/Components/Paginator.js
@@ -1,38 +1,119 @@
-import {__, sprintf} from '@wordpress/i18n';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import {__} from '@wordpress/i18n';
+
+function getPageNumbers(currentPage, totalPages) {
+  const delta = 2; // how many pages to show around the current page
+  const range = [];
+  const rangeWithDots = [];
+  let last;
+
+  for (let i = 1; i <= totalPages; i++) {
+    if (i === 1 || i === totalPages || (i >= currentPage - delta && i <= currentPage + delta)) {
+      range.push(i);
+    }
+  }
+
+  range.forEach(i => {
+    if (last) {
+      if (i - last === 2) {
+        rangeWithDots.push(last + 1);
+      } else if (i - last > 2) {
+        rangeWithDots.push('...');
+      }
+    }
+    rangeWithDots.push(i);
+    last = i;
+  });
+
+  return rangeWithDots;
+}
+
+function handleClick(event, page, onPageChange) {
+  event.preventDefault();
+  onPageChange(page);
+}
+
+function PageNumber({page, currentPage, onPageChange}) {
+  if (page === '...') {
+    return (
+      <span className="page-numbers dots">
+				…
+      </span>
+    );
+  }
+
+  if (page === currentPage) {
+    return (
+      <span aria-current="page" className="page-numbers current">
+        { page }
+      </span>
+    );
+  }
+
+  return (
+    <a
+      href="#"
+      className="page-numbers"
+      onClick={event => handleClick(event, page, onPageChange)}
+    >
+      { page }
+    </a>
+  );
+}
 
 function Paginator({currentPage, totalPages, onPageChange}) {
   if (totalPages <= 1) {
     return null;
   }
 
+  const isFirstPage = currentPage <= 1;
+  const isLastPage = currentPage >= totalPages;
+  const pageNumbers = getPageNumbers(currentPage, totalPages);
+
   return (
-    <nav className="listing-page-paginator" aria-label="Pagination">
-      <button
-        type="button"
-        className="btn btn-secondary"
-        disabled={currentPage <= 1}
-        onClick={() => onPageChange(currentPage - 1)}
+    <nav className="wp-block-query-pagination is-layout-flex wp-block-query-pagination-is-layout-flex" aria-label="Pagination">
+      <a
+        href="#"
+        className={`wp-block-query-pagination-previous${isFirstPage ? ' disabled' : ''}`}
+        tabIndex={isFirstPage ? -1 : undefined}
+        aria-hidden={isFirstPage ? 'true' : undefined}
+        onClick={event => {
+          if (isFirstPage) {
+            event.preventDefault();
+            return;
+          }
+          handleClick(event, currentPage - 1, onPageChange);
+        }}
       >
-        { __('Previous', 'planet4-master-theme') }
-      </button>
+        { __('Prev', 'planet4-master-theme') }
+      </a>
 
-      <span className="listing-page-paginator-status">
-        { sprintf(
-          /* translators: %d: page number */
-          __('Page %1$d of %2$d', 'planet4-master-theme'),
-          currentPage,
-          totalPages
-        ) }
-      </span>
+      <div className="wp-block-query-pagination-numbers">
+        { pageNumbers.map((page, index) => (
+          <PageNumber
+            // eslint-disable-next-line react/no-array-index-key
+            key={page === '...' ? `dots-${index}` : page}
+            page={page}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+          />
+        )) }
+      </div>
 
-      <button
-        type="button"
-        className="btn btn-secondary"
-        disabled={currentPage >= totalPages}
-        onClick={() => onPageChange(currentPage + 1)}
+      <a
+        href="#"
+        className={`wp-block-query-pagination-next${isLastPage ? ' disabled' : ''}`}
+        onClick={event => {
+          if (isLastPage) {
+            event.preventDefault();
+            return;
+          }
+          handleClick(event, currentPage + 1, onPageChange);
+        }}
       >
         { __('Next', 'planet4-master-theme') }
-      </button>
+      </a>
     </nav>
   );
 }
