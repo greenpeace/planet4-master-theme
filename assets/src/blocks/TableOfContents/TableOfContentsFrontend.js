9ecbefe7ddb18cb7578e023d871212f3a0bb7502
Osong Agberndifor <38656549+Osong-Michael@users.noreply.github.com>
2026-07-27 13:02:53 +0100

    PLANET-8229 New Timeline block: Added heading to ToC block (#3049)
    - Added function to include Timeline block heading to ToC in editor and frontend

--- assets/src/blocks/TableOfContents/TableOfContentsFrontend.js
+++ assets/src/blocks/TableOfContents/TableOfContentsFrontend.js
@@ -2,9 +2,28 @@ import {getTableOfContentsStyle} from './getTableOfContentsStyle';
 import {TableOfContentsItems} from './TableOfContentsItems';
 import {makeHierarchical} from './makeHierarchical';
 import {getHeadingsFromDom} from '../../functions/getHeadingsFromDom';
+import {observeTimelineHeadings} from '../../functions/timelineObserver';
+
+const {useState, useEffect} = wp.element;
 
 export const TableOfContentsFrontend = ({title, className, levels, submenu_style}) => {
-  const headings = getHeadingsFromDom(levels);
+
+  const [headings, setHeadings] = useState([]);
+
+  useEffect(() => {
+    const updateHeadings = () => {
+      setHeadings(getHeadingsFromDom(levels));
+    };
+
+    updateHeadings();
+
+    const observer = observeTimelineHeadings(updateHeadings);
+
+    return () => {
+      observer?.();
+    };
+  }, [levels]);
+
   const menuItems = makeHierarchical(headings);
   const style = getTableOfContentsStyle(className, submenu_style);
 
