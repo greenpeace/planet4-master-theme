f6de28ea9a1d0207606afdb3dbc95abe20d5cfc5
sagarsdeshmukh <sagarsdeshmukh91@gmail.com>
2026-09-01 15:55:51 +0530

    PLANET-8264 Added a space between title and help text
    Ref. https://greenpeace-planet4.atlassian.net/browse/PLANET-8264

--- assets/src/block-editor/Sidebar/SearchEngineOptimizationsSidebar.js
+++ assets/src/block-editor/Sidebar/SearchEngineOptimizationsSidebar.js
@@ -21,34 +21,36 @@ export const SearchEngineOptimizationsSidebar = {
         name="planet4-search-engine-optimizations"
         title={__('Search Engine Optimizations', 'planet4-master-theme-backend')}
       >
-        <p className="components-base-control__help">
-          {createInterpolateElement(
-            __('Check optimal length at <a>SERP Snippet Generator</a>.', 'planet4-master-theme-backend'),
-            {
-              a: (
-                <ExternalLink href="https://app.sistrix.com/en/serp-snippet-generator" />
-              ),
-            }
-          )}
-        </p>
-        <TextControl
-          label={__('Meta Title', 'planet4-master-theme-backend')}
-          value={meta[META_TITLE] || ''}
-          onChange={value => editPost({meta: {[META_TITLE]: value}})}
-          help={__('Leave empty to use the Open Graph Title (if set) or the Post Title.', 'planet4-master-theme-backend')}
-        />
-        <TextareaControl
-          label={__('Meta Description', 'planet4-master-theme-backend')}
-          value={meta[META_DESCRIPTION] || ''}
-          onChange={value => editPost({meta: {[META_DESCRIPTION]: value}})}
-          help={__('Leave empty to use Open Graph Description (if set) or the Post Excerpt.', 'planet4-master-theme-backend')}
-        />
-        <URLInput
-          label={__('Canonical link', 'planet4-master-theme-backend')}
-          value={meta[CANONICAL_URL]}
-          onChange={value => editPost({meta: {[CANONICAL_URL]: value}})}
-          help={__('If emtpy a self-reference canonical link will be used', 'planet4-master-theme-backend')}
-        />
+        <div className="planet4-seo-sidebar">
+          <p className="components-base-control__help">
+            {createInterpolateElement(
+              __('Check optimal length at <a>SERP Snippet Generator</a>.', 'planet4-master-theme-backend'),
+              {
+                a: (
+                  <ExternalLink href="https://app.sistrix.com/en/serp-snippet-generator" />
+                ),
+              }
+            )}
+          </p>
+          <TextControl
+            label={__('Meta Title', 'planet4-master-theme-backend')}
+            value={meta[META_TITLE] || ''}
+            onChange={value => editPost({meta: {[META_TITLE]: value}})}
+            help={__('Leave empty to use the Open Graph Title (if set) or the Post Title.', 'planet4-master-theme-backend')}
+          />
+          <TextareaControl
+            label={__('Meta Description', 'planet4-master-theme-backend')}
+            value={meta[META_DESCRIPTION] || ''}
+            onChange={value => editPost({meta: {[META_DESCRIPTION]: value}})}
+            help={__('Leave empty to use Open Graph Description (if set) or the Post Excerpt.', 'planet4-master-theme-backend')}
+          />
+          <URLInput
+            label={__('Canonical link', 'planet4-master-theme-backend')}
+            value={meta[CANONICAL_URL]}
+            onChange={value => editPost({meta: {[CANONICAL_URL]: value}})}
+            help={__('If emtpy a self-reference canonical link will be used', 'planet4-master-theme-backend')}
+          />
+        </div>
       </PluginDocumentSettingPanel>
     );
   },
