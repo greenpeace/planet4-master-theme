a2e584c2b0f2548db2cc91c0148fb62e55c77a3f
Dan Tovbein <dtovbein@greenpeace.org>
2026-09-08 15:17:45 -0300

    Add pattern migration file as a boilerplate

---
+++ src/Migrations/M068MigratePatternTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace P4\MasterTheme\Migrations;
+
+use P4\MasterTheme\MigrationRecord;
+use P4\MasterTheme\MigrationScript;
+
+/**
+ * Migrate the Test Pattern.
+ */
+class M068MigratePatternTest extends MigrationScript
+{
+    /**
+     * Perform the actual migration.
+     *
+     * @param MigrationRecord $record Information on the execution, can be used to add logs.
+     * phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter -- interface implementation
+     */
+    protected static function execute(MigrationRecord $record): void
+    {
+        echo "Execute pattern migration" . "<br>";
+    }
+}
--- src/Patterns/TestPattern.php
+++ src/Patterns/TestPattern.php
@@ -40,4 +40,3 @@ class TestPattern extends BlockPattern
         ];
     }
 }
- 
